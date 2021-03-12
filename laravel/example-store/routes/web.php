c7a154efa652ecfa8f59b88025877e4a988846cf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-12 10:18:19 +0100

    Update web.php

--- laravel/example-store/routes/web.php
+++ laravel/example-store/routes/web.php
@@ -23,5 +23,5 @@ Route::any('/any', function () {
 });
 
 Route::get('/parametric/path/{id?}', function ($id=1) {
-    return 'Your request has an identification parameter: ' . $id;
-});
+    return 'Your request has an identifying numeric parameter: ' . $id;
+})->where(['id' => '[0-9]+']);
