af510f9eb7229649f539689387045f4c1be0001d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-12 10:33:56 +0100

    Update web.php

--- laravel/example-store/routes/web.php
+++ laravel/example-store/routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', [ 'as' => 'home', function () {
     return view('welcome');
+}]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
  
 Route::get('/hello', function () {
