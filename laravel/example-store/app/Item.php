f8d7bf2c504535836ca955f5d0e8fbabb27f0d57
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-14 10:03:43 +0100

    Update Item.php

--- laravel/example-store/app/Item.php
+++ laravel/example-store/app/Item.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $table = 'item';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $fillable = ['name','description','price'];
 }
