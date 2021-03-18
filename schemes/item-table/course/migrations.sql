30001f5ecba73f17cd9d208c0c551273745055e5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-18 21:38:58 +0100

    Create migrations.sql

---
+++ schemes/item-table/course/migrations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE example_store.migrations (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
