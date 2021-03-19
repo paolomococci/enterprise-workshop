a5377612e4e07c3b81f95c2e10eb7bdae5197413
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-19 21:33:27 +0100

    Update DatabaseSeeder.php

--- laravel/course/database/seeds/DatabaseSeeder.php
+++ laravel/course/database/seeds/DatabaseSeeder.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
-    {}
+    {
+        $faker = Faker::create();
+        // TODO
+    }
 }
