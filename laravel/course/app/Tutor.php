bfcd1796432ad004c91e2469772af1aba5b39bba
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 07:02:05 +0100

    Update Tutor.php

--- laravel/course/app/Tutor.php
+++ laravel/course/app/Tutor.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tutor extends Model
 {
-    //
+    use HasFactory;
+    public $fillable = ['name', 'surname', 'email',];
 }
