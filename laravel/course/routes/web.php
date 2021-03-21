fba1637d8a75d28f3bcbfafa7d7c09150a34afd9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 07:00:36 +0100

    Update web.php

--- laravel/course/routes/web.php
+++ laravel/course/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TutorController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tutors', TutorController::class);
