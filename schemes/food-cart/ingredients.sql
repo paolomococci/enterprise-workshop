d8298044656ebf8aca0448a35d9c39fc81219e81
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 11:03:41 +0100

    Create ingredients.sql

---
+++ schemes/food-cart/ingredients.sql
@@ -0,0 +1,10 @@
+CREATE TABLE food_cart_db.ingredients (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  category varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  description mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  price decimal(8,3) NOT NULL,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
