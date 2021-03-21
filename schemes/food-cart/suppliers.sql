94c5b83f5c0a07a2c708e82332bdc01455bb28dd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-21 11:11:45 +0100

    Create suppliers.sql

---
+++ schemes/food-cart/suppliers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE food_cart_db.suppliers (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  surname varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  email varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
