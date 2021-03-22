b74c15100b387626a3df1807ae9f9cb70f1cb592
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-22 02:25:23 +0100

    Create IngredientFactory.php

---
+++ laravel/food-cart/database/factories/IngredientFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Ingredient;
+use Faker\Generator as Faker;
+
+$factory->define(Ingredient::class, function (Faker $faker) {
+    return [];
+});
