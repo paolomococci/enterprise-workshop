5b53a564a793e5502172220da8a7a7ca219daae6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-22 09:09:39 +0100

    Update 2021_03_21_070930_create_ingredients_table.php

--- laravel/food-cart/database/migrations/2021_03_21_070930_create_ingredients_table.php
+++ laravel/food-cart/database/migrations/2021_03_21_070930_create_ingredients_table.php
@@ -1,1 +1,35 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('category');
+            $table->mediumText('description')->nullable();
+            $table->decimal('price', 8, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
