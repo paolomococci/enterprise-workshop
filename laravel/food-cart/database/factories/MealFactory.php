a240df87b3d77f5b6b1dea7b4d26850056e5a795
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-22 02:26:17 +0100

    Create MealFactory.php

---
+++ laravel/food-cart/database/factories/MealFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Meal;
+use Faker\Generator as Faker;
+
+$factory->define(Meal::class, function (Faker $faker) {
+    return [];
+});
