39e05a82f2f7f0383d02030c87f6a9c2b1dec20f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-22 09:04:51 +0100

    Update api.php

--- laravel/food-cart/routes/api.php
+++ laravel/food-cart/routes/api.php
@@ -1,1 +1,20 @@
+<?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\MealController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\SupplierController;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/customers', [CustomerController::class, 'index']);
+Route::get('/ingredients', [IngredientController::class, 'index']);
+Route::get('/meals', [MealController::class, 'index']);
+Route::get('/recipes', [RecipeController::class, 'index']);
+Route::get('/suppliers', [SupplierController::class, 'index']);
