9dcc4797677756cfdd82bb43171178e93a2320ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-23 15:47:13 +0100

    Update CustomerSeeder.php

--- laravel/food-cart/database/seeds/CustomerSeeder.php
+++ laravel/food-cart/database/seeds/CustomerSeeder.php
@@ -12,6 +12,32 @@ class CustomerSeeder extends Seeder
     public function run()
     {
         DB::table('customers')->insert([
+            // a series of completely invented names for the sole purpose of example
+            [
+                'name' => 'Susan',
+                'surname' => 'Weave',
+                'email' => 'susan.weave@example.local',
+            ],
+            [
+                'name' => 'Fiona',
+                'surname' => 'Win',
+                'email' => 'fiona.win@example.local',
+            ],
+            [
+                'name' => 'John',
+                'surname' => 'Tell',
+                'email' => 'john.tell@example.local',
+            ],
+            [
+                'name' => 'Ann',
+                'surname' => 'Think',
+                'email' => 'ann.think@example.local',
+            ],
+            [
+                'name' => 'Bob',
+                'surname' => 'Swing',
+                'email' => 'bob.swing@example.local',
+            ],
             // TODO
         ]);
     }
