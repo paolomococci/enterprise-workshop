36fa1143a6d0a5a0f66a924fa4a4bfa27ed9cdc3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-24 15:52:43 +0100

    Update 2021_03_24_140125_create_ingredient_recipe_relation_table.php

--- laravel/food-cart/database/migrations/2021_03_24_140125_create_ingredient_recipe_relation_table.php
+++ laravel/food-cart/database/migrations/2021_03_24_140125_create_ingredient_recipe_relation_table.php
@@ -1,1 +1,31 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientRecipeRelationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient_recipe_relation', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredient_recipe_relation');
+    }
+}
