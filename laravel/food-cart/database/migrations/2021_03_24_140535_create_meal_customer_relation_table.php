bac9373a41725ab9562e2f00ad8faf25922e9141
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-24 15:53:43 +0100

    Update 2021_03_24_140535_create_meal_customer_relation_table.php

--- laravel/food-cart/database/migrations/2021_03_24_140535_create_meal_customer_relation_table.php
+++ laravel/food-cart/database/migrations/2021_03_24_140535_create_meal_customer_relation_table.php
@@ -1,1 +1,31 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealCustomerRelationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal_customer_relation', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal_customer_relation');
+    }
+}
