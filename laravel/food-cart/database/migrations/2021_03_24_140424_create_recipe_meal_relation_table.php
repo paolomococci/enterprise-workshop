bda9a4409fdd370b94c23f0efdd3eeff382bfae5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-25 18:43:14 +0100

    Update 2021_03_24_140424_create_recipe_meal_relation_table.php

--- laravel/food-cart/database/migrations/2021_03_24_140424_create_recipe_meal_relation_table.php
+++ laravel/food-cart/database/migrations/2021_03_24_140424_create_recipe_meal_relation_table.php
@@ -14,7 +14,7 @@ class CreateRecipeMealRelationTable extends Migration
     public function up()
     {
         Schema::create('recipe_meal_relation', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->timestamps();
         });
     }
