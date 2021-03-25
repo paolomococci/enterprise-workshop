1da98d08a1876d22659bbc80f403a9bd26994d96
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-25 19:10:11 +0100

    Update recipe_meal_relation.sql

--- schemes/food-cart/recipe_meal_relation.sql
+++ schemes/food-cart/recipe_meal_relation.sql
@@ -1,6 +1,12 @@
 CREATE TABLE food_cart_db.recipe_meal_relation (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `recipe_id` bigint(20) unsigned NOT NULL,
+  `meal_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `recipe_meal_relation_recipe_id_index` (`recipe_id`),
+  KEY `recipe_meal_relation_meal_id_index` (`meal_id`),
+  CONSTRAINT `recipe_meal_relation_meal_id_foreign` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`id`),
+  CONSTRAINT `recipe_meal_relation_recipe_id_foreign` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
