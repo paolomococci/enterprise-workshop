66f06323d6b03dcc8c3756f136520d1ee52cecd8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-25 19:02:20 +0100

    Update 2021_03_24_140125_create_ingredient_recipe_relation_table.php

--- laravel/food-cart/database/migrations/2021_03_24_140125_create_ingredient_recipe_relation_table.php
+++ laravel/food-cart/database/migrations/2021_03_24_140125_create_ingredient_recipe_relation_table.php
@@ -18,6 +18,9 @@ class CreateIngredientRecipeRelationTable extends Migration
             $table->bigInteger('ingredient_id')->unsigned()->index();
             $table->bigInteger('recipe_id')->unsigned()->index();
             $table->timestamps();
+            
+            $table->foreign('ingredient_id')->references('id')->on('ingredients');
+            $table->foreign('recipe_id')->references('id')->on('recipes');
         });
     }
 
