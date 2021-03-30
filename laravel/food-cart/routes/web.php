19569d02131a4c8583dc3a4047149fee8f89a1a9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-30 09:23:52 +0200

    Update web.php

--- laravel/food-cart/routes/web.php
+++ laravel/food-cart/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource(
+    'recipes', 
+    RecipeController::class
+);
