e5943d26b34975a70b58f268b71f791391da29ae
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-31 08:16:59 +0200

    Update MealController.php

--- laravel/food-cart/app/Http/Controllers/MealController.php
+++ laravel/food-cart/app/Http/Controllers/MealController.php
@@ -23,7 +23,7 @@ class MealController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create() {
-        // TODO
+        return view('meals.create');
     }
     
     /**
