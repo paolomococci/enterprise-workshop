d95bd4ccf405f909cc78fcedc9d193acba6d82f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-03-31 07:18:54 +0200

    Update Recipe.php

--- laravel/food-cart/app/Models/Recipe.php
+++ laravel/food-cart/app/Models/Recipe.php
@@ -7,22 +7,5 @@ use Illuminate\Support\Str;
 
 class Recipe extends Model
 {
-    protected static function boot() {
-        parent::boot();
-        static::creating(function ($recipe) {
-            $recipe->{$recipe->getKeyName()} = (string) Str::uuid();
-        });
-    }
-    
-    public function getIncrementing() {
-        return false;
-    }
-    
-    public function getKeyType() {
-        return 'string';
-    }
-    
-    public function ingredients() {
-        return $this->belongsToMany('App\Models\Ingredient')->using('App\Models\IngredientRecipe')->withPivot(['quantity']);
-    }
+    protected $fillable = ['id', 'name', 'category', 'todo'];
 }
