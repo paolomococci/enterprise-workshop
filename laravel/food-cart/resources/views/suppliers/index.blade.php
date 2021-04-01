a193c97396146432478c9b35bd7c4299c5bcbf6d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-01 07:05:42 +0200

    Create index.blade.php

---
+++ laravel/food-cart/resources/views/suppliers/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ url('css/welcome.css')}}" type="text/css" media="screen"/>
+        <title>index</title>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    view of the supplier index
+                </div>
+
+                <div class="links">
+                    <a href="#">TODO</a>
+                    <a href="#">TODO</a>
+                    <a href="#">TODO</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
