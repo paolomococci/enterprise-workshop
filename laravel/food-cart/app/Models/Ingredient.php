2800fb5c5cf115a6363e182b04d341c3e97d71bf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-02 07:44:34 +0200

    Update Ingredient.php

--- laravel/food-cart/app/Models/Ingredient.php
+++ laravel/food-cart/app/Models/Ingredient.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    protected $fillable = ['id', 'name', 'category', 'description', 'price'];
+    protected $guarded = ['id'];
 }
