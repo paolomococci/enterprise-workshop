d7811705396d2a814b0dc7acad9f9a9e296af57e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-04 08:39:53 +0200

    Update layout.blade.php

--- laravel/ateneo/resources/views/layout.blade.php
+++ laravel/ateneo/resources/views/layout.blade.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>ateneo</title>
+		<link rel="stylesheet" media="screen" href="{{ asset('css/app.css') }}">
+	</head>
+	<body>
+		<div class="container">
+			@yield('content')
+		</div>
+		<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+	</body>
+</html>
