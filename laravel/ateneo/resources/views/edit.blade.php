1c3795153805a2ab97a7972a495f89a6619f0282
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-04 08:41:01 +0200

    Update edit.blade.php

--- laravel/ateneo/resources/views/edit.blade.php
+++ laravel/ateneo/resources/views/edit.blade.php
@@ -1,1 +1,59 @@
+@extends('layout')
 
+@section('content')
+<div class="card push-top">
+	<div class="card-header">
+		edit/update
+	</div>
+
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('students.update', $student->id) }}">
+			<div class="form-group">
+				@csrf
+				@method('PATCH')
+				<label for="name">name</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="name" 
+					value="{{ $student->name }}"/>
+			</div>
+			<div class="form-group">
+				<label for="surname">surname</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="surname" 
+					value="{{ $student->surname }}"/>
+			</div>
+			<div class="form-group">
+				<label for="email">email</label>
+				<input 
+					type="email" 
+					class="form-control" 
+					name="email" 
+					value="{{ $student->email }}"/>
+			</div>
+			<div class="form-group">
+				<label for="phone">phone</label>
+				<input 
+					type="tel" 
+					class="form-control" 
+					name="phone" 
+					pattern="^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$" 
+					value="{{ $student->phone }}"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-danger">update student</button>
+		</form>
+	</div>
+</div>
+@endsection
