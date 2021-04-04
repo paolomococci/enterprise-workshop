7865fd528ce0ff67b01ac08ee1c018b0fcee2a35
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-04 08:12:22 +0200

    Update welcome.blade.php

--- laravel/ateneo/resources/views/welcome.blade.php
+++ laravel/ateneo/resources/views/welcome.blade.php
@@ -11,11 +11,11 @@
 			@if (Route::has('login'))
 				<div class="top-right links">
 					@auth
-						<a href="{{ url('/home') }}">Home</a>
+						<a href="{{ url('/home') }}">home</a>
 					@else
-						<a href="{{ route('login') }}">Login</a>
+						<a href="{{ route('login') }}">login</a>
 						@if (Route::has('register'))
-							<a href="{{ route('register') }}">Register</a>
+							<a href="{{ route('register') }}">register</a>
 						@endif
 					@endauth
 				</div>
