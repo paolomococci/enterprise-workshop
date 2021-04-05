cdcd73032f39f932d93c9b3dc0f81cb995913bc1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 12:08:55 +0200

    Update 2021_04_05_014837_create_teachers_table.php

--- laravel/ateneo/database/migrations/2021_04_05_014837_create_teachers_table.php
+++ laravel/ateneo/database/migrations/2021_04_05_014837_create_teachers_table.php
@@ -15,6 +15,10 @@ class CreateTeachersTable extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('emai');
+            $table->string('phone');
             $table->timestamps();
         });
     }
