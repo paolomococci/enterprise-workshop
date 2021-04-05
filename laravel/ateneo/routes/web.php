6f2ba7a7e91f35e8029fc816ba35e70b87fa699c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 10:22:51 +0200

    Update web.php

--- laravel/ateneo/routes/web.php
+++ laravel/ateneo/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('students', 'StudentController');
+Route::resource('tutors', 'TutorController');
+Route::resource('teachers', 'TeacherController');
+Route::resource('employees', 'EmployeeController');
+Route::resource('chancellors', 'ChancellorController');
+Route::resource('presidents', 'PresidentController');
