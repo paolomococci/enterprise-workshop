9c62ec26d2d38296cd83692aca4973535d39fb16
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 16:44:33 +0200

    Update master.blade.php

--- laravel/ateneo/resources/views/layouts/master.blade.php
+++ laravel/ateneo/resources/views/layouts/master.blade.php
@@ -5,11 +5,29 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>ateneo</title>
-		<link rel="stylesheet" media="screen" href="{{ asset('css/app.css') }}">
+		<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}">
+		<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/welcome.css') }}">
 	</head>
 	<body>
-		<section class="container">
-			@yield('content')
+		<section class="content">
+				<aside class="title m-b-md">
+					<a href="/" title="home">ateneo</a>
+				</aside>
+
+				<aside class="links">
+					<a href="/students">studests</a>
+					<a href="/students/create">add student</a>
+					<a href="#">todo</a>
+					<a href="#">todo</a>
+					<a href="#">todo</a>
+					<a href="#">todo</a>
+					<a href="#">todo</a>
+					<a href="#">todo</a>
+				</aside>
+				
+				<section class="container">
+					@yield('content')
+				</section>
 		</section>
 		<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 	</body>
