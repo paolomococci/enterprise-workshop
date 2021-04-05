d39b52e7af404a3b72a613971c47e58eb130fd1e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 09:28:50 +0200

    Create students.sql

---
+++ schemes/ateneo/students.sql
@@ -0,0 +1,10 @@
+CREATE TABLE ateneo_db.students (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `emai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
