04bfc6659e014cbaa017d55ab042cf3e9ba75c60
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 09:36:48 +0200

    Create PresidentController.php

---
+++ laravel/ateneo/app/Http/Controllers/PresidentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\President;
+use Illuminate\Http\Request;
+
+class PresidentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\President  $president
+     * @return \Illuminate\Http\Response
+     */
+    public function show(President $president)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\President  $president
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(President $president)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\President  $president
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, President $president)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\President  $president
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(President $president)
+    {
+        //
+    }
+}
