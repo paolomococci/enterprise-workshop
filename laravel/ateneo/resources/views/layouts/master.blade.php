1a045992fc091f2eeedf7562236c46c4bd59d8bb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 17:35:42 +0200

    Update master.blade.php

--- laravel/ateneo/resources/views/layouts/master.blade.php
+++ laravel/ateneo/resources/views/layouts/master.blade.php
@@ -14,16 +14,56 @@
 					<a href="/" title="home">ateneo</a>
 				</aside>
 
-				<aside class="links">
-					<a href="/students">studests</a>
-					<a href="/students/create">add student</a>
-					<a href="#">todo</a>
-					<a href="#">todo</a>
-					<a href="#">todo</a>
-					<a href="#">todo</a>
-					<a href="#">todo</a>
-					<a href="#">todo</a>
-				</aside>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="/"><span class="badge rounded-pill bg-info text-dark">ATENEO</span></a>
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<li class="nav-link active">
+								<a href="/students">studests</a>
+							</li>
+							<li class="nav-link active">
+								<a href="/students/create">add student</a>
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">tutors</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">add tutor</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">teachers</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">add teacher</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">employees</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">add employee</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">todo</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">chancellors</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">add chancellor</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">presidents</a>						
+							</li>
+							<li class="nav-link disabled">
+								<a href="#">add president</a>						
+							</li>
+						</ul>
+						<form class="d-flex">
+							<input class="form-control me-2" type="search" placeholder="search" aria-label="search">
+							<button class="btn btn-outline-success" type="submit">search</button>
+						</form>
+					</div>
+				</nav>
 				
 				<section class="container">
 					@yield('content')
