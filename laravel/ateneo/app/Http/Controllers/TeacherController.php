daa3f4c3806b5be690c009075d9b1ddaed100210
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-05 18:24:57 +0200

    Update TeacherController.php

--- laravel/ateneo/app/Http/Controllers/TeacherController.php
+++ laravel/ateneo/app/Http/Controllers/TeacherController.php
@@ -14,7 +14,8 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+        $teachers = Teacher::all();
+        return view('teachers.index')->with('teacher', $teachers);
     }
 
     /**
