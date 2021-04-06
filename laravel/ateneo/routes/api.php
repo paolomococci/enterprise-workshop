3f35b8d38ad0d8b739d940ccd4ee57b9fee65432
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-06 04:23:15 +0200

    Update api.php

--- laravel/ateneo/routes/api.php
+++ laravel/ateneo/routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ChancellorController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PresidentController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\TutorController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/students', [StudentController::class, 'restShowAll']);
+Route::get('/tutors', [TutorController::class, 'restShowAll']);
+Route::get('/teachers', [TeacherController::class, 'restShowAll']);
+Route::get('/employees', [EmployeeController::class, 'restShowAll']);
+Route::get('/chancellors', [ChancellorController::class, 'restShowAll']);
+Route::get('/presidents', [PresidentController::class, 'restShowAll']);
