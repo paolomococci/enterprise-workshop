021628dce37528771f6dd7587f645f74f1fd32ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-06 03:43:45 +0200

    Update ChancellorController.php

--- laravel/ateneo/app/Http/Controllers/ChancellorController.php
+++ laravel/ateneo/app/Http/Controllers/ChancellorController.php
@@ -96,6 +96,8 @@ class ChancellorController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $chancellor = Chancellor::findOrFail($id);
+        $chancellor->delete();
+        return redirect('/chancellors')->with('completed', 'chancellor has been deleted');
     }
 }
