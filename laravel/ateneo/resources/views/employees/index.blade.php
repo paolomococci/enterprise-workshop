590d3b9b2ec6cea75ca34c21bff44be8016b229e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-07 22:49:42 +0200

    Update index.blade.php

--- laravel/ateneo/resources/views/employees/index.blade.php
+++ laravel/ateneo/resources/views/employees/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="push-top">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br/>
+	@endif
 	<!-- TODO -->
+</div>
 @endsection
