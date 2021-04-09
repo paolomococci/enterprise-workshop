6a9f2eeffc84910921d128fff3fdf02a85e4eaac
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-09 22:35:40 +0200

    Update create.blade.php

--- laravel/ateneo/resources/views/teachers/create.blade.php
+++ laravel/ateneo/resources/views/teachers/create.blade.php
@@ -7,7 +7,52 @@
 	</div>
 
 	<div class="card-body">
-	   <!-- TODO -->
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('teachers.store') }}">
+			<div class="form-group">
+				@csrf
+				<label for="name">name</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="name" 
+					placeholder="John"/>
+			</div>
+			<div class="form-group">
+				<label for="surname">surname</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="surname" 
+					placeholder="Doe"/>
+			</div>
+			<div class="form-group">
+				<label for="email">email</label>
+				<input 
+					type="email" 
+					class="form-control" 
+					name="email" 
+					placeholder="john.doe@example.local"/>
+			</div>
+			<div class="form-group">
+				<label for="phone">phone</label>
+				<input 
+					type="tel" 
+					class="form-control" 
+					name="phone" 
+					pattern="^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$" 
+					placeholder="+12 1234567890"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-danger">create teacher</button>
+		</form>
 	</div>
 </div>
 @endsection
