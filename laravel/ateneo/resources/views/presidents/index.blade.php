c0f0d1be9e0e52b388ed09654fca676d6ac6643a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-09 23:01:23 +0200

    Update index.blade.php

--- laravel/ateneo/resources/views/presidents/index.blade.php
+++ laravel/ateneo/resources/views/presidents/index.blade.php
@@ -7,6 +7,36 @@
 		{{ session()->get('success') }}
 	</div><br/>
 	@endif
-	<!-- TODO -->
+	<table class="table">
+		<thead>
+			<tr class="table-warning">
+				<td>id</td>
+				<td>name</td>
+				<td>surname</td>
+				<td>email</td>
+				<td>phone</td>
+				<td class="text-center">action</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($president as $presidents)
+			<tr>
+				<td>{{$presidents->id}}</td>
+				<td>{{$presidents->name}}</td>
+				<td>{{$presidents->surname}}</td>
+				<td>{{$presidents->email}}</td>
+				<td>{{$presidents->phone}}</td>
+				<td class="text-center">
+					<a href="{{ route('presidents.edit', $presidents->id)}}" class="btn btn-primary btn-sm"">edit</a>
+					<form action="{{ route('presidents.destroy', $presidents->id)}}" method="post" style="display: inline-block">
+						@csrf
+						@method('DELETE')
+						<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+					</form>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
 </div>
 @endsection
