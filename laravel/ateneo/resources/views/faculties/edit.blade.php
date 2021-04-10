b2fa676648039332fa4f79b288138b4196a79691
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-10 18:29:44 +0200

    Update edit.blade.php

--- laravel/ateneo/resources/views/faculties/edit.blade.php
+++ laravel/ateneo/resources/views/faculties/edit.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.master')
 
 @section('content')
-	<!-- TODO -->
+<div class="card push-top">
+	<div class="card-header">
+		edit/update
+	</div>
+
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('faculties.update', $faculty->id) }}">
+			<div class="form-group">
+				@csrf
+				@method('PATCH')
+				<label for="name">name</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="name" 
+					value="{{ $faculty->name }}"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-danger">update faculty</button>
+		</form>
+	</div>
+</div>
 @endsection
