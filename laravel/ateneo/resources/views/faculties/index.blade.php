2a08b00c4be6a258d41d1f8042ce74cd88ce0c0f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-10 18:30:16 +0200

    Update index.blade.php

--- laravel/ateneo/resources/views/faculties/index.blade.php
+++ laravel/ateneo/resources/views/faculties/index.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.master')
 
 @section('content')
-	<!-- TODO -->
+<div class="push-top">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br/>
+	@endif
+	<table class="table">
+		<thead>
+			<tr class="table-warning">
+				<td>id</td>
+				<td>name</td>
+				<td class="text-center">action</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($faculty as $faculties)
+			<tr>
+				<td>{{$faculties->id}}</td>
+				<td>{{$faculties->name}}</td>
+				<td class="text-center">
+					<a href="{{ route('faculties.edit', $faculties->id)}}" class="btn btn-primary btn-sm"">edit</a>
+					<form action="{{ route('faculties.destroy', $faculties->id)}}" method="post" style="display: inline-block">
+						@csrf
+						@method('DELETE')
+						<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+					</form>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
 @endsection
