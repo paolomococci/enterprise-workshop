c0e9d062db64b0368473b7bc97ec85c85ddc9393
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-10 07:30:56 +0200

    Update 2021_04_10_045743_create_faculties_table.php

--- laravel/ateneo/database/migrations/2021_04_10_045743_create_faculties_table.php
+++ laravel/ateneo/database/migrations/2021_04_10_045743_create_faculties_table.php
@@ -15,6 +15,7 @@ class CreateFacultiesTable extends Migration
     {
         Schema::create('faculties', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
