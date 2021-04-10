546e0900487ee1b9f289de9dad58cddfc5b759b9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-10 18:29:24 +0200

    Update create.blade.php

--- laravel/ateneo/resources/views/faculties/create.blade.php
+++ laravel/ateneo/resources/views/faculties/create.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-	<!-- TODO -->
+<div class="card push-top">
+	<div class="card-header">
+		add faculty
+	</div>
+
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('faculties.store') }}">
+			<div class="form-group">
+				@csrf
+				<label for="name">name</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="name" 
+					placeholder="John"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-danger">create faculty</button>
+		</form>
+	</div>
+</div>
 @endsection
