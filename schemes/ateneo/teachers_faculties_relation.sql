0597f0b3d7a41396cfbe06b6f2878ebf7462e4fa
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-14 17:42:13 +0200

    Update teachers_faculties_relation.sql

--- schemes/ateneo/teachers_faculties_relation.sql
+++ schemes/ateneo/teachers_faculties_relation.sql
@@ -1,6 +1,12 @@
 CREATE TABLE ateneo_db.teachers_faculties_relation (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `teacher_id` bigint(20) unsigned NOT NULL,
+  `faculty_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `teachers_faculties_relation_teacher_id_index` (`teacher_id`),
+  KEY `teachers_faculties_relation_faculty_id_index` (`faculty_id`),
+  CONSTRAINT `teachers_faculties_relation_faculty_id_foreign` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`id`),
+  CONSTRAINT `teachers_faculties_relation_teacher_id_foreign` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
