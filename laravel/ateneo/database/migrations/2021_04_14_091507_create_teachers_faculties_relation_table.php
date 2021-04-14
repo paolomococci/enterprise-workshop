8989a5319f4d196f275039476aa3152aad8d2b51
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-14 17:40:23 +0200

    Update 2021_04_14_091507_create_teachers_faculties_relation_table.php

--- laravel/ateneo/database/migrations/2021_04_14_091507_create_teachers_faculties_relation_table.php
+++ laravel/ateneo/database/migrations/2021_04_14_091507_create_teachers_faculties_relation_table.php
@@ -14,8 +14,13 @@ class CreateTeachersFacultiesRelationTable extends Migration
     public function up()
     {
         Schema::create('teachers_faculties_relation', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('teacher_id')->unsigned()->index();
+            $table->bigInteger('faculty_id')->unsigned()->index();
             $table->timestamps();
+            
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->foreign('faculty_id')->references('id')->on('faculties');
         });
     }
 
