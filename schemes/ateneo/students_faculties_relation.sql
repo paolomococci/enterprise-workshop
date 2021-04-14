86276cd6c4bf59e132cb252832661184acf7ff53
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-14 17:41:28 +0200

    Update students_faculties_relation.sql

--- schemes/ateneo/students_faculties_relation.sql
+++ schemes/ateneo/students_faculties_relation.sql
@@ -1,6 +1,12 @@
 CREATE TABLE ateneo_db.students_faculties_relation (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` bigint(20) unsigned NOT NULL,
+  `faculty_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `students_faculties_relation_student_id_index` (`student_id`),
+  KEY `students_faculties_relation_faculty_id_index` (`faculty_id`),
+  CONSTRAINT `students_faculties_relation_faculty_id_foreign` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`id`),
+  CONSTRAINT `students_faculties_relation_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
