949d0a657aa444b33017217782ad3ebbf0b632cf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-16 18:47:19 +0200

    Update Chancellor.php

--- laravel/ateneo/app/Models/Chancellor.php
+++ laravel/ateneo/app/Models/Chancellor.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Chancellor extends Model
 {
     protected $guarded = ['id'];
+    
+    /**
+     * get the faculty record associated with the chancellor
+     * 
+     */
+    public function faculty() {
+        return $this->hasOne(Faculty::class);
+    }
 }
