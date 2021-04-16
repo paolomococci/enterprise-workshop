d9acacb740a81921b979ddbf03abc7a0cf2b5dd9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-16 18:48:33 +0200

    Update Faculty.php

--- laravel/ateneo/app/Models/Faculty.php
+++ laravel/ateneo/app/Models/Faculty.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Faculty extends Model
 {
     protected $guarded = ['id'];
+    
+    /**
+     * get the chancellor inverse relationship associated with the faculty
+     *
+     */
+    public function faculty() {
+        return $this->belongsTo(Chancellor::class);
+    }
 }
