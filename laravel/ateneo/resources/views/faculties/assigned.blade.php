6126b9de8cb83c87028951f8595d050aa8b630a2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 03:14:57 +0200

    Create assigned.blade.php

---
+++ laravel/ateneo/resources/views/faculties/assigned.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="push-top">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br/>
+	@endif
+	<h3>list of active faculties</h3>
+	<table class="table" summary="list of active faculties">
+		<thead>
+			<tr class="table-warning">
+				<td>id</td>
+				<td>name</td>
+				<td class="text-center">action</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($faculty as $faculties)
+			<tr>
+				<td>{{$faculties->id}}</td>
+				<td>{{$faculties->name}}</td>
+				<td class="text-center">
+					<a href="{{ route('faculties.edit', $faculties->id)}}" class="btn btn-primary btn-sm"">edit</a>
+					<form action="{{ route('faculties.destroy', $faculties->id)}}" method="post" style="display: inline-block">
+						@csrf
+						@method('DELETE')
+						<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+					</form>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+@endsection
