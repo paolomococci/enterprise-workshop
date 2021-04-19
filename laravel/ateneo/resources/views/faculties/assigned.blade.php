f8609d60ddb2e3e48f0a8a4bc01069831f54e5df
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 09:02:59 +0200

    Update assigned.blade.php

--- laravel/ateneo/resources/views/faculties/assigned.blade.php
+++ laravel/ateneo/resources/views/faculties/assigned.blade.php
@@ -7,31 +7,74 @@
 		{{ session()->get('success') }}
 	</div><br/>
 	@endif
-	<h3>list of faculties which have a chancellor</h3>
-	<table class="table" summary="list of active faculties">
-		<thead>
-			<tr class="table-warning">
-				<td>id</td>
-				<td>name</td>
-				<td class="text-center">action</td>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($faculty as $faculties)
-			<tr>
-				<td>{{$faculties->id}}</td>
-				<td>{{$faculties->name}}</td>
-				<td class="text-center">
-					<a href="{{ route('faculties.edit', $faculties->id)}}" class="btn btn-primary btn-sm"">edit</a>
-					<form action="{{ route('faculties.destroy', $faculties->id)}}" method="post" style="display: inline-block">
-						@csrf
-						@method('DELETE')
-						<button class="btn btn-danger btn-sm"" type="submit">delete</button>
-					</form>
-				</td>
-			</tr>
+	<h3><nobr>list of faculties which have a chancellor</nobr></h3>
+	<section>
+		<h5>list of faculties</h5>
+		<table class="table" summary="list of faculties">
+			<thead>
+				<tr class="table-warning">
+					<td>id</td>
+					<td>name</td>
+					<td>chancellor id</td>
+					<td class="text-center">action</td>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($faculty as $faculties)
+				<tr>
+					<td>{{$faculties->id}}</td>
+					<td>{{$faculties->name}}</td>
+					<td>{{$faculties->chancellor_id}}</td>
+					<td class="text-center">
+						<nobr>
+							<a href="{{ route('faculties.edit', $faculties->id)}}" class="btn btn-primary btn-sm"">edit</a>
+							<form action="{{ route('faculties.destroy', $faculties->id)}}" method="post" style="display: inline-block">
+								@csrf
+								@method('DELETE')
+								<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+							</form>
+						</nobr>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</section>
+	<section>
+		<h5>list of chancellors</h5>
+		<table class="table" summary="list of chancellors">
+			<thead>
+				<tr class="table-warning">
+					<td>id</td>
+					<td>name</td>
+					<td>surname</td>
+					<td>email</td>
+					<td>phone</td>
+					<td class="text-center">action</td>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($chancellor as $chancellors)
+				<tr>
+					<td>{{$chancellors->id}}</td>
+					<td>{{$chancellors->name}}</td>
+					<td>{{$chancellors->surname}}</td>
+					<td>{{$chancellors->email}}</td>
+					<td>{{$chancellors->phone}}</td>
+					<td class="text-center">
+						<nobr>
+							<a href="{{ route('chancellors.edit', $chancellors->id)}}" class="btn btn-primary btn-sm"">edit</a>
+							<form action="{{ route('chancellors.destroy', $chancellors->id)}}" method="post" style="display: inline-block">
+								@csrf
+								@method('DELETE')
+								<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+							</form>
+						</nobr>
+					</td>
+				</tr>
 			@endforeach
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	</section>
 </div>
 @endsection
