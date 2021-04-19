5ac582957c857da426adfd53613f6d773451904e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 03:00:25 +0200

    Update faculties.sql

--- schemes/ateneo/faculties.sql
+++ schemes/ateneo/faculties.sql
@@ -1,7 +1,10 @@
 CREATE TABLE ateneo_db.faculties (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `chancellor_id` bigint(20) unsigned DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `faculties_chancellor_id_index` (`chancellor_id`),
+  CONSTRAINT `faculties_chancellor_id_foreign` FOREIGN KEY (`chancellor_id`) REFERENCES `chancellors` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
