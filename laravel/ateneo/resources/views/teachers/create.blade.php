743271b98c4086b3b231e4db3ea3ccc7223c4aaf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 09:40:16 +0200

    Update create.blade.php

--- laravel/ateneo/resources/views/teachers/create.blade.php
+++ laravel/ateneo/resources/views/teachers/create.blade.php
@@ -52,6 +52,7 @@
 					placeholder="+12 1234567890"/>
 			</div>
 			<button type="submit" class="btn btn-block btn-danger">create teacher</button>
+			<button type="button" onclick="window.location='/teachers'" class="btn btn-block">cancel</button>
 		</form>
 	</div>
 </div>
