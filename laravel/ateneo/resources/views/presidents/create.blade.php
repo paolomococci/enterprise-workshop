af54ee72f9d4b036c06b503d1ea2462777d6d37f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 09:41:24 +0200

    Update create.blade.php

--- laravel/ateneo/resources/views/presidents/create.blade.php
+++ laravel/ateneo/resources/views/presidents/create.blade.php
@@ -52,6 +52,7 @@
 					placeholder="+12 1234567890"/>
 			</div>
 			<button type="submit" class="btn btn-block btn-danger">create president</button>
+			<button type="button" onclick="window.location='/presidents'" class="btn btn-block">cancel</button>
 		</form>
 	</div>
 </div>
