068f454a03fc30a11783b9c4ca340afbecdf5774
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 09:21:39 +0200

    Update index.blade.php

--- laravel/ateneo/resources/views/tutors/index.blade.php
+++ laravel/ateneo/resources/views/tutors/index.blade.php
@@ -28,12 +28,14 @@
 				<td>{{$tutors->email}}</td>
 				<td>{{$tutors->phone}}</td>
 				<td class="text-center">
-					<a href="{{ route('tutors.edit', $tutors->id)}}" class="btn btn-primary btn-sm"">edit</a>
-					<form action="{{ route('tutors.destroy', $tutors->id)}}" method="post" style="display: inline-block">
-						@csrf
-						@method('DELETE')
-						<button class="btn btn-danger btn-sm"" type="submit">delete</button>
-					</form>
+					<nobr>
+						<a href="{{ route('tutors.edit', $tutors->id)}}" class="btn btn-primary btn-sm"">edit</a>
+						<form action="{{ route('tutors.destroy', $tutors->id)}}" method="post" style="display: inline-block">
+							@csrf
+							@method('DELETE')
+							<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+						</form>
+					</nobr>
 				</td>
 			</tr>
 			@endforeach
