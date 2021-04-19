2618f46537214b46832cdf9305d9c37f7e94b00a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-19 09:42:35 +0200

    Update create.blade.php

--- laravel/ateneo/resources/views/employees/create.blade.php
+++ laravel/ateneo/resources/views/employees/create.blade.php
@@ -52,6 +52,7 @@
 					placeholder="+12 1234567890"/>
 			</div>
 			<button type="submit" class="btn btn-block btn-danger">create employee</button>
+			<button type="button" onclick="window.location='/employees'" class="btn btn-block">cancel</button>
 		</form>
 	</div>
 </div>
