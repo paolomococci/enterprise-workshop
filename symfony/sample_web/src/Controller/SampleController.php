a60c3b9e76fe648be8d54d99d4f98c955b955783
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-26 04:48:05 +0200

    Create SampleController.php

---
+++ symfony/sample_web/src/Controller/SampleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SampleController extends AbstractController
+{
+    /**
+     * @Route("/sample", name="sample", methods={"GET","HEAD"})
+     */
+    public function index(): Response
+    {
+        return $this->render(
+            'sample/index.html.twig', 
+            ['controller_name' => 'SampleController',]
+        );
+    }
+}
