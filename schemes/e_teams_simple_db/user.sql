e4a1f16e68f9fb174772e3eaba2d4c3381b68247
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-01 08:59:19 +0200

    Create user.sql

---
+++ schemes/e_teams_simple_db/user.sql
@@ -0,0 +1,10 @@
+CREATE TABLE e_teams_simple_db.user (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profession` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
