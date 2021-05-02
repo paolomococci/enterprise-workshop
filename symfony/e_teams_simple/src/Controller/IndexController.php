3f60002f0cecc445a054a8591fce62ebbd19cff6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-02 04:38:26 +0200

    Update IndexController.php

--- symfony/e_teams_simple/src/Controller/IndexController.php
+++ symfony/e_teams_simple/src/Controller/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/index", name="index", methods={"GET","HEAD"})
      */
     public function index(): Response
     {
