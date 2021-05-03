34b919eb1c01c02d875af763fccd24a31a13d7e8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-03 11:33:53 +0200

    Update DashboardController.php

--- symfony/e_teams_simple/src/Controller/Admin/DashboardController.php
+++ symfony/e_teams_simple/src/Controller/Admin/DashboardController.php
@@ -5,8 +5,15 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+use App\Entity\Category;
+use App\Entity\Post;
+
+use App\Controller\Admin\UserCrudController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -15,18 +22,25 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(UserCrudController::class)->generateUrl();
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('E Teams Simple');
+            ->setTitle('E-Teams Simple');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // yield MenuItem::linktoRoute('Back to the home', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-hashtag', Category::class);
+        yield MenuItem::linkToCrud('Post', 'fas fa-comment', Post::class);
     }
 }
