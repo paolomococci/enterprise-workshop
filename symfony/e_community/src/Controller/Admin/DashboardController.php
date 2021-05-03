0981d3e352ccbf97501b94c82c25fec6e6b025ad
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-03 17:57:00 +0200

    Create DashboardController.php

---
+++ symfony/e_community/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+
+use App\Controller\Admin\UserCrudController;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(UserCrudController::class)->generateUrl();
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('E-Community');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('admin', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
+    }
+}
