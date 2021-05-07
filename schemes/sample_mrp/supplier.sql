4be4a0be6b8e9c6b52ca06d200363ab6253a9f76
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-07 18:26:46 +0200

    Create supplier.sql

---
+++ schemes/sample_mrp/supplier.sql
@@ -0,0 +1,5 @@
+CREATE TABLE sample_mrp_db.supplier (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
