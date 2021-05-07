9b7934511a5374986fe1ff56ec975f89862352bf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-07 18:29:18 +0200

    Create component.sql

---
+++ schemes/sample_mrp/component.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sample_mrp_db.component (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commodity_id` int(11) DEFAULT NULL,
+  `code` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantity` decimal(10,5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_49FEA157B4ACC212` (`commodity_id`),
+  CONSTRAINT `FK_49FEA157B4ACC212` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
