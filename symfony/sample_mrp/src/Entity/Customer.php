0c6b2501868bb221e54ec357f35e5d29716462a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 22:42:15 +0200

    Update Customer.php

--- symfony/sample_mrp/src/Entity/Customer.php
+++ symfony/sample_mrp/src/Entity/Customer.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CustomerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,26 @@ class Customer
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Commodity::class, mappedBy="customer")
+     */
+    private $goods;
+
+    public function __construct()
+    {
+        $this->goods = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +57,60 @@ class Customer
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Commodity[]
+     */
+    public function getGoods(): Collection
+    {
+        return $this->goods;
+    }
+
+    public function addGood(Commodity $good): self
+    {
+        if (!$this->goods->contains($good)) {
+            $this->goods[] = $good;
+            $good->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGood(Commodity $good): self
+    {
+        if ($this->goods->removeElement($good)) {
+            // set the owning side to null (unless already changed)
+            if ($good->getCustomer() === $this) {
+                $good->setCustomer(null);
+            }
+        }
 
         return $this;
     }
