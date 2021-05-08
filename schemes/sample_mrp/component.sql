658102fb22771a2e67e1e1143af1261ce650f247
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 23:06:20 +0200

    Update component.sql

--- schemes/sample_mrp/component.sql
+++ schemes/sample_mrp/component.sql
@@ -4,7 +4,10 @@ CREATE TABLE sample_mrp_db.component (
   `code` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` decimal(10,5) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_49FEA157B4ACC212` (`commodity_id`),
+  KEY `IDX_49FEA1572ADD6D8C` (`supplier_id`),
+  CONSTRAINT `FK_49FEA1572ADD6D8C` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
   CONSTRAINT `FK_49FEA157B4ACC212` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
