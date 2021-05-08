b65ebdaf1667a155c3bd84a7566422ab87c8574f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 22:11:47 +0200

    Update DashboardController.php

--- symfony/sample_mrp/src/Controller/Admin/DashboardController.php
+++ symfony/sample_mrp/src/Controller/Admin/DashboardController.php
@@ -5,8 +5,15 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Commodity;
+use App\Entity\Component;
+
+use App\Controller\Admin\CommodityCrudController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -15,7 +22,9 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(CommodityCrudController::class)->generateUrl();
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -26,7 +35,8 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoDashboard('Admin', 'fa fa-home');
+        yield MenuItem::linkToCrud('commodities', 'fa-caret-square-down', Commodity::class);
+        yield MenuItem::linkToCrud('components', 'fas fa-caret-square-up', Component::class);
     }
 }
