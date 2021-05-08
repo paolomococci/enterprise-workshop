488534c2cafe0d8e6d7626133d3443d3bb32399a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 21:27:40 +0200

    Create Component.php

---
+++ symfony/sample_mrp/src/Entity/Component.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComponentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ComponentRepository::class)
+ */
+class Component
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=5, nullable=true)
+     */
+    private $quantity;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Commodity::class, inversedBy="items")
+     */
+    private $commodity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?string $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getCommodity(): ?Commodity
+    {
+        return $this->commodity;
+    }
+
+    public function setCommodity(?Commodity $commodity): self
+    {
+        $this->commodity = $commodity;
+
+        return $this;
+    }
+}
