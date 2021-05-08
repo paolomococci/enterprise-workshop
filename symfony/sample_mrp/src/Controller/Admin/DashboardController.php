2f65ad91116859a3706904a83fabb8f6b77370b9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 23:14:18 +0200

    Update DashboardController.php

--- symfony/sample_mrp/src/Controller/Admin/DashboardController.php
+++ symfony/sample_mrp/src/Controller/Admin/DashboardController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Commodity;
 use App\Entity\Component;
+use App\Entity\Customer;
+use App\Entity\Supplier;
 
 use App\Controller\Admin\CommodityCrudController;
 
@@ -36,7 +38,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Admin', 'fa fa-home');
-        yield MenuItem::linkToCrud('commodities', 'fa-caret-square-down', Commodity::class);
+        yield MenuItem::linkToCrud('commodities', 'fas fa-caret-square-down', Commodity::class);
         yield MenuItem::linkToCrud('components', 'fas fa-caret-square-up', Component::class);
+        yield MenuItem::linkToCrud('commodities', 'fas fa-address-book', Customer::class);
+        yield MenuItem::linkToCrud('components', 'fas fa-address-book', Supplier::class);
     }
 }
