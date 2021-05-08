23436a0d662dab7da7d1682dc086a92491c244fc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 23:05:20 +0200

    Update commodity.sql

--- schemes/sample_mrp/commodity.sql
+++ schemes/sample_mrp/commodity.sql
@@ -3,5 +3,8 @@ CREATE TABLE sample_mrp_db.commodity (
   `code` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `customer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5E8D2F749395C3F3` (`customer_id`),
+  CONSTRAINT `FK_5E8D2F749395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
