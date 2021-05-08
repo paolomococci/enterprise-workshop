dcafbc4436879ca371361934ea965c5e3da91e79
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 22:42:46 +0200

    Update Supplier.php

--- symfony/sample_mrp/src/Entity/Supplier.php
+++ symfony/sample_mrp/src/Entity/Supplier.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SupplierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,26 @@ class Supplier
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Component::class, mappedBy="supplier")
+     */
+    private $elements;
+
+    public function __construct()
+    {
+        $this->elements = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +57,60 @@ class Supplier
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Component[]
+     */
+    public function getElements(): Collection
+    {
+        return $this->elements;
+    }
+
+    public function addElement(Component $element): self
+    {
+        if (!$this->elements->contains($element)) {
+            $this->elements[] = $element;
+            $element->setSupplier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElement(Component $element): self
+    {
+        if ($this->elements->removeElement($element)) {
+            // set the owning side to null (unless already changed)
+            if ($element->getSupplier() === $this) {
+                $element->setSupplier(null);
+            }
+        }
 
         return $this;
     }
