86a2a990922aa8aab7f117d39a3510567bbf7040
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-08 23:06:51 +0200

    Update customer.sql

--- schemes/sample_mrp/customer.sql
+++ schemes/sample_mrp/customer.sql
@@ -1,5 +1,7 @@
 CREATE TABLE sample_mrp_db.customer (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
