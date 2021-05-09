d0f622d2d49544aee4177d9101bbd5e8af7e4871
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-09 08:25:31 +0200

    Update HomeController.php

--- symfony/sample_mrp/src/Controller/HomeController.php
+++ symfony/sample_mrp/src/Controller/HomeController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home", methods={"GET","HEAD"})
+     * @Route("/", name="home", methods={"GET","HEAD"})
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
