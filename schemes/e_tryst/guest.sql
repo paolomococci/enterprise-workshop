74fd8a75154831529b0f0deb365a90651c826624
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-10 09:37:16 +0200

    Update guest.sql

--- schemes/e_tryst/guest.sql
+++ schemes/e_tryst/guest.sql
@@ -4,5 +4,8 @@ CREATE TABLE e_tryst_db.guest (
   `surname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `birthday` date NOT NULL,
-  PRIMARY KEY (`id`)
+  `tryst_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_ACB79A35D9CD6CFE` (`tryst_id`),
+  CONSTRAINT `FK_ACB79A35D9CD6CFE` FOREIGN KEY (`tryst_id`) REFERENCES `tryst` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
