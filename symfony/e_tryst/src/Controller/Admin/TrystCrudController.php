5f4f281b29da13917eff85f7d981220c699c9da4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-12 10:09:46 +0200

    Update TrystCrudController.php

--- symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
+++ symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
@@ -6,8 +6,12 @@ use App\Entity\Tryst;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TrystCrudController extends AbstractCrudController
 {
@@ -29,6 +33,27 @@ class TrystCrudController extends AbstractCrudController
     {
         return $filters->add(EntityFilter::new('title'));
     }
-
-    // TODO
+    
+    public function configureFields(string $pageName): iterable 
+    {
+        yield TextField::new('name');
+        yield TextField::new('title');
+        yield TextareaField::new('topic')->hideOnIndex();
+        $day = DateField::new('day')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+        $hour = TimeField::new('hour')->setFormTypeOptions([
+            'html5' => true,
+            'widget' => 'single_text',
+        ]);
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $day->setFormTypeOption('disabled', true);
+            yield $hour->setFormTypeOption('disabled', true);
+        } else {
+            yield $day;
+            yield $hour;
+        }
+    }
 }
