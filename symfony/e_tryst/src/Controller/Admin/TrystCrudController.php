8d897031d8abb9c539db33b9ce07af5b00904131
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-12 09:50:44 +0200

    Update TrystCrudController.php

--- symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
+++ symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Tryst;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TrystCrudController extends AbstractCrudController
@@ -21,6 +23,11 @@ class TrystCrudController extends AbstractCrudController
                 ->setEntityLabelInPlural('some Guest Trysts')
                 ->setSearchFields(['name', 'title','topic','day','hour'])
                 ->setDefaultSort(['day' => 'DESC']);
+    }
+    
+    public function configureFilters(Filters $filters): Filters 
+    {
+        return $filters->add(EntityFilter::new('title'));
     }
 
     // TODO
