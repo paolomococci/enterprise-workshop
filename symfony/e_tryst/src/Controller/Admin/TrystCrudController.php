436790b179c3b4eaf2a9b3cbfa380fcd67192cc2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-12 09:47:11 +0200

    Update TrystCrudController.php

--- symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
+++ symfony/e_tryst/src/Controller/Admin/TrystCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Tryst;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TrystCrudController extends AbstractCrudController
@@ -11,6 +12,15 @@ class TrystCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Tryst::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud 
+    {
+        return $crud
+                ->setEntityLabelInSingular('a Guest Tryst')
+                ->setEntityLabelInPlural('some Guest Trysts')
+                ->setSearchFields(['name', 'title','topic','day','hour'])
+                ->setDefaultSort(['day' => 'DESC']);
     }
 
     // TODO
