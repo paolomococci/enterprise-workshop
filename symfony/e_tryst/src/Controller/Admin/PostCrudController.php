e19b15e603c9227c366692b9e2eb9a018576fb71
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-12 09:28:35 +0200

    Update PostCrudController.php

--- symfony/e_tryst/src/Controller/Admin/PostCrudController.php
+++ symfony/e_tryst/src/Controller/Admin/PostCrudController.php
@@ -3,7 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PostCrudController extends AbstractCrudController
 {
@@ -12,5 +20,34 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    // TODO
+    public function configureCrud(Crud $crud): Crud 
+    {
+        return $crud
+                ->setEntityLabelInSingular('Tryst Post')
+                ->setEntityLabelInPlural('Tryst Posts')
+                ->setSearchFields(['title', 'merit'])
+                ->setDefaultSort(['at' => 'DESC']);
+    }
+    
+    public function configureFilters(Filters $filters): Filters 
+    {
+        return $filters->add(EntityFilter::new('guest'));
+    }
+    
+    public function configureFields(string $pageName): iterable 
+    {
+        yield AssociationField::new('guest');
+        yield TextField::new('title');
+        yield TextareaField::new('merit')->hideOnIndex();
+        $at = DateTimeField::new('at')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $at->setFormTypeOption('disabled', true);
+        } else {
+            yield $at;
+        }
+    }
 }
