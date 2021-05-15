de4f397100eda79c9147249681a11ae91decf94e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-15 07:15:59 +0200

    Create CustomerController.php

---
+++ lumen/sample/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller {
+
+    public function index() {
+        // TODO
+    }
+    
+    public function getCustomer($id) {
+        // TODO
+    }
+    
+    public function createCustomer(Request $request) {
+        // TODO
+    }
+    
+    public function updateCustomer(Request $request, $id) {
+        // TODO
+    }
+    
+    public function deleteCustomer($id) {
+        // TODO
+    }
+}
