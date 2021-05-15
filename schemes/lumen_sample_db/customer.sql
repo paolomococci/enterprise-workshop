b526e9a141e6c0ab53009659e15147d9e28f9a25
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-15 06:46:54 +0200

    Create customer.sql

---
+++ schemes/lumen_sample_db/customer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE lumen_sample_db.customer (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
