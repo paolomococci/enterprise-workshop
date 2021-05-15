a1d509a729d89bebd26304967f9c2d7d4639817c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-15 07:16:36 +0200

    Create SupplierController.php

---
+++ lumen/sample/app/Http/Controllers/SupplierController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller {
+
+    public function index() {
+        // TODO
+    }
+    
+    public function getSupplier($id) {
+        // TODO
+    }
+    
+    public function createSupplier(Request $request) {
+        // TODO
+    }
+    
+    public function updateSupplier(Request $request, $id) {
+        // TODO
+    }
+    
+    public function deleteSupplier($id) {
+        // TODO
+    }
+}
