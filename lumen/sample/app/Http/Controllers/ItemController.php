71e4b68eb18058971b61efa721208137d67e9cc8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-15 07:15:22 +0200

    Create ItemController.php

---
+++ lumen/sample/app/Http/Controllers/ItemController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemController extends Controller {
+
+    public function index() {
+        // TODO
+    }
+    
+    public function getItem($id) {
+        // TODO
+    }
+    
+    public function createItem(Request $request) {
+        // TODO
+    }
+    
+    public function updateItem(Request $request, $id) {
+        // TODO
+    }
+    
+    public function deleteItem($id) {
+        // TODO
+    }
+}
