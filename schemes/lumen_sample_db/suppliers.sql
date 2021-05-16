471d3d699509ed3b9111b7e55221b15aae851ac0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-16 09:50:32 +0200

    Update suppliers.sql

--- schemes/lumen_sample_db/suppliers.sql
+++ schemes/lumen_sample_db/suppliers.sql
@@ -1,5 +1,8 @@
-CREATE TABLE lumen_sample_db.supplier (
+CREATE TABLE lumen_sample_db.suppliers (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
