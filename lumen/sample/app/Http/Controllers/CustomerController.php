776b5a3cb868a41575c40a0643d4aa617102b97d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-16 09:43:46 +0200

    Update CustomerController.php

--- lumen/sample/app/Http/Controllers/CustomerController.php
+++ lumen/sample/app/Http/Controllers/CustomerController.php
@@ -4,25 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use \App\Models\Customer;
+
 class CustomerController extends Controller {
 
-    public function index() {
-        // TODO
+    public function readAllCustomers() {
+        $customers = Customer::all();
+        return response()->json($customers, '200');
     }
     
-    public function getCustomer($id) {
-        // TODO
+    public function readCustomer($id) {
+        $customer = Customer::find($id);
+        return response()->json($customer, '200');
     }
     
     public function createCustomer(Request $request) {
-        // TODO
+        $customer = Customer::create($request->all());
+        return response()->json($customer, '201');
     }
     
     public function updateCustomer(Request $request, $id) {
-        // TODO
+        $customer = Customer::find($id);
+        $customer->code = $request->input('code');
+        $customer->name = $request->input('name');
+        $customer->description = $request->input('description');
+        $customer->save();
+        return response()->json($customer, '205');
     }
     
     public function deleteCustomer($id) {
-        // TODO
+        $customer = Customer::find($id);
+        yield response()->json($customer, '200');
+        $customer->delete();
+        return response()->json('No Content', '204');
     }
 }
