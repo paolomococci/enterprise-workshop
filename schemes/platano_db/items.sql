7ade647902b4264024b5a53c665bcca029ce5dc3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:34:01 +0200

    Update items.sql

--- schemes/platano_db/items.sql
+++ schemes/platano_db/items.sql
@@ -1,6 +1,16 @@
 CREATE TABLE platano_db.items (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(8,2) unsigned NOT NULL,
+  `stock` int(10) unsigned NOT NULL,
+  `subcategory_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `items_code_unique` (`code`),
+  KEY `items_subcategory_id_foreign` (`subcategory_id`),
+  CONSTRAINT `items_subcategory_id_foreign` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
