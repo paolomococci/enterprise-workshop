a3bf88cbf9012759b2dd5a1251309ae636506637
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 22:56:19 +0200

    Update 2021_05_19_204325_create_item_order_table.php

--- laravel/platano/database/migrations/2021_05_19_204325_create_item_order_table.php
+++ laravel/platano/database/migrations/2021_05_19_204325_create_item_order_table.php
@@ -15,6 +15,10 @@ class CreateItemOrderTable extends Migration
     {
         Schema::create('item_order', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->timestamps();
         });
     }
