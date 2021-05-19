ed8fa82ac3bedd4a834e2f851c446aaad12f9013
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:36:24 +0200

    Update transactions.sql

--- schemes/platano_db/transactions.sql
+++ schemes/platano_db/transactions.sql
@@ -1,6 +1,13 @@
 CREATE TABLE platano_db.transactions (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payment` decimal(8,2) NOT NULL,
+  `order_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `transactions_code_unique` (`code`),
+  KEY `transactions_order_id_foreign` (`order_id`),
+  CONSTRAINT `transactions_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
