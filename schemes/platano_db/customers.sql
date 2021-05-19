a273b7d11bb1fc550a10164cd8ed0a9c77463e70
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:31:25 +0200

    Update customers.sql

--- schemes/platano_db/customers.sql
+++ schemes/platano_db/customers.sql
@@ -1,6 +1,9 @@
 CREATE TABLE platano_db.customers (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `customers_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
