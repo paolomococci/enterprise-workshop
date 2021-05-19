0ead1887da09bbd0559b682508cc982f89bbb768
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:35:53 +0200

    Update subcategories.sql

--- schemes/platano_db/subcategories.sql
+++ schemes/platano_db/subcategories.sql
@@ -1,6 +1,11 @@
 CREATE TABLE platano_db.subcategories (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `subcategories_name_unique` (`name`),
+  KEY `subcategories_category_id_foreign` (`category_id`),
+  CONSTRAINT `subcategories_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
