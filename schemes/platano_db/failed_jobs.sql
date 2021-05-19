093a49e439572555a30e6f8aeb0828a0808eada4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 22:07:11 +0200

    Create failed_jobs.sql

---
+++ schemes/platano_db/failed_jobs.sql
@@ -0,0 +1,9 @@
+CREATE TABLE platano_db.failed_jobs (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
