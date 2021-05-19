594fcef0f3971abd3126547dcfab955c1a5e6727
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 22:59:34 +0200

    Update 2021_05_18_020808_create_customers_table.php

--- laravel/platano/database/migrations/2021_05_18_020808_create_customers_table.php
+++ laravel/platano/database/migrations/2021_05_18_020808_create_customers_table.php
@@ -15,6 +15,8 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
