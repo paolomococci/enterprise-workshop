f14d23d22ea6ff5ad7733b7ac2b56194bc2201e1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:35:00 +0200

    Update orders.sql

--- schemes/platano_db/orders.sql
+++ schemes/platano_db/orders.sql
@@ -1,6 +1,13 @@
 CREATE TABLE platano_db.orders (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` int(10) unsigned NOT NULL,
+  `tax` int(10) unsigned NOT NULL,
+  `shipping` int(10) unsigned NOT NULL,
+  `customer_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `orders_customer_id_foreign` (`customer_id`),
+  CONSTRAINT `orders_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
