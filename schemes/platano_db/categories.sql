a340b9f7a8399ec69fa926533f7dfd321cca152b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:30:54 +0200

    Update categories.sql

--- schemes/platano_db/categories.sql
+++ schemes/platano_db/categories.sql
@@ -1,6 +1,9 @@
 CREATE TABLE platano_db.categories (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descrition` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `categories_name_unique` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
