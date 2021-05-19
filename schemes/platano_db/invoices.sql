9588542b91669d58181f3bcb5175809dd938ecb2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 23:32:24 +0200

    Update invoices.sql

--- schemes/platano_db/invoices.sql
+++ schemes/platano_db/invoices.sql
@@ -1,6 +1,11 @@
 CREATE TABLE platano_db.invoices (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` decimal(8,2) NOT NULL,
+  `order_id` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `invoices_order_id_unique` (`order_id`),
+  CONSTRAINT `invoices_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
