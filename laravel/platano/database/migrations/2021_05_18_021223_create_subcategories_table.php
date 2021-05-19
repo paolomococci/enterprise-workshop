dea43898711d3a0655dc66d717bd16a054b73cab
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-19 22:58:36 +0200

    Update 2021_05_18_021223_create_subcategories_table.php

--- laravel/platano/database/migrations/2021_05_18_021223_create_subcategories_table.php
+++ laravel/platano/database/migrations/2021_05_18_021223_create_subcategories_table.php
@@ -15,6 +15,9 @@ class CreateSubcategoriesTable extends Migration
     {
         Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
