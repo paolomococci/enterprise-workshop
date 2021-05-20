33d6ed0f9eda67b699e017d07e8814892b2b8687
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 21:49:12 +0200

    Update ItemSeeder.php

--- laravel/platano/database/seeds/ItemSeeder.php
+++ laravel/platano/database/seeds/ItemSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ItemSeeder extends Seeder
 {
@@ -11,6 +14,13 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('customers')->insert([
+            'code' => Hash::make(Str::random(12)),
+            'name' => Str::random(20),
+            'descrition' => Str::random(40),
+            'type' => Str::random(8),
+            'price' => 0.0,
+            'stock' => 0,
+        ]);
     }
 }
