8305da30b3b376092096460a06289bd92bd92afa
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 21:47:56 +0200

    Update CustomerSeeder.php

--- laravel/platano/database/seeds/CustomerSeeder.php
+++ laravel/platano/database/seeds/CustomerSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CustomerSeeder extends Seeder
 {
@@ -11,6 +13,9 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('customers')->insert([
+            'name' => Str::random(20),
+            'email' => Str::random(20).'@example.local',
+        ]);
     }
 }
