b2b6e943b057fb8059e82d3460abb0c29541c45c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 20:42:01 +0200

    Update Invoice.php

--- laravel/platano/app/Models/Invoice.php
+++ laravel/platano/app/Models/Invoice.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     protected $guarded = ['id'];
+    
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
 }
