7161b4865438d9821efdd3353baf50ed27d5a6a3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 22:01:48 +0200

    Create SubcategoryFactory.php

---
+++ laravel/platano/database/factories/SubcategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Subcategory;
+use Faker\Generator as Faker;
+
+$factory->define(Subcategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
