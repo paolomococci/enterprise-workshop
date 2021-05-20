62ac8a4bc5f41ebb1b35bbde44556a589cd6f17e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 20:41:08 +0200

    Update Order.php

--- laravel/platano/app/Models/Order.php
+++ laravel/platano/app/Models/Order.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded = ['id'];
+    
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
+    
+    public function invoice() {
+        return $this->hasOne(Invoice::class);
+    }
+    
+    public function items() {
+        return $this->belongsToMany(Item::class);
+    }
 }
