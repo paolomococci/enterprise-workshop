1e5e6d9ef167fb085c21c817e6ef211eb617de9f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-20 21:49:36 +0200

    Update OrderSeeder.php

--- laravel/platano/database/seeds/OrderSeeder.php
+++ laravel/platano/database/seeds/OrderSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OrderSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('customers')->insert([
+            'status' => 'sample',
+            'total' => 0,
+            'tax' => 0,
+            'shipping' => 0,
+        ]);
     }
 }
