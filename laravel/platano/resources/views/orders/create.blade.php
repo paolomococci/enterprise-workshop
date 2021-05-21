4c21b92d5e4eb762ee8da778aa5a38f6714b3718
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-21 22:19:24 +0200

    Update create.blade.php

--- laravel/platano/resources/views/orders/create.blade.php
+++ laravel/platano/resources/views/orders/create.blade.php
@@ -1,1 +1,34 @@
+@extends('layouts.master')
 
+@section('content')
+<div class="card push-top">
+	<div class="card-header">
+		add order
+	</div>
+
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('orders.store') }}">
+			<div class="form-group">
+				@csrf
+				<label for="todo">todo</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="todo" 
+					placeholder="todo"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-outline-primary">create order</button>
+			<button type="button" onclick="window.location='/categories'" class="btn btn-block btn-outline-secondary">cancel</button>
+		</form>
+	</div>
+</div>
+@endsection
