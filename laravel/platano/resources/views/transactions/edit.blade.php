fea3fc504973f0a57af50af079f81d5cb743e6f2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-21 22:45:21 +0200

    Update edit.blade.php

--- laravel/platano/resources/views/transactions/edit.blade.php
+++ laravel/platano/resources/views/transactions/edit.blade.php
@@ -1,1 +1,35 @@
+@extends('layouts.master')
 
+@section('content')
+<div class="card push-top">
+	<div class="card-header">
+		edit/update
+	</div>
+
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div><br/>
+		@endif
+		<form method="post" action="{{ route('transactions.update', $transaction->id) }}">
+			<div class="form-group">
+				@csrf
+				@method('PATCH')
+				<label for="todo">todo</label>
+				<input 
+					type="text" 
+					class="form-control" 
+					name="todo" 
+					value="{{ $transaction->todo }}"/>
+			</div>
+			<button type="submit" class="btn btn-block btn-outline-danger">update transaction</button>
+			<button type="button" onclick="window.location='/transactions'" class="btn btn-block btn-outline-secondary">cancel</button>
+		</form>
+	</div>
+</div>
+@endsection
