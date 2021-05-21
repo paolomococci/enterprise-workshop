6ed98a061dc2f21982609263baf753a5c3854523
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-21 22:51:42 +0200

    Update index.blade.php

--- laravel/platano/resources/views/invoices/index.blade.php
+++ laravel/platano/resources/views/invoices/index.blade.php
@@ -1,1 +1,39 @@
+@extends('layouts.master')
 
+@section('content')
+<div class="push-top">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br/>
+	@endif
+	<h3>list of registered invoices</h3>
+	<table class="table" summary="list of registered invoices">
+		<thead>
+			<tr class="table-warning">
+				<td>id</td>
+				<td>todo</td>
+				<td class="text-center">action</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($invoice as $invoices)
+			<tr>
+				<td>{{$invoices->id}}</td>
+				<td>{{$invoices->todo}}</td>
+				<td class="text-center">
+					<nobr>
+						<a href="{{ route('invoices.edit', $invoices->id)}}" class="btn btn-outline-success btn-sm"">edit</a>
+						<form action="{{ route('invoices.destroy', $invoices->id)}}" method="post" style="display: inline-block">
+							@csrf
+							@method('DELETE')
+							<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+						</form>
+					</nobr>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+@endsection
