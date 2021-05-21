48202bc4dd5df8d1e9d5f4fcba9e245b353046b9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-21 22:26:34 +0200

    Update create.blade.php

--- laravel/platano/resources/views/transactions/create.blade.php
+++ laravel/platano/resources/views/transactions/create.blade.php
@@ -27,7 +27,7 @@
 					placeholder="todo"/>
 			</div>
 			<button type="submit" class="btn btn-block btn-outline-primary">create transaction</button>
-			<button type="button" onclick="window.location='/categories'" class="btn btn-block btn-outline-secondary">cancel</button>
+			<button type="button" onclick="window.location='/transactions'" class="btn btn-block btn-outline-secondary">cancel</button>
 		</form>
 	</div>
 </div>
