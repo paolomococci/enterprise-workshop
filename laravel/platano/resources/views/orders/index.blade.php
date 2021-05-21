1567ecf8e9c11649b034654b9e3f87d808324d9a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-21 22:55:55 +0200

    Update index.blade.php

--- laravel/platano/resources/views/orders/index.blade.php
+++ laravel/platano/resources/views/orders/index.blade.php
@@ -1,1 +1,39 @@
+@extends('layouts.master')
 
+@section('content')
+<div class="push-top">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br/>
+	@endif
+	<h3>list of registered orders</h3>
+	<table class="table" summary="list of registered orders">
+		<thead>
+			<tr class="table-warning">
+				<td>id</td>
+				<td>todo</td>
+				<td class="text-center">action</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($order as $orders)
+			<tr>
+				<td>{{$orders->id}}</td>
+				<td>{{$orders->todo}}</td>
+				<td class="text-center">
+					<nobr>
+						<a href="{{ route('orders.edit', $orders->id)}}" class="btn btn-outline-success btn-sm"">edit</a>
+						<form action="{{ route('orders.destroy', $orders->id)}}" method="post" style="display: inline-block">
+							@csrf
+							@method('DELETE')
+							<button class="btn btn-danger btn-sm"" type="submit">delete</button>
+						</form>
+					</nobr>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+@endsection
