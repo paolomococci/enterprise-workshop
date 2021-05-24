b57d085ad3744989442624ed75458c0b089b1bf2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-24 17:06:46 +0200

    Update Application.java

--- vaadin/said/src/main/java/local/example/said/Application.java
+++ vaadin/said/src/main/java/local/example/said/Application.java
@@ -24,8 +24,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Application {
 	
 	@PostConstruct
