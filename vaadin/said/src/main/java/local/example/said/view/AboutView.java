1e5728e12121cc5ba6668dbfd6b72c4a55189499
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-24 11:31:01 +0200

    Update AboutView.java

--- vaadin/said/src/main/java/local/example/said/view/AboutView.java
+++ vaadin/said/src/main/java/local/example/said/view/AboutView.java
@@ -18,5 +18,20 @@
 
 package local.example.said.view;
 
-public class AboutView {
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("About")
+@Route(value = "about", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+public class AboutView
+        extends Div {
+
+    public AboutView() {
+        this.addClassName("about-view");
+        this.add(new Text("Description about this demo web application"));
+    }
 }
