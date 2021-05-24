bb83a1581b5a5b93b9d313c0fa72e98bb7e070ee
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-24 16:53:13 +0200

    Update Customer.java

--- vaadin/said/src/main/java/local/example/said/data/model/Customer.java
+++ vaadin/said/src/main/java/local/example/said/data/model/Customer.java
@@ -19,9 +19,14 @@
 package local.example.said.data.model;
 
 import local.example.said.data.AbstractData;
+
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -29,11 +34,42 @@ import java.time.LocalDate;
 public class Customer
         extends AbstractData {
 
+    @NotNull
+    @Size(min = 1, max = 20, message = "size range of this field is 1 to 20 characters")
+    @Pattern(
+            regexp = "^[a-zA-Z_-]*$",
+            message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
+    )
     private String firstName;
+
+    @NotNull
+    @Size(min = 1, max = 20, message = "size range of this field is 1 to 20 characters")
+    @Pattern(
+            regexp = "^[a-zA-Z_-]*$",
+            message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
+    )
     private String lastName;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 6, max = 20, message = "size range of this field is 6 to 20 characters")
+    @Pattern(
+            regexp = "^[0-9-]*$",
+            message = "only the characters `0-9` `-` are allowed"
+    )
     private String phone;
+
     private LocalDate dateOfBirth;
+
+    @NotNull
+    @Size(min = 3, max = 50, message = "size range of this field is 3 to 50 characters")
+    @Pattern(
+            regexp = "^[a-zA-Z_-]*$",
+            message = "only the characters `a-z`, `A-Z`, `_` and `-` are allowed"
+    )
     private String occupation;
+
     private boolean important;
 }
