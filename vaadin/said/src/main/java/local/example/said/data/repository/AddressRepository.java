5b654255d5c4092fa9f19b2e5e419276392cce48
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-25 10:42:35 +0200

    Update AddressRepository.java

--- vaadin/said/src/main/java/local/example/said/data/repository/AddressRepository.java
+++ vaadin/said/src/main/java/local/example/said/data/repository/AddressRepository.java
@@ -19,10 +19,11 @@
 package local.example.said.data.repository;
 
 import local.example.said.data.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository
-        extends JpaRepository<Address, Long> {
+        extends CrudRepository<Address, Long> {
 }
