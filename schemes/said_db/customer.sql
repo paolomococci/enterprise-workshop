5bd6bd83f39096f2544ea6ab3a48388ede5ce106
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-25 16:09:49 +0200

    Update customer.sql

--- schemes/said_db/customer.sql
+++ schemes/said_db/customer.sql
@@ -8,5 +8,8 @@ CREATE TABLE said_db.customer (
   `last_name` varchar(20) NOT NULL,
   `occupation` varchar(50) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
+  `address_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKglkhkmh2vyn790ijs6hiqqpi` (`address_id`),
+  CONSTRAINT `FKglkhkmh2vyn790ijs6hiqqpi` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
