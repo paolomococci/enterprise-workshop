73fb92934c057e704b5e8be13d742f2ef10691f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-26 18:26:07 +0200

    Create HelloView.java

---
+++ vaadin/said/src/main/java/local/example/said/view/HelloView.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.said.view;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import local.example.said.view.layout.MainLayout;
+
+@PageTitle("hello")
+@Route(value = "hello", layout = MainLayout.class)
+public class HelloView
+        extends Div {
+
+    public HelloView() {
+        addClassName("hello-view");
+        add(
+                new VerticalLayout(
+                        new Label("the content of the hello page has yet to be developed")
+                )
+        );
+    }
+}
