e8ffd8eedf7b54dcba4b16fda30ace0c64bff537
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-29 15:00:50 +0200

    Update CustomerEntity.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/entity/CustomerEntity.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/entity/CustomerEntity.java
@@ -21,8 +21,10 @@ package local.example.capstone.data.entity;
 import local.example.capstone.data.AbstractCompany;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
-public class CustomerEntity 
+@Table(name = "customers")
+public class CustomerEntity
         extends AbstractCompany {
 }
