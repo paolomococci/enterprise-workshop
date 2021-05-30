340d4a96e3afc4ae205ab9511debd9cc64d1adf5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-30 11:03:19 +0200

    Update InvoiceService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/InvoiceService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/InvoiceService.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
@@ -34,5 +35,9 @@ public class InvoiceService {
 
     public List<InvoiceEntity> readAll() {
         return (List<InvoiceEntity>) invoiceRepository.findAll();
+    }
+
+    public Optional<InvoiceEntity> read(Long id) {
+        return invoiceRepository.findById(id);
     }
 }
