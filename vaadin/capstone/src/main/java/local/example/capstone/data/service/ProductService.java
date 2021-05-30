3ed445335e28c2d77cb4ad386062701917eac5fc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-30 11:14:16 +0200

    Update ProductService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -34,5 +35,9 @@ public class ProductService {
 
     public List<ProductEntity> readAll() {
         return (List<ProductEntity>) productRepository.findAll();
+    }
+    
+    public Optional<ProductEntity> read(Long id) {
+        return productRepository.findById(id);
     }
 }
