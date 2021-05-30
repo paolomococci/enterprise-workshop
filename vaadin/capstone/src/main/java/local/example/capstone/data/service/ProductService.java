83bb4fec11fd9beb297eacd3be2975e6b125aae3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-30 10:30:18 +0200

    Update ProductService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
@@ -18,8 +18,21 @@
 
 package local.example.capstone.data.service;
 
+import local.example.capstone.data.entity.ProductEntity;
+import local.example.capstone.data.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<ProductEntity> readAll() {
+        return (List<ProductEntity>) productRepository.findAll();
+    }
 }
