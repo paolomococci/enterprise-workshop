f350ed185bebfa7a43eb01207ca9e9299ca8ef10
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-30 09:56:31 +0200

    Create CarrierService.java

---
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/CarrierService.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.capstone.data.service;
+
+import local.example.capstone.data.entity.CarrierEntity;
+import local.example.capstone.data.repository.CarrierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarrierService {
+
+    @Autowired
+    CarrierRepository carrierRepository;
+
+    public List<CarrierEntity> readAll() {
+        return (List<CarrierEntity>) carrierRepository.findAll();
+    }
+}
