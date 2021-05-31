e572154c7a8a7208bea0f389fb7417c3d39d59e4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:48:57 +0200

    Create contact-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/contact-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('contact-form-view')
+export class ContactFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
