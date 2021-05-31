f491cf3b8c8cd94718124138d792b00944968770
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:59:01 +0200

    Create product-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/product-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('product-form-view')
+export class ProductFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
