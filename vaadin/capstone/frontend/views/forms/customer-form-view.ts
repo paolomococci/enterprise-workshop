2946c73493ed6ac5a2809adc47e77d2cb9862d6d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:50:12 +0200

    Create customer-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/customer-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('customer-form-view')
+export class CustomerFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
