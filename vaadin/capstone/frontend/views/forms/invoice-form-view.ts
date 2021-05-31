a04bd50045f5beb0ac2a1372d68a77d5c982f3e9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:55:00 +0200

    Create invoice-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/invoice-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('invoice-form-view')
+export class InvoiceFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
