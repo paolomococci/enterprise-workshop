726f91be99cb952d861100cc51694cd4c7111cc6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:49:36 +0200

    Create crew-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/crew-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('crew-form-view')
+export class CrewFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
