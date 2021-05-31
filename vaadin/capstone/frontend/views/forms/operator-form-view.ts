30269852ed32201dbfce05aa8e7804d505ede9f2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:57:40 +0200

    Create operator-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/operator-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('operator-form-view')
+export class OperatorFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
