a004bb4928f8361622633aeeec9696cc2f322197
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:47:20 +0200

    Create carrier-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/carrier-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('carrier-form-view')
+export class CarrierFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
