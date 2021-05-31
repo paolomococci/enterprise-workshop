279ba7d1938d738b44a6c0a1fc2f973e1b6a4c9b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 20:45:18 +0200

    Create address-form-view.ts

---
+++ vaadin/capstone/frontend/views/forms/address-form-view.ts
@@ -0,0 +1,11 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('address-form-view')
+export class AddressFormView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<div>todo</div>`;
+  }
+}
