1bec55742b7f159548a2e22a6c964d9cc3edd63c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-05-31 16:48:17 +0200

    Update CrewService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/CrewService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/CrewService.java
@@ -57,7 +57,7 @@ public class CrewService {
                 }));
     }
 
-    public void delete() {
-        // TODO
+    public void delete(Long id) {
+        crewRepository.deleteById(id);
     }
 }
