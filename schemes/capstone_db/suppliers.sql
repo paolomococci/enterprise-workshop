8b5348c034478193f5a23e050354a43b6bc4cb15
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-02 08:47:07 +0200

    Create suppliers.sql

---
+++ schemes/capstone_db/suppliers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE capstone_db.suppliers (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `sticker` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
