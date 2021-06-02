30fe80b21eb03104a2a5abee768bed5692d387a3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-02 08:45:42 +0200

    Create operators.sql

---
+++ schemes/capstone_db/operators.sql
@@ -0,0 +1,8 @@
+CREATE TABLE capstone_db.operators (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
