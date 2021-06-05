7cb185370afd7aa3e82de533f9b9386d2ced00ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-05 08:48:14 +0200

    Update AddressFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/AddressFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/AddressFormView.java
@@ -18,5 +18,58 @@
 
 package local.example.capstone.view.form;
 
-public class AddressFormView {
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import local.example.capstone.data.entity.AddressEntity;
+import local.example.capstone.data.service.AddressService;
+import local.example.capstone.view.MainView;
+
+@Route(value = "address-form", layout = MainView.class)
+@PageTitle("Address Form")
+@Tag("address-form-view")
+@JsModule("./views/forms/address-form-view.ts")
+public class AddressFormView
+        extends LitTemplate {
+
+    @Id("streetAddress")
+    private TextField street;
+    @Id("postalCode")
+    private TextField postalCode;
+    @Id("city")
+    private TextField city;
+    @Id("state")
+    private TextField state;
+    @Id("country")
+    private TextField country;
+
+    @Id("save")
+    private Button save;
+    @Id("cancel")
+    private Button cancel;
+
+    private Binder<AddressEntity> addressEntityBinder = new Binder<>(AddressEntity.class);
+
+    public AddressFormView(AddressService addressService) {
+        this.addressEntityBinder.bindInstanceFields(this);
+        this.clearForm();
+        this.cancel.addClickListener(e -> this.clearForm());
+        this.save.addClickListener(event -> {
+            addressService.create(this.addressEntityBinder.getBean());
+            Notification.show(this.addressEntityBinder.getBean().getClass().getSimpleName() + " item created");
+            this.clearForm();
+        });
+    }
+
+    private void clearForm() {
+        this.addressEntityBinder.setBean(new AddressEntity());
+    }
 }
