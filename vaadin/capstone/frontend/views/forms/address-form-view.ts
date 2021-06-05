49f5578773fec90cf04772716883570c70449ea6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-05 08:46:37 +0200

    Update address-form-view.ts

--- vaadin/capstone/frontend/views/forms/address-form-view.ts
+++ vaadin/capstone/frontend/views/forms/address-form-view.ts
@@ -1,3 +1,7 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('address-form-view')
@@ -6,6 +10,19 @@ export class AddressFormView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>todo</div>`;
+    return html`<h3>address</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field label="street address" id="streetAddress" colspan="2"></vaadin-text-field>
+        <vaadin-text-field label="postal code" id="postalCode" pattern="[0-9]{0,6}" prevent-invalid-input></vaadin-text-field>
+        <vaadin-text-field label="city" id="city"></vaadin-text-field>
+        <vaadin-text-field label="state" id="state"></vaadin-text-field>
+        <vaadin-text-field label="country" id="country"></vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="save">save</vaadin-button>
+        <vaadin-button id="cancel">cancel</vaadin-button>
+      </vaadin-horizontal-layout>`;
   }
 }
