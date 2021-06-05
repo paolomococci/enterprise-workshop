bd5d4285854077726ef3d54a2cd9196b5bc0c12f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-05 09:32:12 +0200

    Update carrier-form-view.ts

--- vaadin/capstone/frontend/views/forms/carrier-form-view.ts
+++ vaadin/capstone/frontend/views/forms/carrier-form-view.ts
@@ -1,3 +1,7 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('carrier-form-view')
@@ -6,6 +10,16 @@ export class CarrierFormView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>todo</div>`;
+    return html`<h3>carrier</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field label="carrier name" id="carrierName"></vaadin-text-field></br>
+        <vaadin-text-field label="sticker" id="sticker" pattern="[a-z]{0,12}" prevent-invalid-input></vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="save">save</vaadin-button>
+        <vaadin-button id="cancel">cancel</vaadin-button>
+      </vaadin-horizontal-layout>`;
   }
 }
