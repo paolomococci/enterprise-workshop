bfc8927d59bba62e7cfadf693cd30901f5b09154
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-06 19:14:27 +0200

    Update component-form-view.ts

--- vaadin/capstone/frontend/views/forms/component-form-view.ts
+++ vaadin/capstone/frontend/views/forms/component-form-view.ts
@@ -1,5 +1,6 @@
 import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-text-field/vaadin-integer-field';
 import '@vaadin/vaadin-form-layout';
 import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
@@ -19,15 +20,16 @@ export class ComponentFormView extends LitElement {
           helper-text="it accepts only twelve digits from zero to nine">
           <iron-icon slot="prefix" icon="vaadin:list-ol"></iron-icon>
         </vaadin-text-field></br>
-        <vaadin-text-field
+        <vaadin-integer-field
           label="amount"
           id="amount"
           helper-text="max 9999"
-          pattern="[0-9]{0,4}" prevent-invalid-input
+          min="0"
+          max="9999"
           has-controls>
           <iron-icon slot="prefix" icon="vaadin:abacus"></iron-icon>
           <div slot="suffix">pieces</div>
-        </vaadin-text-field>
+        </vaadin-integer-field>
       </vaadin-form-layout>
       <vaadin-horizontal-layout
         style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
