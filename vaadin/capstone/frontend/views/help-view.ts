279fb21c290d63f43db0feb3606d5e0a51c0351a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-06 20:43:37 +0200

    Create help-view.ts

---
+++ vaadin/capstone/frontend/views/help-view.ts
@@ -0,0 +1,18 @@
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('help-view')
+export class AboutView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<section class="help-view">
+      <h3>help</h3>
+      <ul>
+        <li>short help note</li>
+        <li>short help note</li>
+        <li>short help note</li>
+      </ul>
+    </section>`;
+  }
+}
