38ec61cba5ffc44fa9832821add7668ab4929f6b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-06 20:44:42 +0200

    Create HelpView.java

---
+++ vaadin/capstone/src/main/java/local/example/capstone/view/HelpView.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.capstone.view;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "help", layout = MainView.class)
+@PageTitle("Help")
+@Tag("help-view")
+@JsModule("./views/help-view.ts")
+public class HelpView
+        extends LitTemplate {
+
+    public HelpView() {}
+}
