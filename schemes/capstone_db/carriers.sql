59a1f126503096525eff63ae6ee89a3a344595ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-09 03:31:36 +0200

    Update carriers.sql

--- schemes/capstone_db/carriers.sql
+++ schemes/capstone_db/carriers.sql
@@ -3,5 +3,6 @@ CREATE TABLE capstone_db.carriers (
   `name` varchar(255) DEFAULT NULL,
   `sticker` varchar(255) DEFAULT NULL,
   `updated` datetime(6) DEFAULT NULL,
+  `created` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
