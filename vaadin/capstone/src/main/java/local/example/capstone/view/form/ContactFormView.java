c22df5d93ffc7a5bc6533704f45fc0251213d00a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-09 03:18:21 +0200

    Update ContactFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/ContactFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/ContactFormView.java
@@ -18,5 +18,67 @@
 
 package local.example.capstone.view.form;
 
-public class ContactFormView {
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import local.example.capstone.data.entity.ContactEntity;
+import local.example.capstone.data.service.ContactService;
+import local.example.capstone.view.MainView;
+
+@Route(value = "contact-form", layout = MainView.class)
+@PageTitle("Contact Form")
+@Tag("contact-form-view")
+@JsModule("./views/forms/contact-form-view.ts")
+public class ContactFormView
+        extends LitTemplate {
+
+    @Id("name")
+    private TextField name;
+
+    @Id("surname")
+    private TextField surname;
+
+    @Id("dateOfBirth")
+    private DatePicker dateOfBirth;
+
+    @Id("phoneMobileNumber")
+    private String phoneMobileNumber;
+
+    @Id("contributoryIdentifier")
+    private TextField contributoryIdentifier;
+
+    @Id("email")
+    private EmailField email;
+
+    @Id("profession")
+    private TextField profession;
+
+    @Id("save")
+    private Button save;
+    @Id("cancel")
+    private Button cancel;
+
+    private Binder<ContactEntity> contactEntityBinder = new Binder(ContactEntity.class);
+
+    public ContactFormView(ContactService contactService) {
+        this.contactEntityBinder.bindInstanceFields(this);
+        this.clearForm();
+        this.cancel.addClickListener(e -> clearForm());
+        this.save.addClickListener(e -> {
+            // TODO
+        });
+    }
+
+    private void clearForm() {
+        contactEntityBinder.setBean(new ContactEntity());
+    }
 }
