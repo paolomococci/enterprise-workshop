1b0cc74a6641ab289fd0b27a7091aa400ece05b7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-09 02:38:46 +0200

    Update contact-form-view.ts

--- vaadin/capstone/frontend/views/forms/contact-form-view.ts
+++ vaadin/capstone/frontend/views/forms/contact-form-view.ts
@@ -1,3 +1,11 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-combo-box';
+import '@vaadin/vaadin-custom-field';
+import '@vaadin/vaadin-date-picker';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-text-field/vaadin-email-field';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('contact-form-view')
