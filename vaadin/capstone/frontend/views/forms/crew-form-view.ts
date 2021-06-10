e678b6882f428583589fc5f0487af49ff3e0cac7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 20:44:30 +0200

    Update crew-form-view.ts

--- vaadin/capstone/frontend/views/forms/crew-form-view.ts
+++ vaadin/capstone/frontend/views/forms/crew-form-view.ts
@@ -1,6 +1,5 @@
 import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-text-field';
-import '@vaadin/vaadin-text-field/vaadin-integer-field';
 import '@vaadin/vaadin-form-layout';
 import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
@@ -11,6 +10,26 @@ export class CrewFormView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>todo</div>`;
+    return html`<h3>crew</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field
+          label="crew code"
+          id="crewCode"
+          pattern="[0-9]{0,12}" prevent-invalid-input
+          helper-text="it accepts only twelve digits from zero to nine">
+        </vaadin-text-field></br>
+        <vaadin-text-field
+          label="crew name"
+          id="crewName"
+          pattern="[a-zA-Z0-9]{0,12}" prevent-invalid-input
+          helper-text="this field only accepts a string of twelve letters or numeric digits">
+        </vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="save">save</vaadin-button>
+        <vaadin-button id="cancel">cancel</vaadin-button>
+      </vaadin-horizontal-layout>`;
   }
 }
