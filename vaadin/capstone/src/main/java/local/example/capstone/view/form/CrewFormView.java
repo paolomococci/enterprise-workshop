540261e6b303c7ff53203946c594f2933d18cd3b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 21:28:32 +0200

    Update CrewFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/CrewFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/CrewFormView.java
@@ -19,11 +19,18 @@
 package local.example.capstone.view.form;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import local.example.capstone.data.entity.CrewEntity;
+import local.example.capstone.data.service.CrewService;
 import local.example.capstone.view.MainView;
 
 @Route(value = "crew-form", layout = MainView.class)
@@ -32,4 +39,32 @@ import local.example.capstone.view.MainView;
 @JsModule("./views/forms/crew-form-view.ts")
 public class CrewFormView
         extends LitTemplate {
+
+    @Id("crewCode")
+    private TextField code;
+
+    @Id("crewName")
+    private TextField name;
+
+    @Id("save")
+    private Button save;
+    @Id("cancel")
+    private Button cancel;
+
+    private Binder<CrewEntity> crewEntityBinder = new Binder<>(CrewEntity.class);
+
+    public CrewFormView(CrewService crewService) {
+        this.crewEntityBinder.bindInstanceFields(this);
+        this.clearForm();
+        this.cancel.addClickListener(buttonClickEvent -> this.clearForm());
+        this.save.addClickListener(buttonClickEvent -> {
+            crewService.create(this.crewEntityBinder.getBean());
+            Notification.show("added an item " + this.crewEntityBinder.getBean().getClass().getSimpleName());
+            this.clearForm();
+        });
+    }
+
+    private void clearForm() {
+        this.crewEntityBinder.setBean(new CrewEntity());
+    }
 }
