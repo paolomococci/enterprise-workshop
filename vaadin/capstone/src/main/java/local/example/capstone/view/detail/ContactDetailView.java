d02ab8e510bc14ee59c99ecf6dffa62f155fa2e3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 21:56:42 +0200

    Update ContactDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ContactDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ContactDetailView.java
@@ -18,5 +18,10 @@
 
 package local.example.capstone.view.detail;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("Contact Detail")
+@Tag("contact-detail-view")
 public class ContactDetailView {
 }
