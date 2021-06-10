15eaabd1b69500e19f277189f86cdd41e7cba906
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 21:57:07 +0200

    Update ComponentDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
@@ -18,5 +18,10 @@
 
 package local.example.capstone.view.detail;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("Component Detail")
+@Tag("component-detail-view")
 public class ComponentDetailView {
 }
