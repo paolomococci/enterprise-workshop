fbed34eae6043885534a620ca3f1fd6d4a5c65bf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 21:30:35 +0200

    Update CrewFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/CrewFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/CrewFormView.java
@@ -25,6 +25,7 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -51,7 +52,7 @@ public class CrewFormView
     @Id("cancel")
     private Button cancel;
 
-    private Binder<CrewEntity> crewEntityBinder = new Binder<>(CrewEntity.class);
+    private Binder<CrewEntity> crewEntityBinder = new BeanValidationBinder<>(CrewEntity.class);
 
     public CrewFormView(CrewService crewService) {
         this.crewEntityBinder.bindInstanceFields(this);
