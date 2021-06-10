0a60f765308447e2b0021c59b3fb0620dbab222d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-10 21:58:00 +0200

    Update AddressDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
@@ -18,5 +18,10 @@
 
 package local.example.capstone.view.detail;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("Address Detail")
+@Tag("address-detail-view")
 public class AddressDetailView {
 }
