8a3077202ce7ac3e80f5c0940730e91188bbdd43
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-11 23:20:07 +0200

    Create App.vue

---
+++ vue/capstone/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="nav">
+    <router-link to="/">home</router-link> |
+    <router-link to="/about">about</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="less">
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #235;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #235;
+
+    &.router-link-exact-active {
+      color: #4b8;
+    }
+  }
+}
+</style>
