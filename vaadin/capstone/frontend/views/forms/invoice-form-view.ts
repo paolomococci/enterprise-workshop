9389366d9ad6d3484ec6fb00b2859098dfc997f0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-12 18:43:46 +0200

    Update invoice-form-view.ts

--- vaadin/capstone/frontend/views/forms/invoice-form-view.ts
+++ vaadin/capstone/frontend/views/forms/invoice-form-view.ts
@@ -1,3 +1,8 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-text-field/vaadin-number-field';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('invoice-form-view')
