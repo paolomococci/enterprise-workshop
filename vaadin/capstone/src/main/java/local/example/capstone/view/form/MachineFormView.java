e5cfffa31d94f3b01c448f430c7a6cac01498c0d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-12 19:44:22 +0200

    Update MachineFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/MachineFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/MachineFormView.java
@@ -18,5 +18,16 @@
 
 package local.example.capstone.view.form;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import local.example.capstone.view.MainView;
+
+@Route(value = "machine-form", layout = MainView.class)
+@PageTitle("Machine Form")
+@Tag("machine-form-view")
+@JsModule("./views/forms/machine-form-view.ts")
 public class MachineFormView {
 }
