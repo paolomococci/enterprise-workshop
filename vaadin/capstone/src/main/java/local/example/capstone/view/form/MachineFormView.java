bf70b5be5028d087dc0fcbe22ab5c9e245a1a9fd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-12 19:49:31 +0200

    Update MachineFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/MachineFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/MachineFormView.java
@@ -19,8 +19,12 @@
 package local.example.capstone.view.form;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -32,4 +36,15 @@ import local.example.capstone.view.MainView;
 @JsModule("./views/forms/machine-form-view.ts")
 public class MachineFormView
         extends LitTemplate {
+
+    @Id("machineLabel")
+    private TextField label;
+
+    @Id("capacity")
+    private IntegerField capacity;
+
+    @Id("save")
+    private Button save;
+    @Id("cancel")
+    private Button cancel;
 }
