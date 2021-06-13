fcdac0c9df7d37d421867aac82ef26298368c060
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 19:49:34 +0200

    Update supplier-form-view.ts

--- vaadin/capstone/frontend/views/forms/supplier-form-view.ts
+++ vaadin/capstone/frontend/views/forms/supplier-form-view.ts
@@ -10,6 +10,16 @@ export class SupplierFormView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>todo</div>`;
+    return html`<h3>supplier</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field label="supplier name" id="supplierName"></vaadin-text-field></br>
+        <vaadin-text-field label="sticker" id="sticker" pattern="[a-z]{0,12}" prevent-invalid-input></vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="save">save</vaadin-button>
+        <vaadin-button id="cancel">cancel</vaadin-button>
+      </vaadin-horizontal-layout>`;
   }
 }
