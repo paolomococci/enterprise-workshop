1f99b1d5c62ae4a207c901ea26121f559c0367a7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 19:46:47 +0200

    Update supplier-form-view.ts

--- vaadin/capstone/frontend/views/forms/supplier-form-view.ts
+++ vaadin/capstone/frontend/views/forms/supplier-form-view.ts
@@ -1,3 +1,7 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('supplier-form-view')
