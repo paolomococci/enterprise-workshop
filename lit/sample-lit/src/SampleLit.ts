80d692b41b0ecd4ed7899f12540c9e471d89642c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 08:31:21 +0200

    Create SampleLit.ts

---
+++ lit/sample-lit/src/SampleLit.ts
@@ -0,0 +1,62 @@
+import { LitElement, html, css, property } from 'lit-element';
+import { openWcLogo } from './open-wc-logo.js';
+
+export class SampleLit extends LitElement {
+  @property({ type: String }) title = 'My app';
+
+  static styles = css`
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--sample-lit-background-color);
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+    .logo > svg {
+      margin-top: 36px;
+      animation: app-logo-spin infinite 20s linear;
+    }
+
+    @keyframes app-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+
+    .app-footer a {
+      margin-left: 5px;
+    }
+  `;
+
+  render() {
+    return html`
+      <main>
+        <div class="logo">${openWcLogo}</div>
+        <h1>${this.title}</h1>
+
+        <p>Edit <code>src/SampleLit.ts</code> and save to reload.</p>
+      </main>
+
+      <p class="app-footer">sample-lit</p>
+    `;
+  }
+}
