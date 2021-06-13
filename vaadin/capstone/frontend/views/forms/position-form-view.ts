ea3d009728a65d021500a7742de8670dbe455d06
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 18:21:18 +0200

    Update position-form-view.ts

--- vaadin/capstone/frontend/views/forms/position-form-view.ts
+++ vaadin/capstone/frontend/views/forms/position-form-view.ts
@@ -11,6 +11,30 @@ export class PositionFormView extends LitElement {
     return this;
   }
   render() {
-    return html`<div>todo</div>`;
+    return html`<h3>position</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field
+          label="position label"
+          id="positionLabel"
+          pattern="[0-9]{0,12}" prevent-invalid-input
+          helper-text="it accepts only twelve digits from zero to nine">
+        </vaadin-text-field></br>
+        <vaadin-integer-field
+          label="capacity"
+          id="capacity"
+          helper-text="max 99"
+          value="0"
+          min="0"
+          max="99"
+          has-controls>
+          <div slot="suffix">capacity</div>
+        </vaadin-integer-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="save">save</vaadin-button>
+        <vaadin-button id="cancel">cancel</vaadin-button>
+      </vaadin-horizontal-layout>`;
   }
 }
