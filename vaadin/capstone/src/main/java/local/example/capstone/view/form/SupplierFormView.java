de2a0f675c5f4fca98f9c2c49dfd61835d19479e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 19:45:34 +0200

    Update SupplierFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/SupplierFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/SupplierFormView.java
@@ -18,5 +18,16 @@
 
 package local.example.capstone.view.form;
 
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import local.example.capstone.view.MainView;
+
+@Route(value = "supplier-form", layout = MainView.class)
+@PageTitle("Supplier Form")
+@Tag("supplier-form-view")
+@JsModule("./views/forms/supplier-form-view.ts")
 public class SupplierFormView {
 }
