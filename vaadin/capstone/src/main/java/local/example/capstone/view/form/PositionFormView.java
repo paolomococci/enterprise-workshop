f708df4f8e47d797f3ebf0b93e268f4a9ce12cb0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 18:15:00 +0200

    Update PositionFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/PositionFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/PositionFormView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.form;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import local.example.capstone.view.MainView;
@@ -28,5 +29,6 @@ import local.example.capstone.view.MainView;
 @PageTitle("position Form")
 @Tag("position-form-view")
 @JsModule("./views/forms/position-form-view.ts")
-public class PositionFormView {
+public class PositionFormView
+        extends LitTemplate {
 }
