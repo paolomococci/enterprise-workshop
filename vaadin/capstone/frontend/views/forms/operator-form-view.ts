a9dcba11733ab5765a28fb5586917768f30b80ae
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-13 17:10:25 +0200

    Update operator-form-view.ts

--- vaadin/capstone/frontend/views/forms/operator-form-view.ts
+++ vaadin/capstone/frontend/views/forms/operator-form-view.ts
@@ -1,3 +1,9 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-date-picker';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-text-field/vaadin-email-field';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('operator-form-view')
