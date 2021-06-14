cb6d0035505638a6da7c740f2aded3b117674868
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-14 17:18:18 +0200

    Create invoice-detail-view.ts

---
+++ vaadin/capstone/frontend/views/details/invoice-detail-view.ts
@@ -0,0 +1,12 @@
+import '@vaadin/vaadin-split-layout';
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('invoice-detail-view')
+export class InvoiceDetailView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<vaadin-split-layout class="w-full h-full"></vaadin-split-layout>`;
+  }
+}
