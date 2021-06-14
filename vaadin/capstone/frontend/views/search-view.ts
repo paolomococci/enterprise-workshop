cb2c06c4d459178c68cb8094dfd93258223cad74
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-14 06:21:35 +0200

    Create search-view.ts

---
+++ vaadin/capstone/frontend/views/search-view.ts
@@ -0,0 +1,29 @@
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('search-view')
+export class SearchView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<h3>search</h3>
+      <vaadin-form-layout style="width: 100%;">
+        <vaadin-text-field
+          label="search"
+          id="search"
+          colspan="3"
+          pattern="[a-zA-Z0-9]{0,20}"
+          prevent-invalid-input>
+        </vaadin-text-field>
+      </vaadin-form-layout>
+      <vaadin-horizontal-layout
+        style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
+        theme="spacing">
+        <vaadin-button theme="primary" id="search">search</vaadin-button>
+      </vaadin-horizontal-layout>`;
+  }
+}
