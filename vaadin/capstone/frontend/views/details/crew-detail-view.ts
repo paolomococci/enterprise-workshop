da1c22997e50fd0a55c13fadd37b6217759be2f6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-14 17:17:16 +0200

    Create crew-detail-view.ts

---
+++ vaadin/capstone/frontend/views/details/crew-detail-view.ts
@@ -0,0 +1,12 @@
+import '@vaadin/vaadin-split-layout';
+import { customElement, html, LitElement } from 'lit-element';
+
+@customElement('crew-detail-view')
+export class CrewDetailView extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`<vaadin-split-layout class="w-full h-full"></vaadin-split-layout>`;
+  }
+}
