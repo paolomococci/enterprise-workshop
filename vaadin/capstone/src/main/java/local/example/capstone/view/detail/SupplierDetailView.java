821630f8f98234934387cb96291851d8acbf2e5f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 17:33:41 +0200

    Update SupplierDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
@@ -20,10 +20,12 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.router.PageTitle;
 
 @PageTitle("Supplier Detail")
 @Tag("supplier-detail-view")
 @JsModule("./views/details/supplier-detail-view.ts")
-public class SupplierDetailView {
+public class SupplierDetailView
+        extends LitTemplate {
 }
