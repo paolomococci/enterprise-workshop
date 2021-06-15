b0a79e1d431e8af642e7594023cc5c8a8c4c43df
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 17:31:21 +0200

    Update CarrierDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CarrierDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CarrierDetailView.java
@@ -19,9 +19,11 @@
 package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.PageTitle;
 
 @PageTitle("Carrier Detail")
 @Tag("carrier-detail-view")
+@JsModule("./views/details/carrier-detail-view.ts")
 public class CarrierDetailView {
 }
