e2f862b912a83d7198c8600fb9b63ff165823751
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 18:05:20 +0200

    Update crew-detail-view.ts

--- vaadin/capstone/frontend/views/details/crew-detail-view.ts
+++ vaadin/capstone/frontend/views/details/crew-detail-view.ts
@@ -12,6 +12,19 @@ export class CrewDetailView extends LitElement {
   }
   render() {
     return html`<vaadin-split-layout class="w-full h-full">
+      <div class="flex-grow w-full" id="grid-wrapper">
+        <vaadin-grid id="grid"></vaadin-grid>
+      </div>
+      <div class="flex flex-col" style="width: 400px;">
+        <div class="p-l flex-grow">
+          <vaadin-form-layout>
+          </vaadin-form-layout>
+        </div>
+        <vaadin-horizontal-layout class="w-full flex-wrap bg-contrast-5 py-s px-l" theme="spacing">
+          <vaadin-button theme="primary" id="save">save</vaadin-button>
+          <vaadin-button theme="tertiary" slot="" id="cancel">cancel</vaadin-button>
+        </vaadin-horizontal-layout>
+      </div>
     </vaadin-split-layout>`;
   }
 }
