02a3e24e3a2c14738061bf9851af9c22cbfd48ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 17:55:47 +0200

    Update customer-detail-view.ts

--- vaadin/capstone/frontend/views/details/customer-detail-view.ts
+++ vaadin/capstone/frontend/views/details/customer-detail-view.ts
@@ -1,4 +1,8 @@
 import '@vaadin/vaadin-split-layout';
+import '@vaadin/vaadin-grid';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-button';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('customer-detail-view')
@@ -7,6 +11,7 @@ export class CustomerDetailView extends LitElement {
     return this;
   }
   render() {
-    return html`<vaadin-split-layout class="w-full h-full"></vaadin-split-layout>`;
+    return html`<vaadin-split-layout class="w-full h-full">
+    </vaadin-split-layout>`;
   }
 }
