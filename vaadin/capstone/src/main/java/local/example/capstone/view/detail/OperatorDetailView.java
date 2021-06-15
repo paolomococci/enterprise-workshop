2cedc921c5d4f1b793be3210033f0fe7325d3271
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 17:27:52 +0200

    Update OperatorDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/OperatorDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/OperatorDetailView.java
@@ -19,9 +19,11 @@
 package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.PageTitle;
 
 @PageTitle("Operator Detail")
 @Tag("operator-detail-view")
+@JsModule("./views/details/operator-detail-view.ts")
 public class OperatorDetailView {
 }
