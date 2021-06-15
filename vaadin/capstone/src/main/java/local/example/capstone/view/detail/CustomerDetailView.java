ef3f1c4211f9fcb6e1dc34813b98cd50335c0959
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-15 17:29:41 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -19,9 +19,11 @@
 package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.PageTitle;
 
 @PageTitle("Customer Detail")
 @Tag("customer-detail-view")
+@JsModule("./views/details/customer-detail-view.ts")
 public class CustomerDetailView {
 }
