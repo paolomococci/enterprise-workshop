f0e321f212faf9aed298411b817c0e3cf889a043
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-16 13:54:16 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -21,15 +21,20 @@ package local.example.capstone.view.detail;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
+import local.example.capstone.data.entity.CustomerEntity;
 
 @PageTitle("Customer Detail")
 @Tag("customer-detail-view")
 @JsModule("./views/details/customer-detail-view.ts")
 public class CustomerDetailView
         extends LitTemplate {
+
+    @Id("grid")
+    private Grid<CustomerEntity> customerEntityGrid;
 
     @Id("save")
     private Button save;
