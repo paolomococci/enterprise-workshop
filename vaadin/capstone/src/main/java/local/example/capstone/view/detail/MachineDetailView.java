de15747998f990cfcfcfcb97b9fb72270e27b22d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-16 13:53:02 +0200

    Update MachineDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
@@ -21,15 +21,20 @@ package local.example.capstone.view.detail;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
+import local.example.capstone.data.entity.MachineEntity;
 
 @PageTitle("Machine Detail")
 @Tag("machine-detail-view")
 @JsModule("./views/details/machine-detail-view.ts")
 public class MachineDetailView
         extends LitTemplate {
+
+    @Id("grid")
+    private Grid<MachineEntity> machineEntityGrid;
 
     @Id("save")
     private Button save;
