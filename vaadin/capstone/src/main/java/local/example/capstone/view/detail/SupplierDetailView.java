6a114c9e750809f05255ef00aae4d3c695f30762
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-16 13:51:07 +0200

    Update SupplierDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
@@ -21,15 +21,20 @@ package local.example.capstone.view.detail;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
+import local.example.capstone.data.entity.SupplierEntity;
 
 @PageTitle("Supplier Detail")
 @Tag("supplier-detail-view")
 @JsModule("./views/details/supplier-detail-view.ts")
 public class SupplierDetailView
         extends LitTemplate {
+
+    @Id("grid")
+    private Grid<SupplierEntity> supplierEntityGrid;
 
     @Id("save")
     private Button save;
