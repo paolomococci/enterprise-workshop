299406fb483be515149972342ea0b7e1bd2db2f2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-16 13:52:34 +0200

    Update OperatorDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/OperatorDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/OperatorDetailView.java
@@ -21,15 +21,20 @@ package local.example.capstone.view.detail;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
+import local.example.capstone.data.entity.OperatorEntity;
 
 @PageTitle("Operator Detail")
 @Tag("operator-detail-view")
 @JsModule("./views/details/operator-detail-view.ts")
 public class OperatorDetailView
         extends LitTemplate {
+
+    @Id("grid")
+    private Grid<OperatorEntity> operatorEntityGrid;
 
     @Id("save")
     private Button save;
