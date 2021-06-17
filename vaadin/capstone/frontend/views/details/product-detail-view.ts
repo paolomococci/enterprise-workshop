57c16b692f16a51137d87aa8158f027ea36f95eb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 15:39:13 +0200

    Update product-detail-view.ts

--- vaadin/capstone/frontend/views/details/product-detail-view.ts
+++ vaadin/capstone/frontend/views/details/product-detail-view.ts
@@ -20,6 +20,24 @@ export class ProductDetailView extends LitElement {
       <div class="flex flex-col" style="width: 400px;">
         <div class="p-l flex-grow">
           <vaadin-form-layout>
+            <vaadin-text-field
+              label="product code"
+              id="productCode"
+              pattern="[0-9]{0,12}" prevent-invalid-input
+              helper-text="it accepts only twelve digits from zero to nine">
+              <iron-icon slot="prefix" icon="vaadin:package"></iron-icon>
+            </vaadin-text-field></br>
+            <vaadin-integer-field
+              label="amount"
+              id="amount"
+              helper-text="max 9999"
+              value="0"
+              min="0"
+              max="9999"
+              has-controls>
+              <iron-icon slot="prefix" icon="vaadin:abacus"></iron-icon>
+              <div slot="suffix">pieces</div>
+            </vaadin-integer-field>
           </vaadin-form-layout>
         </div>
         <vaadin-horizontal-layout class="w-full flex-wrap bg-contrast-5 py-s px-l" theme="spacing">
