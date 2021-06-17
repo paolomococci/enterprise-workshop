4558404937066224af66d11a8b20be54a38a1975
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 15:22:18 +0200

    Update component-detail-view.ts

--- vaadin/capstone/frontend/views/details/component-detail-view.ts
+++ vaadin/capstone/frontend/views/details/component-detail-view.ts
@@ -20,6 +20,24 @@ export class ComponentDetailView extends LitElement {
       <div class="flex flex-col" style="width: 400px;">
         <div class="p-l flex-grow">
           <vaadin-form-layout>
+            <vaadin-text-field
+              label="component code"
+              id="componentCode"
+              pattern="[0-9]{0,12}" prevent-invalid-input
+              helper-text="it accepts only twelve digits from zero to nine">
+              <iron-icon slot="prefix" icon="vaadin:list-ol"></iron-icon>
+            </vaadin-text-field></br>
+            <vaadin-integer-field
+              label="amount"
+              id="amount"
+              helper-text="max 9999"
+              value="0"
+              min="0"
+              max="9999"
+              has-controls>
+              <iron-icon slot="prefix" icon="vaadin:abacus"></iron-icon>
+              <div slot="suffix">pieces</div>
+            </vaadin-integer-field>
           </vaadin-form-layout>
         </div>
         <vaadin-horizontal-layout class="w-full flex-wrap bg-contrast-5 py-s px-l" theme="spacing">
