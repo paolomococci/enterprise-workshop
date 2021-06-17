95e9733db7dac826253baf39dfd0415a656cf606
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 17:43:18 +0200

    Update SupplierService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/SupplierService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/SupplierService.java
@@ -59,16 +59,7 @@ public class SupplierService {
     }
 
     public void update(SupplierEntity updatedSupplierEntity) {
-        Optional.of(supplierRepository.findById(updatedSupplierEntity.getId()).map(
-                storedSupplierEntity -> {
-                    if (updatedSupplierEntity.getName() != null)
-                        storedSupplierEntity.setName(updatedSupplierEntity.getName());
-                    // TODO
-                    return supplierRepository.save(storedSupplierEntity);
-                }).orElseGet(
-                () -> {
-                    return supplierRepository.save(updatedSupplierEntity);
-                }));
+        this.update(updatedSupplierEntity, updatedSupplierEntity.getId());
     }
 
     public void delete(Long id) {
