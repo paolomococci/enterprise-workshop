a3722bc8643996237a9494cedebb4ec99a49c52e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 15:25:22 +0200

    Update crew-detail-view.ts

--- vaadin/capstone/frontend/views/details/crew-detail-view.ts
+++ vaadin/capstone/frontend/views/details/crew-detail-view.ts
@@ -19,6 +19,18 @@ export class CrewDetailView extends LitElement {
       <div class="flex flex-col" style="width: 400px;">
         <div class="p-l flex-grow">
           <vaadin-form-layout>
+            <vaadin-text-field
+              label="crew code"
+              id="crewCode"
+              pattern="[0-9]{0,12}" prevent-invalid-input
+              helper-text="it accepts only twelve digits from zero to nine">
+            </vaadin-text-field></br>
+            <vaadin-text-field
+              label="crew name"
+              id="crewName"
+              pattern="[a-zA-Z0-9]{0,12}" prevent-invalid-input
+              helper-text="this field only accepts a string of twelve letters or numeric digits">
+            </vaadin-text-field>
           </vaadin-form-layout>
         </div>
         <vaadin-horizontal-layout class="w-full flex-wrap bg-contrast-5 py-s px-l" theme="spacing">
