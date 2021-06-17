551508f110400a44ff8ad0e0ef4df4a2eb73d784
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 17:29:45 +0200

    Update ProductService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/ProductService.java
@@ -41,8 +41,8 @@ public class ProductService {
         productRepository.save(productEntity);
     }
 
-    public Optional<ProductEntity> read(Long id) {
-        return productRepository.findById(id);
+    public Optional<ProductEntity> read(String id) {
+        return productRepository.findById(Long.valueOf(id));
     }
 
     public void update(ProductEntity updatedProductEntity, Long id) {
