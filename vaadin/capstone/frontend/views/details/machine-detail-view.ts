79c384f0c883a6b898aa09957022a87ea31d4e6b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-17 15:32:34 +0200

    Update machine-detail-view.ts

--- vaadin/capstone/frontend/views/details/machine-detail-view.ts
+++ vaadin/capstone/frontend/views/details/machine-detail-view.ts
@@ -20,6 +20,23 @@ export class MachineDetailView extends LitElement {
       <div class="flex flex-col" style="width: 400px;">
         <div class="p-l flex-grow">
           <vaadin-form-layout>
+            <vaadin-text-field
+              label="machine label"
+              id="machineLabel"
+              pattern="[0-9]{0,12}" prevent-invalid-input
+              helper-text="it accepts only twelve digits from zero to nine">
+              <iron-icon slot="prefix" icon="vaadin:cog-o"></iron-icon>
+            </vaadin-text-field></br>
+            <vaadin-integer-field
+              label="capacity"
+              id="capacity"
+              helper-text="max 999"
+              value="0"
+              min="0"
+              max="999"
+              has-controls>
+              <div slot="suffix">capacity</div>
+            </vaadin-integer-field>
           </vaadin-form-layout>
         </div>
         <vaadin-horizontal-layout class="w-full flex-wrap bg-contrast-5 py-s px-l" theme="spacing">
