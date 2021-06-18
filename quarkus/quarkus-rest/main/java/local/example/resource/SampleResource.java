11b5b5013528f840c7050b5be93296be2930fead
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-18 19:48:01 +0200

    Create SampleResource.java

---
+++ quarkus/quarkus-rest/main/java/local/example/resource/SampleResource.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.resource;
+
+import local.example.model.Sample;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path(value = "/samples")
+public class SampleResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Sample> readAll(@QueryParam("name") String name) {
+        if (name != null)
+            return Sample.findByName(name);
+        return Sample.listAll();
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Sample sample) {
+        sample.id = null;
+        sample.persist();
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(sample)
+                .build();
+    }
+}
