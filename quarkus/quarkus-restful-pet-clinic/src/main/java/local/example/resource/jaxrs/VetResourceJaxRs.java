0fa2dae0866b7f3adc120e53db785a64362907d4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-20 11:43:46 +0200

    Update VetResourceJaxRs.java

--- quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/VetResourceJaxRs.java
+++ quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/VetResourceJaxRs.java
@@ -24,6 +24,7 @@ import local.example.resource.VetResource;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
