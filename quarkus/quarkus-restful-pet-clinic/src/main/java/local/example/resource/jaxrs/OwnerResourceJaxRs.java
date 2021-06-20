403eb35b40df3799befbdbfd181b2039b75dc1f8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-20 11:42:26 +0200

    Update OwnerResourceJaxRs.java

--- quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/OwnerResourceJaxRs.java
+++ quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/OwnerResourceJaxRs.java
@@ -24,9 +24,7 @@ import local.example.resource.OwnerResource;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
