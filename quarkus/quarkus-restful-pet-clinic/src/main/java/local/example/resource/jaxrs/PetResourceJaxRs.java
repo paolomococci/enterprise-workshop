348c305a0995d4ed4d6c3c301d5d1d150f96410b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-20 09:41:28 +0200

    Update PetResourceJaxRs.java

--- quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/PetResourceJaxRs.java
+++ quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/jaxrs/PetResourceJaxRs.java
@@ -21,11 +21,24 @@ package local.example.resource.jaxrs;
 import local.example.resource.PetResource;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path(value = "/pets")
 public class PetResourceJaxRs {
 
     @Inject
     PetResource petResource;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response readAll() {
+        // TODO
+        return Response
+                .status(Response.Status.OK)
+                .build();
+    }
 }
