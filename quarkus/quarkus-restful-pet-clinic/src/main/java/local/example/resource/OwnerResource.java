4b495d92c50f3934ee4b395b42d495d2d22d0f82
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-21 09:28:34 +0200

    Update OwnerResource.java

--- quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/OwnerResource.java
+++ quarkus/quarkus-restful-pet-clinic/src/main/java/local/example/resource/OwnerResource.java
@@ -18,19 +18,20 @@
 
 package local.example.resource;
 
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
 import io.quarkus.rest.data.panache.MethodProperties;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
 import local.example.model.Owner;
+import local.example.repository.OwnerRepository;
 
 import java.util.List;
 
 @ResourceProperties(hal = true)
 public interface OwnerResource
-        extends PanacheEntityResource<Owner, Long> {
+        extends PanacheRepositoryResource<OwnerRepository, Owner, Long> {
 
     @MethodProperties(path = "all")
     List<Owner> list(Page page, Sort sort);
