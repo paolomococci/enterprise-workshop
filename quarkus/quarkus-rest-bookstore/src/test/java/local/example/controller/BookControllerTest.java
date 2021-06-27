9db100569e6bb9fac4cab1eb1b032671d8785fdd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-06-27 16:31:01 +0200

    Update BookControllerTest.java

--- quarkus/quarkus-rest-bookstore/src/test/java/local/example/controller/BookControllerTest.java
+++ quarkus/quarkus-rest-bookstore/src/test/java/local/example/controller/BookControllerTest.java
@@ -19,6 +19,7 @@
 package local.example.controller;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import javax.ws.rs.core.MediaType;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
@@ -58,7 +60,7 @@ public class BookControllerTest {
 
     @Test
     @Order(value = 3)
-    public void testReadBookResourceEndpointWithNonExistentIdentifier() {
+    public void testReadEndpointWithNonExistentIdentifier() {
         given()
                 .when()
                 .get("/books/100")
@@ -69,11 +71,21 @@ public class BookControllerTest {
     @Test
     @Order(value = 4)
     public void testReadEndpoint() {
+        String id;
+        Response response;
+        response = given()
+                .when()
+                .get("/books")
+                .then()
+                .statusCode(200)
+                .extract().response();
+        id = response.jsonPath().getString("id[0]");
         given()
                 .when()
-                .get("/books/1")
+                .get("/books/"+id)
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("code", containsString("32000004"));
     }
 
     @Test
