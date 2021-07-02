bd875572ac03eb2853143b72f514998556cf0303
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 21:45:06 +0200

    Update TaskRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/TaskRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/TaskRestfulController.kt
@@ -21,8 +21,12 @@ package local.example.staff.controller
 import local.example.staff.assembler.TaskRepresentationModelAssembler
 import local.example.staff.repository.TaskRepository
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.EntityModel
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URISyntaxException
 
 @RestController
 @RequestMapping("/api/tasks")
