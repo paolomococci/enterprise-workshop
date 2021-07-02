f2b07a1c839aedeaf9b430ca8adf88f86c14542b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 04:47:37 +0200

    Update EmployeeRestfulControllerTests.kt

--- spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/EmployeeRestfulControllerTests.kt
+++ spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/EmployeeRestfulControllerTests.kt
@@ -18,5 +18,15 @@
 
 package local.example.staff.controller
 
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class EmployeeRestfulControllerTests {
+
+    @Test
+    fun voidTest() {
+    }
 }
