27c3df1e942f898e8c6cadb5325b6d0e75d1e292
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 18:59:58 +0200

    Update EmployeeRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
@@ -18,5 +18,10 @@
 
 package local.example.staff.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/employees")
 class EmployeeRestfulController {
 }
