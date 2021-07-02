1b706a96294cf3a1dc98eb207c41392dcdf31834
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 22:18:41 +0200

    Update TaskRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/TaskRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/TaskRestfulController.kt
@@ -65,4 +65,10 @@ class TaskRestfulController(
     internal fun readAll(): CollectionModel<EntityModel<TaskEntity>> {
         return CollectionModel.empty()
     }
+
+    @PutMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun update(@RequestBody update: TaskEntity, @PathVariable id: Long?): ResponseEntity<*> {
+        return ResponseEntity.ok(HttpStatus.NOT_IMPLEMENTED)
+    }
 }
