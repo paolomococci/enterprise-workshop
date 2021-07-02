abc7d29daae3c2d2a1dbd7651d922d5b012d85c1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 22:21:16 +0200

    Update EmployeeRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
@@ -77,4 +77,10 @@ class EmployeeRestfulController(
     internal fun partialUpdate(@RequestBody update: EmployeeEntity, @PathVariable id: Long?): ResponseEntity<*> {
         return ResponseEntity.ok(HttpStatus.NOT_IMPLEMENTED)
     }
+
+    @DeleteMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun delete(@PathVariable id: Long?): ResponseEntity<*> {
+        return ResponseEntity.ok(HttpStatus.NOT_IMPLEMENTED)
+    }
 }
