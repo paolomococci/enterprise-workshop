0cdfa163a719ed33e17155a2d28389388376486c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 04:46:47 +0200

    Update JobRepositoryTests.kt

--- spring-boot/staff-handler/src/test/kotlin/local/example/staff/repository/JobRepositoryTests.kt
+++ spring-boot/staff-handler/src/test/kotlin/local/example/staff/repository/JobRepositoryTests.kt
@@ -18,5 +18,16 @@
 
 package local.example.staff.repository
 
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class JobRepositoryTests {
+
+    @Test
+    fun voidTest() {
+    }
 }
