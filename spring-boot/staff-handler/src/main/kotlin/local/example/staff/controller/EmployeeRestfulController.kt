0810f3d008a1eaf704ff128d0631b52eaee7b8ff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 21:44:23 +0200

    Update EmployeeRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/EmployeeRestfulController.kt
@@ -21,8 +21,12 @@ package local.example.staff.controller
 import local.example.staff.assembler.EmployeeRepresentationModelAssembler
 import local.example.staff.repository.EmployeeRepository
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.EntityModel
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URISyntaxException
 
 @RestController
 @RequestMapping("/api/employees")
