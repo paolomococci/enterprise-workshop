7aa6a6ae2c084b4a9a9a02947bf7eccec7bdbf95
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 19:31:26 +0200

    Update JobNotFoundAdvice.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/advice/JobNotFoundAdvice.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/advice/JobNotFoundAdvice.kt
@@ -18,8 +18,20 @@
 
 package local.example.staff.advice
 
+import local.example.staff.exception.JobNotFoundException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class JobNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(JobNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun jobNotFoundHandler(jobNotFoundException: JobNotFoundException): String {
+        return jobNotFoundException.message.toString()
+    }
 }
