fc1dc3765c1a67a8287a7529dc6f918758ea9e93
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 22:33:19 +0200

    Update EmployeeRepresentationModelAssembler.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/assembler/EmployeeRepresentationModelAssembler.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/assembler/EmployeeRepresentationModelAssembler.kt
@@ -18,7 +18,7 @@
 
 package local.example.staff.assembler
 
-import local.example.staff.controller.TaskRestfulController
+import local.example.staff.controller.EmployeeRestfulController
 import local.example.staff.entity.EmployeeEntity
 
 import org.springframework.hateoas.EntityModel
@@ -28,13 +28,13 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.stereotype.Component
 
 @Component
-class EmployeeRepresentationModelAssembler : 
+class EmployeeRepresentationModelAssembler :
     RepresentationModelAssembler<EmployeeEntity, EntityModel<EmployeeEntity>> {
 
     override fun toModel(employeeEntity: EmployeeEntity): EntityModel<EmployeeEntity> {
         return EntityModel.of(employeeEntity,
-            linkTo(methodOn(TaskRestfulController::class.java).read(employeeEntity.id)).withSelfRel(),
-            linkTo(methodOn(TaskRestfulController::class.java).readAll()).withRel("tasks")
+            linkTo(methodOn(EmployeeRestfulController::class.java).read(employeeEntity.id)).withSelfRel(),
+            linkTo(methodOn(EmployeeRestfulController::class.java).readAll()).withRel("employees")
         )
     }
 }
