9598753fe57926e4e3fcc434d5a4449fe361cc18
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 19:31:55 +0200

    Update EmployeeNotFoundAdvice.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/advice/EmployeeNotFoundAdvice.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/advice/EmployeeNotFoundAdvice.kt
@@ -18,8 +18,20 @@
 
 package local.example.staff.advice
 
+import local.example.staff.exception.EmployeeNotFoundException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class EmployeeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(EmployeeNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun employeeNotFoundHandler(employeeNotFoundException: EmployeeNotFoundException): String {
+        return employeeNotFoundException.message.toString()
+    }
 }
