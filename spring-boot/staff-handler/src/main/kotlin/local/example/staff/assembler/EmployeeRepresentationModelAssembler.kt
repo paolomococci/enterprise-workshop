ebbd5cdbfa781af58707439b1c87a9de3df6a896
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-02 21:00:18 +0200

    Update EmployeeRepresentationModelAssembler.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/assembler/EmployeeRepresentationModelAssembler.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/assembler/EmployeeRepresentationModelAssembler.kt
@@ -21,5 +21,5 @@ package local.example.staff.assembler
 import org.springframework.stereotype.Component
 
 @Component
-class EmployeeResourceAssembler {
+class EmployeeRepresentationModelAssembler {
 }
