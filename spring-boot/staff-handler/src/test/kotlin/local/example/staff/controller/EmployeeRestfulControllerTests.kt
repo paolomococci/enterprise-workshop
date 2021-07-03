9f7f58f1ea72ab442b076f8e119f7b8a79806a07
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-03 20:58:55 +0200

    Update EmployeeRestfulControllerTests.kt

--- spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/EmployeeRestfulControllerTests.kt
+++ spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/EmployeeRestfulControllerTests.kt
@@ -18,13 +18,24 @@
 
 package local.example.staff.controller
 
+import local.example.staff.repository.EmployeeRepository
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class EmployeeRestfulControllerTests {
+
+    @Autowired
+    private val mockMvc: MockMvc? = null
+
+    @Autowired
+    private val employeeRepository: EmployeeRepository? = null
+
+    private val employee: String = "{\"name\":\"John\",\"surname\":\"Doe\"}"
 
     @Test
     fun voidTest() {
