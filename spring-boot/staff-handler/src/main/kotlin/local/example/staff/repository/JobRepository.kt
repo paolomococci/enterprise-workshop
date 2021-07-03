b0b6fd9f4e0e25018943ee55f6e4b3c9bb18235b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-03 18:52:12 +0200

    Update JobRepository.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/repository/JobRepository.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/repository/JobRepository.kt
@@ -23,7 +23,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "jobs")
 interface JobRepository : CrudRepository<JobEntity, Long> {
 
     fun findByCode(@Param("code") code: String): List<JobEntity>
