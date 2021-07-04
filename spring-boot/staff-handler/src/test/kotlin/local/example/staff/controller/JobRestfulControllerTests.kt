9c22dcae8e047b320353cc16db8fe34512918abd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-04 18:11:36 +0200

    Update JobRestfulControllerTests.kt

--- spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/JobRestfulControllerTests.kt
+++ spring-boot/staff-handler/src/test/kotlin/local/example/staff/controller/JobRestfulControllerTests.kt
@@ -19,6 +19,7 @@
 package local.example.staff.controller
 
 import local.example.staff.repository.JobRepository
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
@@ -55,5 +56,61 @@ class JobRestfulControllerTests {
                 .exists())
             .andExpect(MockMvcResultMatchers.jsonPath("$._links.self.href")
                 .value("http://localhost/api/jobs"))
+    }
+
+    @Test
+    @Order(2)
+    @Throws(Exception::class)
+    fun `create test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(3)
+    @Throws(Exception::class)
+    fun `read test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(4)
+    @Throws(Exception::class)
+    fun `update test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(5)
+    @Throws(Exception::class)
+    fun `partial update test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(6)
+    @Throws(Exception::class)
+    fun `delete test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(7)
+    @Throws(Exception::class)
+    fun `find by path id test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(8)
+    @Throws(Exception::class)
+    fun `find by surname test`() {
+        // TODO
+    }
+
+    @Test
+    @Order(9)
+    @Throws(Exception::class)
+    fun `find by name test`() {
+        // TODO
     }
 }
