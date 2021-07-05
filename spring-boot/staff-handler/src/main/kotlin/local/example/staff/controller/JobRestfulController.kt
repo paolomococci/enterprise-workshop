e306323f37aa2277f2902b637c236f271fc8025e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-05 16:21:26 +0200

    Update JobRestfulController.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/JobRestfulController.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/controller/JobRestfulController.kt
@@ -30,6 +30,7 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+
 import java.net.URISyntaxException
 
 @RestController
