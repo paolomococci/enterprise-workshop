204253b22dba91d29be95c938c6efa725398d226
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-05 15:32:39 +0200

    Update EmployeeRepository.kt

--- spring-boot/staff-handler/src/main/kotlin/local/example/staff/repository/EmployeeRepository.kt
+++ spring-boot/staff-handler/src/main/kotlin/local/example/staff/repository/EmployeeRepository.kt
@@ -19,13 +19,27 @@
 package local.example.staff.repository
 
 import local.example.staff.entity.EmployeeEntity
+
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.transaction.annotation.Transactional
 
 @RepositoryRestResource(path = "employees", collectionResourceRel = "employees")
 interface EmployeeRepository : CrudRepository<EmployeeEntity, Long> {
 
     fun findByName(@Param("name") name: String): List<EmployeeEntity>
     fun findBySurname(@Param("surname") surname: String): List<EmployeeEntity>
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "UPDATE EMPLOYEES SET TASK_ID = :taskId WHERE ID = :employeeId")
+    fun assignToTask(@Param("taskId") taskId: Long, @Param("employeeId") employeeId: Long)
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "UPDATE EMPLOYEES SET TASK_ID = NULL WHERE ID = :employeeId")
+    fun meltAssignmentToTask(@Param("employeeId") employeeId: Long)
 }
