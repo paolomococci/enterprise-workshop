f220931abad0538a4ba242c4c85e81303498f926
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-08 18:05:44 +0200

    Update AddressDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
@@ -25,6 +25,7 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -46,6 +47,13 @@ public class AddressDetailView
 
     private final String ADDRESS_EDIT_ROUTE_TEMPLATE = "address-detail/%d/edit";
 
+    @Autowired
+    AddressEntity addressEntity;
+
+    private final AddressService addressService;
+
+    private final BeanValidationBinder<AddressEntity> addressEntityBeanValidationBinder;
+
     @Id("grid")
     private Grid<AddressEntity> addressEntityGrid;
 
@@ -55,6 +63,11 @@ public class AddressDetailView
     private Button cancel;
 
     public AddressDetailView(@Autowired AddressService addressService) {
+
+        this.addressService = addressService;
+
+        addressEntityBeanValidationBinder = new BeanValidationBinder<>(AddressEntity.class);
+
 
         this.cancel.addClickListener(e -> {
             // TODO
