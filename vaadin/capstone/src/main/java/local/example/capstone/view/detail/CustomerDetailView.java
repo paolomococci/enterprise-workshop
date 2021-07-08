baf47d33400830c23ce3aa53393b24853e508895
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-08 17:13:49 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -28,12 +28,15 @@ import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 import local.example.capstone.data.entity.CustomerEntity;
 import local.example.capstone.data.service.CustomerService;
+import local.example.capstone.view.MainView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Route(value = "customer-detail/:contactID?/:action?(edit)", layout = MainView.class)
 @PageTitle("Customer Detail")
 @Tag("customer-detail-view")
 @JsModule("./views/details/customer-detail-view.ts")
