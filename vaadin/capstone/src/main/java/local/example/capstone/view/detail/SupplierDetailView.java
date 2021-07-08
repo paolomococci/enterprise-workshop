463528f00251ba902b801430ed7c3debac62838e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-08 17:09:49 +0200

    Update SupplierDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
@@ -28,12 +28,15 @@ import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 import local.example.capstone.data.entity.SupplierEntity;
 import local.example.capstone.data.service.SupplierService;
+import local.example.capstone.view.MainView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Route(value = "supplier-detail/:contactID?/:action?(edit)", layout = MainView.class)
 @PageTitle("Supplier Detail")
 @Tag("supplier-detail-view")
 @JsModule("./views/details/supplier-detail-view.ts")
