c5c9d3d1057ee81218ab2b6eb7012c218bba8738
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-08 17:14:19 +0200

    Update CrewDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CrewDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CrewDetailView.java
@@ -28,12 +28,15 @@ import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 import local.example.capstone.data.entity.CrewEntity;
 import local.example.capstone.data.service.CrewService;
+import local.example.capstone.view.MainView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Route(value = "crew-detail/:contactID?/:action?(edit)", layout = MainView.class)
 @PageTitle("Crew Detail")
 @Tag("crew-detail-view")
 @JsModule("./views/details/crew-detail-view.ts")
