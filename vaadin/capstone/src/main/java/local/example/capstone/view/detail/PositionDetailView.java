13b2113a9688ed3ae053ace47b77ea256bca48d5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-09 07:16:13 +0200

    Update PositionDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
@@ -68,11 +68,11 @@ public class PositionDetailView
 
         this.positionEntityBeanValidationBinder = new BeanValidationBinder<>(PositionEntity.class);
 
-        this.cancel.addClickListener(e -> {
+        this.cancel.addClickListener(cancelButtonClickEvent -> {
             // TODO
         });
 
-        this.save.addClickListener(e -> {
+        this.save.addClickListener(saveButtonClickEvent -> {
             // TODO
         });
     }
