0638ee75f95484e276f3b38c74671b65ae1b3f90
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-09 07:15:29 +0200

    Update MachineDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
@@ -68,11 +68,11 @@ public class MachineDetailView
 
         this.machineEntityBeanValidationBinder = new BeanValidationBinder<>(MachineEntity.class);
 
-        this.cancel.addClickListener(e -> {
+        this.cancel.addClickListener(cancelButtonClickEvent -> {
             // TODO
         });
 
-        this.save.addClickListener(e -> {
+        this.save.addClickListener(saveButtonClickEvent -> {
             // TODO
         });
     }
