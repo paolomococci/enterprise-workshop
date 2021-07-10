9dffe7823f671f7f9bd3b1e0422881b20eec1477
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:16:02 +0200

    Update PositionDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
@@ -88,7 +89,15 @@ public class PositionDetailView
         this.positionEntityGrid.setHeightFull();
 
         this.positionEntityGrid.asSingleSelect().addValueChangeListener(valueChangeEvent -> {
-            // TODO
+            if (valueChangeEvent.getValue() != null) {
+                UI.getCurrent().navigate(String.format(
+                        POSITION_EDIT_ROUTE_TEMPLATE,
+                        valueChangeEvent.getValue().getId()
+                ));
+            } else {
+                this.clearForm();
+                UI.getCurrent().navigate(ContactDetailView.class);
+            }
         });
 
         this.positionEntityBeanValidationBinder = new BeanValidationBinder<>(PositionEntity.class);
