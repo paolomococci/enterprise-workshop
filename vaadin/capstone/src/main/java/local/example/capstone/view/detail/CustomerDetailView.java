d55d3a18f6be72ca6f3aa8e5f3eea940340482e9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:19:31 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
@@ -87,7 +88,15 @@ public class CustomerDetailView
         this.customerEntityGrid.setHeightFull();
 
         this.customerEntityGrid.asSingleSelect().addValueChangeListener(valueChangeEvent -> {
-            // TODO
+            if (valueChangeEvent.getValue() != null) {
+                UI.getCurrent().navigate(String.format(
+                        CUSTOMER_EDIT_ROUTE_TEMPLATE,
+                        valueChangeEvent.getValue().getId()
+                ));
+            } else {
+                this.clearForm();
+                UI.getCurrent().navigate(ContactDetailView.class);
+            }
         });
 
         this.customerEntityBeanValidationBinder = new BeanValidationBinder<>(CustomerEntity.class);
