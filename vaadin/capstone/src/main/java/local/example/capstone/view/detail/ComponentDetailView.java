6bc5db9d0e0d4e40961339bd625bb8b5fcb73092
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:10:20 +0200

    Update ComponentDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
@@ -95,7 +95,8 @@ public class ComponentDetailView
         this.componentEntityBeanValidationBinder.bindInstanceFields(this);
 
         this.cancel.addClickListener(cancelButtonClickEvent -> {
-            // TODO
+            this.clearForm();
+            this.refreshGrid();
         });
 
         this.save.addClickListener(saveButtonClickEvent -> {
