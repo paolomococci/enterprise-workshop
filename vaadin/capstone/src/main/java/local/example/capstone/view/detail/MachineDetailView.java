d35ed1b7856e5f825f2b741a3ddb504e72c5a250
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:31:07 +0200

    Update MachineDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
@@ -96,7 +96,7 @@ public class MachineDetailView
                 ));
             } else {
                 this.clearForm();
-                UI.getCurrent().navigate(ContactDetailView.class);
+                UI.getCurrent().navigate(MachineDetailView.class);
             }
         });
 
