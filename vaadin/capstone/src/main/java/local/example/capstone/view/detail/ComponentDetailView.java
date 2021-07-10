29021a50a9a717dffb0b235a22b782287409df74
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 14:38:04 +0200

    Update ComponentDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
@@ -40,7 +40,7 @@ import local.example.capstone.view.MainView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Route(value = "component-detail/:contactID?/:action?(edit)", layout = MainView.class)
+@Route(value = "component-detail/:componentID?/:action?(edit)", layout = MainView.class)
 @PageTitle("Component Detail")
 @Tag("component-detail-view")
 @JsModule("./views/details/component-detail-view.ts")
