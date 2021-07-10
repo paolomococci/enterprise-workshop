322924eee20360530caa24bbce8d13ff15c5c1a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 14:35:36 +0200

    Update ProductDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ProductDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ProductDetailView.java
@@ -40,7 +40,7 @@ import local.example.capstone.view.MainView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Route(value = "product-detail/:contactID?/:action?(edit)", layout = MainView.class)
+@Route(value = "product-detail/:productID?/:action?(edit)", layout = MainView.class)
 @PageTitle("Product Detail")
 @Tag("product-detail-view")
 @JsModule("./views/details/product-detail-view.ts")
