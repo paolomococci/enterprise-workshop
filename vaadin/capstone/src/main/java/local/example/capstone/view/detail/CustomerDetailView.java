971cb16d42353c96311322aa69e8611e17b614f5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 14:09:20 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -23,6 +23,7 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.component.textfield.TextField;
@@ -99,7 +100,8 @@ public class CustomerDetailView
     }
 
     private void refreshGrid() {
-        // TODO
+        this.customerEntityGrid.select(null);
+        this.customerEntityGrid.getLazyDataView().refreshAll();
     }
 
     private void clearForm() {
