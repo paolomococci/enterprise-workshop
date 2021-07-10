daa0344c7ea0d73099177c1debbfdc8c3f1bb99a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:07:23 +0200

    Update SupplierDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/SupplierDetailView.java
@@ -94,7 +94,8 @@ public class SupplierDetailView
         this.supplierEntityBeanValidationBinder.bindInstanceFields(this);
 
         this.cancel.addClickListener(cancelButtonClickEvent -> {
-            // TODO
+            this.clearForm();
+            this.refreshGrid();
         });
 
         this.save.addClickListener(saveButtonClickEvent -> {
