c0401adf0abb733abf110b6efa9e7a13c980811b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:21:07 +0200

    Update AddressDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
@@ -99,7 +100,15 @@ public class AddressDetailView
         this.addressEntityGrid.setHeightFull();
 
         this.addressEntityGrid.asSingleSelect().addValueChangeListener(valueChangeEvent -> {
-            // TODO
+            if (valueChangeEvent.getValue() != null) {
+                UI.getCurrent().navigate(String.format(
+                        ADDRESS_EDIT_ROUTE_TEMPLATE,
+                        valueChangeEvent.getValue().getId()
+                ));
+            } else {
+                this.clearForm();
+                UI.getCurrent().navigate(ContactDetailView.class);
+            }
         });
 
         this.addressEntityBeanValidationBinder = new BeanValidationBinder<>(AddressEntity.class);
