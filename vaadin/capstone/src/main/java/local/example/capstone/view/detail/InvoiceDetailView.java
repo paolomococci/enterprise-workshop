2e0ba3a340d25be35e1c608dec11062dd0ac3894
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-10 15:18:49 +0200

    Update InvoiceDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/InvoiceDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/InvoiceDetailView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
@@ -88,7 +89,15 @@ public class InvoiceDetailView
         this.invoiceEntityGrid.setHeightFull();
 
         this.invoiceEntityGrid.asSingleSelect().addValueChangeListener(valueChangeEvent -> {
-            // TODO
+            if (valueChangeEvent.getValue() != null) {
+                UI.getCurrent().navigate(String.format(
+                        INVOICE_EDIT_ROUTE_TEMPLATE,
+                        valueChangeEvent.getValue().getId()
+                ));
+            } else {
+                this.clearForm();
+                UI.getCurrent().navigate(ContactDetailView.class);
+            }
         });
 
         this.invoiceEntityBeanValidationBinder = new BeanValidationBinder<>(InvoiceEntity.class);
