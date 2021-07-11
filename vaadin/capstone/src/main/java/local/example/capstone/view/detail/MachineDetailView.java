96b1f19c1c66dc760093d62e52184c08f3309fe9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-11 10:38:18 +0200

    Update MachineDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/MachineDetailView.java
@@ -81,8 +81,8 @@ public class MachineDetailView
 
         this.machineService = machineService;
 
-        this.machineEntityGrid.addColumn((String) null).setHeader((String) null).setAutoWidth(true);
-        this.machineEntityGrid.addColumn((String) null).setHeader((String) null).setAutoWidth(true);
+        this.machineEntityGrid.addColumn(MachineEntity::getLabel).setHeader((String) null).setAutoWidth(true);
+        this.machineEntityGrid.addColumn(MachineEntity::getCapacity).setHeader((String) null).setAutoWidth(true);
 
         this.machineEntityGrid.setItems(this.machineService.readAll());
 
