0f74655aeefdcb61396b3aff7a8b3b101769fbb5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-11 10:37:19 +0200

    Update PositionDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/PositionDetailView.java
@@ -81,8 +81,8 @@ public class PositionDetailView
 
         this.positionService = positionService;
 
-        this.positionEntityGrid.addColumn((String) null).setHeader((String) null).setAutoWidth(true);
-        this.positionEntityGrid.addColumn((String) null).setHeader((String) null).setAutoWidth(true);
+        this.positionEntityGrid.addColumn(PositionEntity::getLabel).setHeader((String) null).setAutoWidth(true);
+        this.positionEntityGrid.addColumn(PositionEntity::getCapacity).setHeader((String) null).setAutoWidth(true);
 
         this.positionEntityGrid.setItems(this.positionService.readAll());
 
