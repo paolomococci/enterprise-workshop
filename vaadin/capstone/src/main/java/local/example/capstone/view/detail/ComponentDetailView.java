7d91b6e84a0c17faddeec893e381b957df0d373e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-11 10:56:51 +0200

    Update ComponentDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
@@ -81,8 +81,8 @@ public class ComponentDetailView
 
         this.componentService = componentService;
 
-        this.componentEntityGrid.addColumn(ComponentEntity::getCode).setHeader((String) null).setAutoWidth(true);
-        this.componentEntityGrid.addColumn(ComponentEntity::getAmount).setHeader((String) null).setAutoWidth(true);
+        this.componentEntityGrid.addColumn(ComponentEntity::getCode).setHeader("Code").setAutoWidth(true);
+        this.componentEntityGrid.addColumn(ComponentEntity::getAmount).setHeader("Amount").setAutoWidth(true);
 
         this.componentEntityGrid.setItems(this.componentService.readAll());
 
