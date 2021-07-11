e2f30b46b14f9ac5f98c78142cdacad63775a47e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-11 10:56:23 +0200

    Update CrewDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CrewDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CrewDetailView.java
@@ -80,8 +80,8 @@ public class CrewDetailView
 
         this.crewService = crewService;
 
-        this.crewEntityGrid.addColumn(CrewEntity::getCode).setHeader((String) null).setAutoWidth(true);
-        this.crewEntityGrid.addColumn(CrewEntity::getName).setHeader((String) null).setAutoWidth(true);
+        this.crewEntityGrid.addColumn(CrewEntity::getCode).setHeader("Code").setAutoWidth(true);
+        this.crewEntityGrid.addColumn(CrewEntity::getName).setHeader("Name").setAutoWidth(true);
 
         this.crewEntityGrid.setItems(this.crewService.readAll());
 
