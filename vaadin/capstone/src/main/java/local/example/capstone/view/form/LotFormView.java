5f834a967644010213105e0bc963a845799eb59e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-12 10:55:51 +0200

    Update LotFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/LotFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/LotFormView.java
@@ -22,11 +22,17 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import local.example.capstone.data.entity.LotEntity;
+import local.example.capstone.data.service.LotService;
 import local.example.capstone.view.MainView;
 
 @Route(value = "lot-form", layout = MainView.class)
@@ -46,4 +52,21 @@ public class LotFormView
     private Button save;
     @Id("cancel")
     private Button cancel;
+
+    private Binder<LotEntity> lotEntityBinder = new BeanValidationBinder<>(LotEntity.class);
+
+    public LotFormView(LotService lotService) {
+        this.lotEntityBinder.bindInstanceFields(this);
+        this.clearForm();
+        this.cancel.addClickListener(buttonClickEvent -> this.clearForm());
+        this.save.addClickListener(buttonClickEvent -> {
+            lotService.create(this.lotEntityBinder.getBean());
+            Notification.show("added an item " + this.lotEntityBinder.getBean().getClass().getSimpleName());
+            this.clearForm();
+        });
+    }
+
+    private void clearForm() {
+        this.lotEntityBinder.setBean(new LotEntity());
+    }
 }
