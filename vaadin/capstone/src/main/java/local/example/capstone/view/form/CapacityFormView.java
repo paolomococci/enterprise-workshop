454aaa8608fcfe6689afd2a032204e6bbe1aeb4f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-12 10:40:02 +0200

    Update CapacityFormView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/form/CapacityFormView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/form/CapacityFormView.java
@@ -19,8 +19,12 @@
 package local.example.capstone.view.form;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import local.example.capstone.view.MainView;
@@ -31,4 +35,18 @@ import local.example.capstone.view.MainView;
 @JsModule("./views/forms/capacity-form-view.ts")
 public class CapacityFormView
         extends LitTemplate {
+
+    @Id("componentCode")
+    private TextField code;
+
+    @Id("usefulWeight")
+    private IntegerField usefulWeight;
+
+    @Id("usefulVolume")
+    private IntegerField usefulVolume;
+
+    @Id("save")
+    private Button save;
+    @Id("cancel")
+    private Button cancel;
 }
