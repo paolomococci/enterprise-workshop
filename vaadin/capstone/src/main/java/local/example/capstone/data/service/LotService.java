1b5e945e7c86226e7de0c10101d89c09c331296f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-12 08:55:22 +0200

    Update LotService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/LotService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/LotService.java
@@ -27,4 +27,8 @@ public class LotService {
 
     @Autowired
     LotRepository lotRepository;
+
+    public List<LotEntity> readAll() {
+        return (List<LotEntity>) lotRepository.findAll();
+    }
 }
