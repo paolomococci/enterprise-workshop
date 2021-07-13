c0093dbd32bb5abc29414edc04a6ad2eb4bab19a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-13 08:03:11 +0200

    Update lot-form-view.ts

--- vaadin/capstone/frontend/views/forms/lot-form-view.ts
+++ vaadin/capstone/frontend/views/forms/lot-form-view.ts
@@ -1,20 +1,22 @@
 import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-text-field/vaadin-integer-field';
+import '@vaadin/vaadin-date-picker';
 import '@vaadin/vaadin-form-layout';
 import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('lot-form-view')
-export class AddressFormView extends LitElement {
+export class LotFormView extends LitElement {
   createRenderRoot() {
     return this;
   }
   render() {
     return html`<h3>lot</h3>
       <vaadin-form-layout style="width: 100%;">
-        <vaadin-text-field label="code" id="code" pattern="[0-9]{0,6}" prevent-invalid-input colspan="2"></vaadin-text-field>
+        <vaadin-text-field label="code" id="lotCode" pattern="[0-9]{0,6}" prevent-invalid-input colspan="2"></vaadin-text-field>
         <vaadin-integer-field label="amount" id="amount" value="0" min="0"></vaadin-integer-field>
+        <vaadin-date-picker label="deadline" id="deadline"></vaadin-date-picker>
       </vaadin-form-layout>
       <vaadin-horizontal-layout
         style="margin-top: var(--lumo-space-m); margin-bottom: var(--lumo-space-l);"
