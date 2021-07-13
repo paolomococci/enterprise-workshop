315178dbcd92c746009d067f18871dcdc87d42a3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-13 08:02:30 +0200

    Update capacity-form-view.ts

--- vaadin/capstone/frontend/views/forms/capacity-form-view.ts
+++ vaadin/capstone/frontend/views/forms/capacity-form-view.ts
@@ -6,14 +6,14 @@ import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { customElement, html, LitElement } from 'lit-element';
 
 @customElement('capacity-form-view')
-export class AddressFormView extends LitElement {
+export class CapacityFormView extends LitElement {
   createRenderRoot() {
     return this;
   }
   render() {
     return html`<h3>capacity</h3>
       <vaadin-form-layout style="width: 100%;">
-        <vaadin-text-field label="code" id="code" pattern="[0-9]{0,6}" prevent-invalid-input colspan="2"></vaadin-text-field>
+        <vaadin-text-field label="code" id="capacityCode" pattern="[0-9]{0,6}" prevent-invalid-input colspan="2"></vaadin-text-field>
         <vaadin-integer-field label="useful weight" id="usefulWeight" value="0" min="0"></vaadin-integer-field>
         <vaadin-integer-field label="useful volume" id="usefulVolume" value="0" min="0"></vaadin-integer-field>
       </vaadin-form-layout>
