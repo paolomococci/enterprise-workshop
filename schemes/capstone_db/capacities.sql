18db148b9386c72d9c075b332c9202dca25ab535
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-13 06:51:16 +0200

    Create capacities.sql

---
+++ schemes/capstone_db/capacities.sql
@@ -0,0 +1,10 @@
+CREATE TABLE capstone_db.capacities (
+  `id` bigint(20) NOT NULL,
+  `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `useful_volume` int(11) DEFAULT NULL,
+  `useful_weight` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
