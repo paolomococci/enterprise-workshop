f4320dd79acfd5f3767866c3725a3852234cc7ba
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-14 08:19:53 +0200

    Update CapacityDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CapacityDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CapacityDetailView.java
@@ -22,11 +22,14 @@ import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
+
+import local.example.capstone.data.entity.CapacityEntity;
 
 @PageTitle("Capacity Detail")
 @Tag("capacity-detail-view")
@@ -34,6 +37,9 @@ import com.vaadin.flow.router.PageTitle;
 public class CapacityDetailView
         extends LitTemplate
         implements HasStyle, BeforeEnterObserver {
+
+    @Id("grid")
+    private Grid<CapacityEntity> capacityEntityGrid;
 
     @Id("save")
     private Button save;
