1b442c0c31d4d4e42e5b9b1430e56e7e53a79f2c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-14 10:08:45 +0200

    Update CapacityDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CapacityDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CapacityDetailView.java
@@ -20,6 +20,7 @@ package local.example.capstone.view.detail;
 
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
@@ -85,10 +86,30 @@ public class CapacityDetailView
         this.capacityEntityGrid.setHeightFull();
 
         this.capacityEntityGrid.asSingleSelect().addValueChangeListener(valueChangeEvent -> {
-
+            if (valueChangeEvent.getValue() != null) {
+                UI.getCurrent().navigate(String.format(
+                        CAPACITY_EDIT_ROUTE_TEMPLATE,
+                        valueChangeEvent.getValue().getId()
+                ));
+            } else {
+                this.clearForm();
+                UI.getCurrent().navigate(CapacityDetailView.class);
+            }
         });
 
         this.capacityEntityBeanValidationBinder = null;
+    }
+
+    private void refreshGrid() {
+
+    }
+
+    private void clearForm() {
+
+    }
+
+    private void populateForm() {
+
     }
 
     @Override
