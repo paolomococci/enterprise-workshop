1467ad5dcc7042fab45261eea67d62300c1eb27e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-17 20:44:44 +0200

    Update CustomerDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/CustomerDetailView.java
@@ -152,7 +152,7 @@ public class CustomerDetailView
     public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
         Optional<Long> customerId = beforeEnterEvent.getRouteParameters().getLong(CUSTOMER_ID);
         if (customerId.isPresent()) {
-            Optional<CustomerEntity> optionalCustomerEntity = customerService.read(CUSTOMER_ID);
+            Optional<CustomerEntity> optionalCustomerEntity = customerService.read(String.valueOf(customerId));
             if (optionalCustomerEntity.isPresent()) {
                 this.populateForm(optionalCustomerEntity.get());
             } else {
