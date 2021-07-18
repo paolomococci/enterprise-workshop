1d812c7615ffca3cadbd4aaf73931cb986aebeab
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-18 09:28:57 +0200

    Update PositionService.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/service/PositionService.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/service/PositionService.java
@@ -22,6 +22,7 @@ import local.example.capstone.data.entity.PositionEntity;
 import local.example.capstone.data.repository.PositionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,6 +35,10 @@ public class PositionService {
     PositionRepository positionRepository;
 
     public List<PositionEntity> readAll() {
+        return (List<PositionEntity>) positionRepository.findAll();
+    }
+
+    public List<PositionEntity> readAll(PageRequest pageRequest) {
         return (List<PositionEntity>) positionRepository.findAll();
     }
 
