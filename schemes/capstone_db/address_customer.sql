2cbbd2d06ccfb6bb2a474fd0e3f51c6b5179d1c4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:25:26 +0200

    Create address_customer.sql

---
+++ schemes/capstone_db/address_customer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE capstone_db.address_customer (
+  `address_id` bigint(20) NOT NULL,
+  `customer_id` bigint(20) NOT NULL,
+  KEY `FKfuxkjh2xs60fcuwvfyfddyr9i` (`customer_id`),
+  KEY `FK9acspv6ike5vy03imppenv7qw` (`address_id`),
+  CONSTRAINT `FK9acspv6ike5vy03imppenv7qw` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `FKfuxkjh2xs60fcuwvfyfddyr9i` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
