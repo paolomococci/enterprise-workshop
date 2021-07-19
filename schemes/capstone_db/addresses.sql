5ae67da7dec578cc27a5dfdb8ffa2a7a9be5fc38
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:15:00 +0200

    Update addresses.sql

--- schemes/capstone_db/addresses.sql
+++ schemes/capstone_db/addresses.sql
@@ -1,11 +1,11 @@
 CREATE TABLE capstone_db.addresses (
   `id` bigint(20) NOT NULL,
-  `updated` datetime(6) DEFAULT NULL,
+  `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
   `city` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
   `postal_code` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   `street` varchar(255) DEFAULT NULL,
-  `created` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
