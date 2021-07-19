e536f9353a661758d0850d11ac14ea95d43caed3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:28:43 +0200

    Create contact_supplier.sql

---
+++ schemes/capstone_db/contact_supplier.sql
@@ -0,0 +1,8 @@
+CREATE TABLE capstone_db.contact_supplier (
+  `contact_id` bigint(20) NOT NULL,
+  `supplier_id` bigint(20) NOT NULL,
+  KEY `FKxv6gsrxvp4bngprg4vp8yf0n` (`supplier_id`),
+  KEY `FKefmga20ed9na758iscwyaqd7k` (`contact_id`),
+  CONSTRAINT `FKefmga20ed9na758iscwyaqd7k` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`id`),
+  CONSTRAINT `FKxv6gsrxvp4bngprg4vp8yf0n` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
