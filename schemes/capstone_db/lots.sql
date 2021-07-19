901afe22d232a6e16286fe6d20ddd0e143cd5564
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:19:36 +0200

    Update lots.sql

--- schemes/capstone_db/lots.sql
+++ schemes/capstone_db/lots.sql
@@ -5,5 +5,11 @@ CREATE TABLE capstone_db.lots (
   `created` datetime(6) NOT NULL,
   `updated` datetime(6) NOT NULL,
   `deadline` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `lot_component_fk` bigint(20) DEFAULT NULL,
+  `lot_product_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKn846hjdbebuc5yttac1417bbs` (`lot_component_fk`),
+  KEY `FK77b6m3qkfagasqxs72gcs7er5` (`lot_product_fk`),
+  CONSTRAINT `FK77b6m3qkfagasqxs72gcs7er5` FOREIGN KEY (`lot_product_fk`) REFERENCES `products` (`id`),
+  CONSTRAINT `FKn846hjdbebuc5yttac1417bbs` FOREIGN KEY (`lot_component_fk`) REFERENCES `components` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
