636cfd06ec3dfeee9a1e2bf73a76f4e6b1ad7291
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:17:29 +0200

    Update contacts.sql

--- schemes/capstone_db/contacts.sql
+++ schemes/capstone_db/contacts.sql
@@ -1,13 +1,13 @@
 CREATE TABLE capstone_db.contacts (
   `id` bigint(20) NOT NULL,
-  `updated` datetime(6) DEFAULT NULL,
-  `date_of_birth` date DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `created` datetime(6) NOT NULL,
+  `date_of_birth` date DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `role` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) NOT NULL,
   `contributory_identifier` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `phone_mobile_number` varchar(255) DEFAULT NULL,
   `profession` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
