ea7d48ee13bd69e4f2b90b179f5364ce0efbefb6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:18:59 +0200

    Update invoices.sql

--- schemes/capstone_db/invoices.sql
+++ schemes/capstone_db/invoices.sql
@@ -1,6 +1,8 @@
 CREATE TABLE capstone_db.invoices (
   `id` bigint(20) NOT NULL,
-  `updated` datetime(6) DEFAULT NULL,
   `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `total` double DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
