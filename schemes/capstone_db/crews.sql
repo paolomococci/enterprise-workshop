51344e4a99d8d003c0302b7694551ae43d829fc3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:18:00 +0200

    Update crews.sql

--- schemes/capstone_db/crews.sql
+++ schemes/capstone_db/crews.sql
@@ -1,6 +1,11 @@
 CREATE TABLE capstone_db.crews (
   `id` bigint(20) NOT NULL,
-  `updated` datetime(6) DEFAULT NULL,
   `created` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`)
+  `updated` datetime(6) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `crew_machine_fk` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK27lkeb4yaa3y3h1isdbb7roub` (`crew_machine_fk`),
+  CONSTRAINT `FK27lkeb4yaa3y3h1isdbb7roub` FOREIGN KEY (`crew_machine_fk`) REFERENCES `machines` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
