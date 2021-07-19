ba34d44badc7dd576cabf1ce206c2634861ee55f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:26:17 +0200

    Create address_supplier.sql

---
+++ schemes/capstone_db/address_supplier.sql
@@ -0,0 +1,8 @@
+CREATE TABLE capstone_db.address_supplier (
+  `address_id` bigint(20) NOT NULL,
+  `supplier_id` bigint(20) NOT NULL,
+  KEY `FKacurc6548mehr1jv4kbjre9dl` (`supplier_id`),
+  KEY `FKehbhwy1cmtftoomkt0vkh2gln` (`address_id`),
+  CONSTRAINT `FKacurc6548mehr1jv4kbjre9dl` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`),
+  CONSTRAINT `FKehbhwy1cmtftoomkt0vkh2gln` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
