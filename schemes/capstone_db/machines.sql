aa811af0309c7813851af0bc6bb85a9b6f59726e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-20 00:20:06 +0200

    Update machines.sql

--- schemes/capstone_db/machines.sql
+++ schemes/capstone_db/machines.sql
@@ -1,8 +1,8 @@
 CREATE TABLE capstone_db.machines (
   `id` bigint(20) NOT NULL,
   `capacity` int(11) DEFAULT NULL,
-  `label` varchar(255) DEFAULT NULL,
-  `updated` datetime(6) DEFAULT NULL,
   `created` datetime(6) NOT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
