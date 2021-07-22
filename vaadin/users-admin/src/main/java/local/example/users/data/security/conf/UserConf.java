1c5b7be67726ce5c0d919993c0dd5fc161778b56
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-22 21:52:17 +0200

    Update UserConf.java

--- vaadin/users-admin/src/main/java/local/example/users/data/security/conf/UserConf.java
+++ vaadin/users-admin/src/main/java/local/example/users/data/security/conf/UserConf.java
@@ -18,8 +18,41 @@
 
 package local.example.users.data.security.conf;
 
-import org.springframework.context.annotation.Configuration;
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
 @Configuration
-public class UserConf {
+public class UserConf
+        extends VaadinWebSecurityConfigurerAdapter {
+
+    public static final String LOGOUT_URL = "/";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity)
+            throws Exception {
+        super.configure(webSecurity);
+        webSecurity.ignoring().antMatchers("/images/logo.png");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/hello").permitAll();
+        super.configure(httpSecurity);
+        this.setLoginView(httpSecurity, "/login", LOGOUT_URL);
+    }
 }
