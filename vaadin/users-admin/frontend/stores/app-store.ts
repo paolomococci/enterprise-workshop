d46d0b184607f6a7cbe4305e160671b633d50c7e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-23 18:15:57 +0200

    Update app-store.ts

--- vaadin/users-admin/frontend/stores/app-store.ts
+++ vaadin/users-admin/frontend/stores/app-store.ts
@@ -1,1 +1,11 @@
+import { RouterLocation } from '@vaadin/router';
+import User from 'Frontend/generated/local/example/users/data/entity/User';
+import Role from 'Frontend/generated/local/example/users/data/Role';
+import { UserEndpoint } from 'Frontend/generated/UserEndpoint';
+import { makeAutoObservable } from 'mobx';
 
+export class AppStore {
+  applicationName = 'users-admin';
+  // TODO
+}
+export const appStore = new AppStore();
