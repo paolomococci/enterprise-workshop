e2b2805910d610fb92b145641b5c334869748e5a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-24 07:56:19 +0200

    Create UserConf.java

---
+++ vaadin/groupware/src/main/java/local/example/groupware/data/security/conf/UserConf.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.groupware.data.security.conf;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class UserConf
+        extends VaadinWebSecurityConfigurerAdapter {
+
+    public static final String LOGOUT_URL = "/";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity)
+            throws Exception {
+        super.configure(webSecurity);
+        webSecurity.ignoring().antMatchers("/images/logo.png");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/hello").permitAll();
+        super.configure(httpSecurity);
+        this.setLoginView(httpSecurity, "/login", LOGOUT_URL);
+    }
+}
