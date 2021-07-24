19196790d336501d3522fba2dc26fcf6747ea520
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-24 07:56:53 +0200

    Create UserDetail.java

---
+++ vaadin/groupware/src/main/java/local/example/groupware/data/security/UserDetail.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.groupware.data.security;
+
+import local.example.groupware.data.entity.User;
+import local.example.groupware.data.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetail
+        implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("there is no user with username: " + username);
+        } else {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getHashedPassword(),
+                    getAuthorities(user)
+            );
+        }
+    }
+
+    private static List<GrantedAuthority> getAuthorities(User user) {
+        return user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
+                .collect(Collectors.toList());
+    }
+}
