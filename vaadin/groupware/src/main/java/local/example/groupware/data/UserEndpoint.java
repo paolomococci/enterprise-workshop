1360f6a44a34155a488ef7454206acb96db658ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-24 07:42:52 +0200

    Create UserEndpoint.java

---
+++ vaadin/groupware/src/main/java/local/example/groupware/data/UserEndpoint.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.groupware.data.endpoint;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.server.connect.Endpoint;
+
+import local.example.groupware.data.entity.User;
+import local.example.groupware.data.security.auth.UserAuth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+@Endpoint
+@AnonymousAllowed
+public class UserEndpoint {
+
+    @Autowired
+    private UserAuth userAuth;
+
+    public Optional<User> getOptionalUserAuth() {
+        return this.userAuth.get();
+    }
+}
