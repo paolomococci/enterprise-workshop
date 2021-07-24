8d7e2eb74728d897535847b92bbdd0cc7daa533b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-24 07:41:41 +0200

    Create user_roles.sql

---
+++ schemes/groupware_db/user_roles.sql
@@ -0,0 +1,6 @@
+CREATE TABLE groupware_db.user_roles (
+  `user_id` bigint(20) NOT NULL,
+  `roles` int(11) DEFAULT NULL,
+  KEY `FKhfh9dx7w3ubf1co1vdev94g3f` (`user_id`),
+  CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
