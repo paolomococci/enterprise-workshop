837e8507003f80e785cf992fc9e1331c4567e958
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-25 21:39:01 +0200

    Update LoginView.java

--- vaadin/groupware/src/main/java/local/example/groupware/view/LoginView.java
+++ vaadin/groupware/src/main/java/local/example/groupware/view/LoginView.java
@@ -18,10 +18,27 @@
 
 package local.example.groupware.view;
 
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 @PageTitle("Login")
+@Route(value = "login")
 public class LoginView
         extends LoginOverlay {
+
+    public LoginView() {
+        setAction("login");
+
+        LoginI18n loginI18n = LoginI18n.createDefault();
+        loginI18n.setHeader(new LoginI18n.Header());
+        loginI18n.getHeader().setTitle("Groupware");
+        loginI18n.getHeader().setDescription("");
+        loginI18n.setAdditionalInformation(null);
+        setI18n(loginI18n);
+
+        setForgotPasswordButtonVisible(false);
+        setOpened(true);
+    }
 }
