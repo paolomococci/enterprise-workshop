a617bece29d5b2f65b9a2ca037eca5588eb4419c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-25 21:09:59 +0200

    Update users.sql

--- schemes/groupware_db/users.sql
+++ schemes/groupware_db/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE groupware_db.users (
   `id` bigint(20) NOT NULL,
   `hashed_password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `profile_picture_url` longtext DEFAULT NULL,
+  `profile_image_url` longtext DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
