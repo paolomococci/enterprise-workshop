069b1b56a40fa4f47debb125f77bb75b2e0de8fd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-25 22:57:09 +0200

    Update Application.java

--- vaadin/users/src/main/java/local/example/user/Application.java
+++ vaadin/users/src/main/java/local/example/user/Application.java
@@ -19,19 +19,23 @@
 package local.example.user;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Push
 @SpringBootApplication
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @Theme(value = "users", variant = Lumo.DARK)
 @PWA(name = "users", shortName = "users", offlineResources = {"images/logo.png"})
-public class Application {
+public class Application
+		extends SpringBootServletInitializer
+		implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
