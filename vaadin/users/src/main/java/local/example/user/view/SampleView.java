251cdaced3d2cdfc2a4c3eb4bc0a2facb6794006
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-26 10:01:25 +0200

    Create SampleView.java

---
+++ vaadin/users/src/main/java/local/example/user/view/SampleView.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.user.view;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Sample")
+@Route(value = "sample", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+public class SampleView
+        extends Div {
+
+    public SampleView() {
+        addClassName("sample-view");
+        add(new Text("explanatory content should be inserted here"));
+    }
+}
