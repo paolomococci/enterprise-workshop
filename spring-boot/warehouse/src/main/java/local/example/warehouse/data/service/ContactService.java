1b04ff2d121bfb3ba30f338d05e2b6ad65e39700
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 14:49:55 +0200

    Create ContactService.java

---
+++ spring-boot/warehouse/src/main/java/local/example/warehouse/data/service/ContactService.java
@@ -0,0 +1,92 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.warehause.data.service;
+
+import local.example.warehause.data.entity.ContactEntity;
+import local.example.warehause.data.repository.ContactRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    public List<ContactEntity> readAll() {
+        return (List<ContactEntity>) contactRepository.findAll();
+    }
+
+    public List<ContactEntity> readAll(PageRequest pageRequest) {
+        return (List<ContactEntity>) contactRepository.findAll();
+    }
+
+    public void create(ContactEntity contactEntity) {
+        contactRepository.save(contactEntity);
+    }
+
+    public Optional<ContactEntity> read(Long id) {
+        return contactRepository.findById(id);
+    }
+
+    public void update(ContactEntity updatedContactEntity, Long id) {
+        Optional.of(contactRepository.findById(id).map(
+                storedContactEntity -> {
+                    if (updatedContactEntity.getName() != null)
+                        storedContactEntity.setName(updatedContactEntity.getName());
+                    if (updatedContactEntity.getSurname() != null)
+                        storedContactEntity.setSurname(updatedContactEntity.getSurname());
+                    if (updatedContactEntity.getEmail() != null)
+                        storedContactEntity.setEmail(updatedContactEntity.getEmail());
+                    if (updatedContactEntity.getPhoneMobileNumber() != null)
+                        storedContactEntity.setPhoneMobileNumber(updatedContactEntity.getPhoneMobileNumber());
+                    if (updatedContactEntity.getDateOfBirth() != null)
+                        storedContactEntity.setDateOfBirth(updatedContactEntity.getDateOfBirth());
+                    if (updatedContactEntity.getProfession() != null)
+                        storedContactEntity.setProfession(updatedContactEntity.getProfession());
+                    if (updatedContactEntity.getRole() != null)
+                        storedContactEntity.setRole(updatedContactEntity.getRole());
+                    return contactRepository.save(storedContactEntity);
+                }).orElseGet(
+                () -> {
+                    return contactRepository.save(updatedContactEntity);
+                }));
+    }
+
+    public void update(ContactEntity updatedContactEntity) {
+        this.update(updatedContactEntity, updatedContactEntity.getId());
+    }
+
+    public void delete(Long id) {
+        contactRepository.deleteById(id);
+    }
+
+    public void delete(ContactEntity contactEntity) {
+        try {
+            this.delete(contactEntity.getId());
+        } catch (Exception exception) {
+            exception.getMessage();
+        }
+    }
+}
