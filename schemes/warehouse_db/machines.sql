23236b1b545f279c03ade88d4f424b24f66e2d3a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 13:05:12 +0200

    Create machines.sql

---
+++ schemes/warehouse_db/machines.sql
@@ -0,0 +1,8 @@
+CREATE TABLE warehouse_db.machines (
+  `id` bigint(20) NOT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `created` datetime(6) NOT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `updated` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
