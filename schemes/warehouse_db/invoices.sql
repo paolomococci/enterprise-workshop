1a3bc6337808ea50b7030f3be45fafbe8ce2ff29
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 13:08:05 +0200

    Create invoices.sql

---
+++ schemes/warehouse_db/invoices.sql
@@ -0,0 +1,8 @@
+CREATE TABLE warehouse_db.invoices (
+  `id` bigint(20) NOT NULL,
+  `created` datetime(6) NOT NULL,
+  `updated` datetime(6) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
