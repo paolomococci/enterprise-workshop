e64d713d18155d74c4df43b46c75a190e21c68c4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 13:20:37 +0200

    Create invoice_carrier.sql

---
+++ schemes/warehouse_db/invoice_carrier.sql
@@ -0,0 +1,8 @@
+CREATE TABLE warehouse_db.invoice_carrier (
+  `invoice_id` bigint(20) NOT NULL,
+  `carrier_id` bigint(20) NOT NULL,
+  KEY `FKbyyhikt1k8mdod4ys6u5iydhh` (`carrier_id`),
+  KEY `FK4legayctg723l1qoqpqfs673b` (`invoice_id`),
+  CONSTRAINT `FK4legayctg723l1qoqpqfs673b` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`id`),
+  CONSTRAINT `FKbyyhikt1k8mdod4ys6u5iydhh` FOREIGN KEY (`carrier_id`) REFERENCES `carriers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
