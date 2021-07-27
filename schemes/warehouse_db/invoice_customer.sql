b67a8e19df2ebe50c60ef9b9d175d8e2d05b4efb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 13:13:29 +0200

    Create invoice_customer.sql

---
+++ schemes/warehouse_db/invoice_customer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE warehouse_db.invoice_customer (
+  `invoice_id` bigint(20) NOT NULL,
+  `customer_id` bigint(20) NOT NULL,
+  KEY `FKn8aktpc51hsb5ucasrily01oi` (`customer_id`),
+  KEY `FKswjwgknf61b8i8qyrwuje6an6` (`invoice_id`),
+  CONSTRAINT `FKn8aktpc51hsb5ucasrily01oi` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `FKswjwgknf61b8i8qyrwuje6an6` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
