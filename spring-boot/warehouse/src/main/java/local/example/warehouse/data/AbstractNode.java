b3034886d45f2e2f74b35dc8820ba2dcce50d0c4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 13:57:50 +0200

    Create AbstractNode.java

---
+++ spring-boot/warehouse/src/main/java/local/example/warehouse/data/AbstractNode.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.warehause.data;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@EqualsAndHashCode
+public abstract class AbstractNode {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Getter
+    @Setter
+    private String label;
+
+    @Getter
+    @Setter
+    private Integer capacity;
+
+    @Getter
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Timestamp updated;
+
+    @Getter
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Timestamp created;
+}
