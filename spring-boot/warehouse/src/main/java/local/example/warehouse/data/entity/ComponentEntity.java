cd3cb38182e2a1c823668ccc9eaf1198f8fc8aa5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 14:15:23 +0200

    Create ComponentEntity.java

---
+++ spring-boot/warehouse/src/main/java/local/example/warehouse/data/entity/ComponentEntity.java
@@ -0,0 +1,46 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.warehause.data.entity;
+
+import local.example.warehause.data.AbstractItem;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Table(name = "components")
+public class ComponentEntity
+        extends AbstractItem {
+
+    @OneToMany(mappedBy = "lotComponent", fetch = FetchType.LAZY)
+    private List<LotEntity> lots = new LinkedList<>();
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "component_product_fk")
+    private ProductEntity componentProduct;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "component_invoice_fk")
+    private InvoiceEntity componentInvoice;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "component_supplier_fk")
+    private SupplierEntity componentSupplier;
+}
