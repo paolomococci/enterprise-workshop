df391143a82b9db24c971450fa1810d31f8b5b2b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-27 14:03:19 +0200

    Create ProductEntity.java

---
+++ spring-boot/warehouse/src/main/java/local/example/warehouse/data/entity/ProductEntity.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.warehause.data.entity;
+
+import local.example.warehause.data.AbstractItem;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity
+        extends AbstractItem {
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_machine_fk")
+    private MachineEntity productMachine;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_invoice_fk")
+    private InvoiceEntity productInvoice;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_customer_fk")
+    private CustomerEntity productCustomer;
+
+    @OneToMany(mappedBy = "lotProduct", fetch = FetchType.LAZY)
+    private List<LotEntity> lots = new LinkedList<>();
+
+    @OneToMany(mappedBy = "componentProduct", fetch = FetchType.LAZY)
+    private List<ComponentEntity> components = new LinkedList<>();
+}
