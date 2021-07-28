38dc1786b6ee0fd900437255aa6fdcb5182404f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-28 15:52:12 +0200

    Update CorsFilterConfig.java

--- spring-boot/warehouse/src/main/java/local/example/warehouse/config/CorsFilterConfig.java
+++ spring-boot/warehouse/src/main/java/local/example/warehouse/config/CorsFilterConfig.java
@@ -18,5 +18,29 @@
 
 package local.example.warehause.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilterConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.addAllowedOrigin("http://127.0.0.1:8010");
+        corsConfiguration.setAllowedMethods(Arrays.asList("DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Accept", "Authorization", "Content-Type", "Origin", "X-Requested-With"));
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
