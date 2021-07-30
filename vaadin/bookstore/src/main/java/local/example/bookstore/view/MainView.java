fde5a755edecfeb008a9d54ae498147de3e935c1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-07-30 17:37:24 +0200

    Update MainView.java

--- vaadin/bookstore/src/main/java/local/example/bookstore/view/MainView.java
+++ vaadin/bookstore/src/main/java/local/example/bookstore/view/MainView.java
@@ -18,14 +18,66 @@
 
 package local.example.bookstore.view;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+
+import local.example.bookstore.view.helper.MenuItemHelper;
+
+import java.util.List;
+import java.util.Optional;
 
 @PageTitle("Main")
 @Theme(themeFolder = "bookstore")
 @PWA(name = "bookstore", shortName = "bookstore", enableInstallPrompt = false)
 public class MainView
         extends AppLayout {
+
+    private final Tabs menuTabs;
+
+    public MainView() {
+        menuTabs = new Tabs();
+    }
+
+    private VerticalLayout createTopBar() {
+        // TODO
+        return null;
+    }
+
+    private HorizontalLayout createHeader() {
+        // TODO
+        return null;
+    }
+
+    private Tabs createMenuTabs() {
+        // TODO
+        return null;
+    }
+
+    private List<Tab> createMenuItems() {
+        // TODO
+        return null;
+    }
+
+    private Tab createTab(MenuItemHelper menuItemHelper) {
+        // TODO
+        return null;
+    }
+
+    private Optional<Tab> getTabForComponent(Component component) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        getTabForComponent(getContent()).ifPresent(menuTabs::setSelectedTab);
+    }
 }
