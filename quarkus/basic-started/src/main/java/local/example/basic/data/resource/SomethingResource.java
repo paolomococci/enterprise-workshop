07b6e7818fdb69ffd6e014927f642af1d54d86f6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-03 10:21:30 +0200

    Update SomethingResource.java

--- quarkus/basic-started/src/main/java/local/example/basic/data/resource/SomethingResource.java
+++ quarkus/basic-started/src/main/java/local/example/basic/data/resource/SomethingResource.java
@@ -18,6 +18,15 @@
 
 package local.example.basic.data.resource;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("things")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class SomethingResource {
 
 }
