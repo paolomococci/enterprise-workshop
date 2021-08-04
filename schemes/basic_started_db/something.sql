edfd84b520188b1e9a962be367ff31cd2a9ab1c6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-04 10:48:53 +0200

    Update something.sql

--- schemes/basic_started_db/something.sql
+++ schemes/basic_started_db/something.sql
@@ -1,8 +1,8 @@
 CREATE TABLE public.something (
 	id int8 NOT NULL,
-	code varchar(16) NULL,
-	description varchar(1024) NULL,
-	name varchar(255) NULL,
+	code varchar(16) NOT NULL,
+	description text NULL,
+	name varchar(255) NOT NULL,
 	CONSTRAINT something_pkey PRIMARY KEY (id),
 	CONSTRAINT uk_fyx02if07tnlum0vogq03shxo UNIQUE (code),
 	CONSTRAINT uk_o5isrkqd0ouogudmo66swbfgp UNIQUE (name)
