0b9e0a4ac60fd46d62c7d686f6a7c144803d94e3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-05 11:28:42 +0200

    Update SomethingResource.java

--- quarkus/basic-started/src/main/java/local/example/basic/data/resource/SomethingResource.java
+++ quarkus/basic-started/src/main/java/local/example/basic/data/resource/SomethingResource.java
@@ -33,6 +33,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -51,8 +52,8 @@ import local.example.basic.error.RestApplicationException;
 
 @Path("things")
 @ApplicationScoped
-@Produces("application/json")
-@Consumes("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SomethingResource {
 
 	private static final Logger LOGGER = Logger.getLogger(SomethingResource.class.getName());
