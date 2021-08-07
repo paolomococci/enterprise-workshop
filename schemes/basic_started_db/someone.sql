60770f81c6cca44a5b40362fa6bcae92a04654ed
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-07 18:43:36 +0200

    Update someone.sql

--- schemes/basic_started_db/someone.sql
+++ schemes/basic_started_db/someone.sql
@@ -1,10 +1,11 @@
 CREATE TABLE public.someone (
 	id int8 NOT NULL,
 	email varchar(255) NOT NULL,
-	"name" varchar(64) NOT NULL,
+	name varchar(64) NOT NULL,
 	phone varchar(64) NOT NULL,
 	surname varchar(64) NOT NULL,
 	CONSTRAINT someone_pkey PRIMARY KEY (id),
 	CONSTRAINT uk_8d661ibgubljxalyx0sf0bbg UNIQUE (phone),
-	CONSTRAINT uk_icn2eejc9d8eu6p820u4yc0no UNIQUE (email)
+	CONSTRAINT uk_icn2eejc9d8eu6p820u4yc0no UNIQUE (email),
+	CONSTRAINT ukhqbwoynk7ww5cmxiohdo2su9c UNIQUE (email, phone)
 );
