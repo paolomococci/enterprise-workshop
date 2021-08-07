0227d21433cd9077a41ee6f6a7249bf2523bb5e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-07 16:05:46 +0200

    Update something.sql

--- schemes/basic_started_db/something.sql
+++ schemes/basic_started_db/something.sql
@@ -2,8 +2,10 @@ CREATE TABLE public.something (
 	id int8 NOT NULL,
 	code varchar(16) NOT NULL,
 	description text NULL,
-	"name" varchar(255) NOT NULL,
+	name varchar(255) NOT NULL,
+	someone_id int8 NULL,
 	CONSTRAINT something_pkey PRIMARY KEY (id),
 	CONSTRAINT uk_fyx02if07tnlum0vogq03shxo UNIQUE (code),
-	CONSTRAINT uk_o5isrkqd0ouogudmo66swbfgp UNIQUE (name)
+	CONSTRAINT uk_o5isrkqd0ouogudmo66swbfgp UNIQUE (name),
+	CONSTRAINT fkp94t22ugr8j79lr86m0mdwuh1 FOREIGN KEY (someone_id) REFERENCES public.someone(id)
 );
