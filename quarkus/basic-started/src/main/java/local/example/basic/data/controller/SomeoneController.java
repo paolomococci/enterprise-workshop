2ebc37dd0c72aa38d39c3ac6f142ebd8f1bd5c56
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-07 10:16:09 +0200

    Update SomeoneController.java

--- quarkus/basic-started/src/main/java/local/example/basic/data/controller/SomeoneController.java
+++ quarkus/basic-started/src/main/java/local/example/basic/data/controller/SomeoneController.java
@@ -18,6 +18,16 @@
 
 package local.example.basic.data.controller;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("somes")
+@ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SomeoneController {
 
 }
