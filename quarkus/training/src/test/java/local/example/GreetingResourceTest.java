4fe0314d3715866b29849766fa61d578f2be0158
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-11 21:36:38 +0200

    Update GreetingResourceTest.java

--- quarkus/training/src/test/java/local/example/GreetingResourceTest.java
+++ quarkus/training/src/test/java/local/example/GreetingResourceTest.java
@@ -30,9 +30,9 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/work")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is("it works"));
     }
 }
