ae5e593bf29e87ca250292dc7015f5a783bf6cff
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-15 12:04:41 +0200

    Update Schedule.java

--- quarkus/training/src/main/java/local/example/data/model/Schedule.java
+++ quarkus/training/src/main/java/local/example/data/model/Schedule.java
@@ -18,6 +18,16 @@
 
 package local.example.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SCHEDULE")
 public class Schedule {
 
+	@Id
+	@GeneratedValue
+	private Long id;
 }
