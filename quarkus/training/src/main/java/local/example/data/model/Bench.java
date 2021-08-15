d513111a19d903ed39f427c123af2f4cf68d7353
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-15 18:49:59 +0200

    Update Bench.java

--- quarkus/training/src/main/java/local/example/data/model/Bench.java
+++ quarkus/training/src/main/java/local/example/data/model/Bench.java
@@ -31,7 +31,17 @@ public class Bench {
 	@GeneratedValue
 	private Long id;
 
+	private String name;
+
 	public Long getId() {
 		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
