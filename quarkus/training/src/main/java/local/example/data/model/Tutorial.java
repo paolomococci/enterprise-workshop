2984b91e73bbb9c05b3ab197e832cd77da46d6a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-15 12:03:57 +0200

    Update Tutorial.java

--- quarkus/training/src/main/java/local/example/data/model/Tutorial.java
+++ quarkus/training/src/main/java/local/example/data/model/Tutorial.java
@@ -18,6 +18,16 @@
 
 package local.example.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TUTORIAL")
 public class Tutorial {
 
+	@Id
+	@GeneratedValue
+	private Long id;
 }
