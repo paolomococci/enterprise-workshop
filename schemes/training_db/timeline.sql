c1508fe69c9ef06c8814d98c3a71301981e43224
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-16 04:00:43 +0200

    Update timeline.sql

--- schemes/training_db/timeline.sql
+++ schemes/training_db/timeline.sql
@@ -1,4 +1,9 @@
 CREATE TABLE public.timeline (
 	id int8 NOT NULL,
-	CONSTRAINT timeline_pkey PRIMARY KEY (id)
+	dayofweek int4 NULL,
+	"start" time NULL,
+	stop time NULL,
+	schedule_id int8 NULL,
+	CONSTRAINT timeline_pkey PRIMARY KEY (id),
+	CONSTRAINT fk2fm2be9niwjmet2cmxpvm77su FOREIGN KEY (schedule_id) REFERENCES public.schedule(id)
 );
