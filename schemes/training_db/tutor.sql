3782b2de50f606c04826f100dba313aac55fd7fd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-16 05:09:34 +0200

    Update tutor.sql

--- schemes/training_db/tutor.sql
+++ schemes/training_db/tutor.sql
@@ -1,5 +1,7 @@
 CREATE TABLE public.tutor (
 	id int8 NOT NULL,
 	"name" varchar(255) NULL,
-	CONSTRAINT tutor_pkey PRIMARY KEY (id)
+	tutorial_id int8 NULL,
+	CONSTRAINT tutor_pkey PRIMARY KEY (id),
+	CONSTRAINT fklllsfntx73bgssu178w2we1wx FOREIGN KEY (tutorial_id) REFERENCES public.tutorial(id)
 );
