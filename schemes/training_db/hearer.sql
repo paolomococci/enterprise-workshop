a39b33720f12a4a2599a42bfc38d58c1a05a50a5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-16 05:09:03 +0200

    Update hearer.sql

--- schemes/training_db/hearer.sql
+++ schemes/training_db/hearer.sql
@@ -1,5 +1,7 @@
 CREATE TABLE public.hearer (
 	id int8 NOT NULL,
-	name varchar(255) NULL,
-	CONSTRAINT hearer_pkey PRIMARY KEY (id)
+	"name" varchar(255) NULL,
+	tutorial_id int8 NULL,
+	CONSTRAINT hearer_pkey PRIMARY KEY (id),
+	CONSTRAINT fk3ymgakf1pfk1wpmpfate1gvqg FOREIGN KEY (tutorial_id) REFERENCES public.tutorial(id)
 );
