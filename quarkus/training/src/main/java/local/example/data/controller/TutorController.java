58c063915f3696b8c10cd3d612f2fdcd368fcad5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-19 20:02:58 +0200

    Update TutorController.java

--- quarkus/training/src/main/java/local/example/data/controller/TutorController.java
+++ quarkus/training/src/main/java/local/example/data/controller/TutorController.java
@@ -21,6 +21,6 @@ package local.example.data.controller;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
 @ResourceProperties(path = "tutor")
-public class TutorController {
+public interface TutorController {
 
 }
