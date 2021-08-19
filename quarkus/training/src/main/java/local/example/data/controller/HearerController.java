c3743cd25ef7a5a8ca5120957743702279044f82
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-19 20:02:21 +0200

    Update HearerController.java

--- quarkus/training/src/main/java/local/example/data/controller/HearerController.java
+++ quarkus/training/src/main/java/local/example/data/controller/HearerController.java
@@ -21,6 +21,6 @@ package local.example.data.controller;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
 @ResourceProperties(path = "hearer")
-public class HearerController {
+public interface HearerController {
 
 }
