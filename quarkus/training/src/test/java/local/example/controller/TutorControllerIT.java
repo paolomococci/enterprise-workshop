a19d2eceffa2f2ca5feeb6b4d12b39b9f014459c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-19 19:20:28 +0200

    Update TutorControllerIT.java

--- quarkus/training/src/test/java/local/example/controller/TutorControllerIT.java
+++ quarkus/training/src/test/java/local/example/controller/TutorControllerIT.java
@@ -21,6 +21,7 @@ package local.example.controller;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class TutorControllerIT {
+public class TutorControllerIT 
+		extends TutorControllerTest {
 
 }
