f3d582a94cdaf11f91215b3e265a520cf7d537c7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-25 09:52:01 +0200

    Update ScheduleControllerIT.java

--- quarkus/training/src/test/java/local/example/controller/it/ScheduleControllerIT.java
+++ quarkus/training/src/test/java/local/example/controller/it/ScheduleControllerIT.java
@@ -20,6 +20,8 @@ package local.example.controller.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+import local.example.controller.ScheduleControllerTest;
+
 @NativeImageTest
 public class ScheduleControllerIT 
 		extends ScheduleControllerTest {
