d77b69480e5460584d2e91f9905e5f0d6b6b443b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-08-26 06:41:56 +0200

    Update ScheduleControllerTest.java

--- quarkus/training/src/test/java/local/example/controller/ScheduleControllerTest.java
+++ quarkus/training/src/test/java/local/example/controller/ScheduleControllerTest.java
@@ -37,19 +37,7 @@ public class ScheduleControllerTest {
 
 	@Test
 	@Order(1)
-	public void create() {
-		// TODO
-	}
-
-	@Test
-	@Order(2)
-	public void read() {
-		// TODO
-	}
-
-	@Test
-	@Order(3)
-	public void readAll() {
+	public void readAllEmptyTest() {
 		List<Schedule> schedules = RestAssured.given()
 				.when().get("/schedule")
 				.then()
@@ -62,14 +50,41 @@ public class ScheduleControllerTest {
 	}
 
 	@Test
-	@Order(4)
-	public void update() {
+	@Order(2)
+	public void createTest() {
 		// TODO
 	}
 
 	@Test
+	@Order(3)
+	public void readTest() {
+		// TODO
+	}
+
+	@Test
+	@Order(4)
+	public void readAllTest() {
+		List<Schedule> schedules = RestAssured.given()
+				.when().get("/schedule")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.jsonPath()
+				.getList(".", Schedule.class);
+		// TODO
+		Assertions.assertTrue(schedules.isEmpty());
+	}
+
+	@Test
 	@Order(5)
-	public void delete() {
+	public void updateTest() {
+		// TODO
+	}
+
+	@Test
+	@Order(6)
+	public void deleteTest() {
 		// TODO
 	}
 }
