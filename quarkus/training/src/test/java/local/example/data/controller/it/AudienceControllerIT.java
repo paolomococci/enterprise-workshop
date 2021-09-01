da20dae2af387bdeb73c2ad714caebb382375b3b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-01 22:42:00 +0200

    Update AudienceControllerIT.java

--- quarkus/training/src/test/java/local/example/data/controller/it/AudienceControllerIT.java
+++ quarkus/training/src/test/java/local/example/data/controller/it/AudienceControllerIT.java
@@ -20,10 +20,10 @@ package local.example.data.controller.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
-import local.example.data.controller.AudienceControllerTest;
+import local.example.data.controller.AudienceControllerTests;
 
 @NativeImageTest
 public class AudienceControllerIT 
-		extends AudienceControllerTest {
+		extends AudienceControllerTests {
 
 }
