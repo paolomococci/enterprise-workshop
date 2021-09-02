1e035ffbf3c2e85fef8a7ae9012e3c9d423cb0c0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-02 18:35:24 +0200

    Update BenchResource.java

--- quarkus/training/src/main/java/local/example/data/resource/BenchResource.java
+++ quarkus/training/src/main/java/local/example/data/resource/BenchResource.java
@@ -21,6 +21,6 @@ package local.example.data.resource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 
 @ResourceProperties(path = "benchs")
-public class BenchResource {
+public interface BenchResource {
 
 }
