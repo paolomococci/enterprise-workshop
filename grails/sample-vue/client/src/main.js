2a18d26b53af0f0e97959cf93f5ea58a45ae91f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-15 11:53:45 +0200

    Create main.js

---
+++ grails/sample-vue/client/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/bootstrap.css'
+import './assets/css/grails.css'
+import './assets/css/main.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
