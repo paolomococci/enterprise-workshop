bf232ddde87e9225a8da116a9cf9138fd23fb5b4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-15 09:41:55 +0200

    Update Invoice.groovy

--- grails/sample-rest/grails-app/domain/sample/rest/data/Invoice.groovy
+++ grails/sample-rest/grails-app/domain/sample/rest/data/Invoice.groovy
@@ -27,6 +27,10 @@ class Invoice {
     Date emission
     Date promise
 
+    static belongsToTheCustomer = [customer: Customer]
+    static belongsToTheSupplier = [supplier: Supplier]
+    static hasManyItems = [items: Item]
+
     static constraints = {
         code size: 8..32, blank: false, unique: true
         cause nullable: true
