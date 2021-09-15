72fa7f70c6ce3dce5f925d51cb5b693681ac1603
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-15 09:30:45 +0200

    Update Customer.groovy

--- grails/sample-rest/grails-app/domain/sample/rest/data/Customer.groovy
+++ grails/sample-rest/grails-app/domain/sample/rest/data/Customer.groovy
@@ -30,11 +30,11 @@ class Customer {
     static hasManyAddresses = [addresses: Address]
 
     static constraints = {
-        code size: 8..32, blank: false
-        name size: 2..32
-        surname size: 2.32
-        gender size: 4..16
-        age min: 18
+        code size: 8..32, blank: false, unique: true
+        name size: 2..32, nullable: true
+        surname size: 2.32, nullable: true
+        gender size: 4..16, nullable: true
+        age min: 18, nullable: true
     }
 
     String toString() {
