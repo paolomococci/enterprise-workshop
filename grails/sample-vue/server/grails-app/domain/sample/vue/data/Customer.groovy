eed1f00cff9d13d0070e8487c829e302f5f45cf0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-16 09:02:17 +0200

    Update Customer.groovy

--- grails/sample-vue/server/grails-app/domain/sample/vue/data/Customer.groovy
+++ grails/sample-vue/server/grails-app/domain/sample/vue/data/Customer.groovy
@@ -20,6 +20,25 @@ package sample.vue.data
 
 class Customer {
 
+    String code
+    String name
+    String surname
+    String gender
+    Integer age
+
+    static hasManyItems = [items: Item]
+    static hasManyInvoices = [invoices: Invoice]
+    static hasManyAddresses = [addresses: Address]
+
     static constraints = {
+        code size: 8..32, blank: false, unique: true
+        name size: 2..32, nullable: true
+        surname size: 2.32, nullable: true
+        gender size: 4..16, nullable: true
+        age min: 18, nullable: true
+    }
+
+    String toString() {
+        code
     }
 }
