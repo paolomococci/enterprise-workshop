7a0ef209c05e46404868cc5ae0b691cd0dbcae2e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-25 21:14:09 +0200

    Create CarrierServiceSpec.groovy

---
+++ grails/sample-angular/server/src/integration-test/groovy/sample/angular/data/CarrierServiceSpec.groovy
@@ -0,0 +1,100 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package sample.angular.data
+
+import grails.testing.mixin.integration.Integration
+import grails.gorm.transactions.Rollback
+
+import org.grails.datastore.mapping.core.Datastore
+import org.springframework.beans.factory.annotation.Autowired
+
+import spock.lang.Specification
+
+@Integration
+@Rollback
+class CarrierServiceSpec
+        extends Specification {
+
+    CarrierService carrierService
+    @Autowired Datastore datastore
+
+    private Long setupData() {
+        // TODO: Populate valid domain instances and return a valid ID
+        //new Carrier(...).save(flush: true, failOnError: true)
+        //new Carrier(...).save(flush: true, failOnError: true)
+        //Carrier carrier = new Carrier(...).save(flush: true, failOnError: true)
+        //new Carrier(...).save(flush: true, failOnError: true)
+        //new Carrier(...).save(flush: true, failOnError: true)
+        assert false, "TODO: Provide a setupData() implementation for this generated test suite"
+        //carrier.id
+    }
+
+    void cleanup() {
+        assert false, "TODO: Provide a cleanup implementation if using MongoDB"
+    }
+
+    void "test get"() {
+        setupData()
+
+        expect:
+        carrierService.get(1) != null
+    }
+
+    void "test list"() {
+        setupData()
+
+        when:
+        List<Carrier> carrierList = carrierService.list(max: 2, offset: 2)
+
+        then:
+        carrierList.size() == 2
+        assert false, "TODO: Verify the correct instances are returned"
+    }
+
+    void "test count"() {
+        setupData()
+
+        expect:
+        carrierService.count() == 5
+    }
+
+    void "test delete"() {
+        Long carrierId = setupData()
+
+        expect:
+        carrierService.count() == 5
+
+        when:
+        carrierService.delete(carrierId)
+        datastore.currentSession.flush()
+
+        then:
+        carrierService.count() == 4
+    }
+
+    void "test save"() {
+        when:
+        assert false, "TODO: Provide a valid instance to save"
+        Carrier carrier = new Carrier()
+        carrierService.save(carrier)
+
+        then:
+        carrier.id != null
+    }
+}
