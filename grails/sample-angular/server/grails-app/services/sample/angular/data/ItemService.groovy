fb458abaf4d4ab5bb9830ba0852cefdb74d91972
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-25 19:46:39 +0200

    Create ItemService.groovy

---
+++ grails/sample-angular/server/grails-app/services/sample/angular/data/ItemService.groovy
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright 2021 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package sample.angular.data
+
+import grails.gorm.services.Service
+
+@Service(Item)
+interface ItemService {
+
+    Item get(Serializable id)
+
+    List<Item> list(Map args)
+
+    Long count()
+
+    Item delete(Serializable id)
+
+    Item save(Item item)
+}
