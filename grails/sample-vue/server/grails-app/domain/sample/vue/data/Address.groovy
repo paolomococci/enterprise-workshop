cd5f802ea146c811bf659c413e2788c10a829c5c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-25 09:26:51 +0200

    Update Address.groovy

--- grails/sample-vue/server/grails-app/domain/sample/vue/data/Address.groovy
+++ grails/sample-vue/server/grails-app/domain/sample/vue/data/Address.groovy
@@ -26,8 +26,10 @@ class Address {
     String street
     String civic
 
-    static belongsToTheCustomer = [customer: Customer]
-    static belongsToTheSupplier = [supplier: Supplier]
+    static belongsTo = [
+            customer: Customer,
+            supplier: Supplier
+    ]
 
     static constraints = {
         code size: 8..32, blank: false, unique: true
