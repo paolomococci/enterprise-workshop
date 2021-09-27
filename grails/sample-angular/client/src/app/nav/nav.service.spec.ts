b71e1b603b9aa80b7918cf794292c9d86447810b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-09-27 17:40:42 +0200

    Update nav.service.spec.ts

--- grails/sample-angular/client/src/app/nav/nav.service.spec.ts
+++ grails/sample-angular/client/src/app/nav/nav.service.spec.ts
@@ -1,5 +1,3 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
 import { NavService } from './nav.service';
 import {HttpClientModule} from "@angular/common/http";
