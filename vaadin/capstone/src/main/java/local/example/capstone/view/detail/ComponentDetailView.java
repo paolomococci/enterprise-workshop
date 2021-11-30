11a5adec278b95a48cda802e6d0a95d8ebccbeb5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-30 06:10:02 +0100

    Update ComponentDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ComponentDetailView.java
@@ -18,7 +18,6 @@
 
 package local.example.capstone.view.detail;
 
-import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -53,7 +52,7 @@ import java.util.Optional;
 @JsModule("./views/details/component-detail-view.ts")
 public class ComponentDetailView
         extends LitTemplate
-        implements HasStyle, BeforeEnterObserver {
+        implements BeforeEnterObserver {
 
     private final String COMPONENT_ID = "componentID";
     private final String COMPONENT_EDIT_ROUTE_TEMPLATE = "component-detail/%d/edit";
