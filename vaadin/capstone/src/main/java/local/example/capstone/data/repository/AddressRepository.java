8712837afff17cc39697e47c0574cd2f7b029110
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-30 05:54:41 +0100

    Update AddressRepository.java

--- vaadin/capstone/src/main/java/local/example/capstone/data/repository/AddressRepository.java
+++ vaadin/capstone/src/main/java/local/example/capstone/data/repository/AddressRepository.java
@@ -20,11 +20,10 @@ package local.example.capstone.data.repository;
 
 import local.example.capstone.data.entity.AddressEntity;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository
-        extends CrudRepository<AddressEntity, Long> {
-
+        extends JpaRepository<AddressEntity, Long> {
 }
