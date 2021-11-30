ac4dacbb3ae26a7d3879fe9cc360c942265eddb0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-30 06:17:43 +0100

    Update ProductDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/ProductDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/ProductDetailView.java
@@ -18,7 +18,6 @@
 
 package local.example.capstone.view.detail;
 
-import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -53,7 +52,7 @@ import java.util.Optional;
 @JsModule("./views/details/product-detail-view.ts")
 public class ProductDetailView
         extends LitTemplate
-        implements HasStyle, BeforeEnterObserver {
+        implements BeforeEnterObserver {
 
     private final String PRODUCT_ID = "productID";
     private final String PRODUCT_EDIT_ROUTE_TEMPLATE = "product-detail/%d/edit";
