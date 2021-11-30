6c376db6db3341a9458737ada13f90de09fc4645
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-30 06:07:44 +0100

    Update AddressDetailView.java

--- vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
+++ vaadin/capstone/src/main/java/local/example/capstone/view/detail/AddressDetailView.java
@@ -18,7 +18,6 @@
 
 package local.example.capstone.view.detail;
 
-import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -52,7 +51,7 @@ import java.util.Optional;
 @JsModule("./views/details/address-detail-view.ts")
 public class AddressDetailView
         extends LitTemplate
-        implements HasStyle, BeforeEnterObserver {
+        implements BeforeEnterObserver {
 
     private final String ADDRESS_ID = "addressID";
     private final String ADDRESS_EDIT_ROUTE_TEMPLATE = "address-detail/%d/edit";
