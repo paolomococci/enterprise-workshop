2afe55c09c35cf332c477a5872431ffd5eed0a03
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-30 06:29:54 +0100

    Update UserView.java

--- vaadin/groupware/src/main/java/local/example/groupware/view/UserView.java
+++ vaadin/groupware/src/main/java/local/example/groupware/view/UserView.java
@@ -41,7 +41,6 @@ import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 
 import elemental.json.Json;
