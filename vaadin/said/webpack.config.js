ca7209eb312d2f9ee89e72b4208294caf7968318
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-12-09 15:27:51 +0100

    Delete webpack.config.js

--- vaadin/said/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const flowDefaults = require('./webpack.generated.js');
-
-/**
- * To change the webpack config, add a new configuration object in
- * the merge arguments below:
- */
-module.exports = merge(flowDefaults,
-);
